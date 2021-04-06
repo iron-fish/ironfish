d7f68ae43833bc1d5242f921f72fe3ce3f3b2218
Jason Spafford <nullprogrammer@gmail.com>
2021-04-05 20:30:45 -0700

    Add github deploy action (#5)
    This deploys the node docker image to github registry as a package
    release.

--- ironfish-cli/scripts/deploy-docker.sh
+++ ironfish-cli/scripts/deploy-docker.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 
-if [ -z "${AWS_REGISTRY_URL-}" ]; then
-    echo "Set AWS_REGISTRY_URL before running deploy-docker.sh"
+if [ -z "${REGISTRY_URL-}" ]; then
+    echo "Set REGISTRY_URL before running deploy-docker.sh"
     exit 1
 fi
 
-docker tag ironfish:latest ${AWS_REGISTRY_URL}/ironfish:latest
-docker push ${AWS_REGISTRY_URL}/ironfish:latest
+docker tag ironfish:latest ${REGISTRY_URL}/ironfish:latest
+docker push ${REGISTRY_URL}/ironfish:latest
