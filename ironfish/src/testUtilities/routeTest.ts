5a2a25953c8116b650de8293727bfc1dc61c28f4
Jason Spafford <nullprogrammer@gmail.com>
2021-04-12 13:33:46 -0700

    Consolidate nodeTest and routeTest (#48)
    In route test, we don't want to use TestStrategy. So we need many of the
    things node test has. We should just inherit it and do the few changes
    we need to make to run route based tests.

---
+++ ironfish/src/testUtilities/routeTest.ts
@@ -0,0 +1,60 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { IronfishNode } from '../node'
+import { IronfishSdk } from '../sdk'
+import { MemoryAdapter } from '../rpc/adapters'
+import { IronfishMemoryClient } from '../rpc/clients'
+import { NodeTest } from './nodeTest'
+import { IronfishBlockchain, IronfishCaptain } from '../strategy'
+import { IronfishTestStrategy } from './strategy'
+/**
+ * Used as an easy wrapper for an RPC route test. Use {@link createRouteTest}
+ * to create one to make sure you call the proper test lifecycle methods on
+ * the RouteTest
+ */
+export class RouteTest extends NodeTest {
+  adapter!: MemoryAdapter
+  client!: IronfishMemoryClient
+
+  async createSetup(): Promise<{
+    sdk: IronfishSdk
+    node: IronfishNode
+    strategy: IronfishTestStrategy
+    captain: IronfishCaptain
+    chain: IronfishBlockchain
+    adapter: MemoryAdapter
+    client: IronfishMemoryClient
+  }> {
+    const setup = await super.createSetup()
+
+    const client = new IronfishMemoryClient()
+    await client.connect(setup.node)
+    const adapter = client.adapter
+
+    return { ...setup, adapter, client }
+  }
+
+  async setup(): Promise<void> {
+    const { sdk, node, captain, strategy, chain, client, adapter } = await this.createSetup()
+
+    this.sdk = sdk
+    this.node = node
+    this.strategy = strategy
+    this.captain = captain
+    this.chain = chain
+    this.client = client
+    this.adapter = adapter
+  }
+}
+
+/** Call this to create a {@link RouteTest} and ensure its test lifecycle
+ * methods are called properly like beforeEach, beforeAll, etc
+ */
+export function createRouteTest(): RouteTest {
+  const routeTest = new RouteTest()
+  beforeAll(() => routeTest.setup())
+  afterEach(() => routeTest.teardownEach())
+  afterAll(() => routeTest.teardownAll())
+  return routeTest
+}
