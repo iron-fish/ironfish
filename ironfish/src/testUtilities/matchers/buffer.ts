0212cc624a0a871feece61595159372c35db8743
Jason Spafford <nullprogrammer@gmail.com>
2021-05-17 15:43:25 -0700

    Add syncer test for actually syncing (#158)
    There was no test that tested paginated syncing, this will test that it
    requests 4 pages of 1 block each.

---
+++ ironfish/src/testUtilities/matchers/buffer.ts
@@ -0,0 +1,35 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import diff from 'jest-diff'
+import { makeResult } from './utils'
+
+function toEqualBuffer(
+  self: Buffer | null | undefined,
+  other: Buffer | null | undefined,
+): jest.CustomMatcherResult {
+  const pass =
+    self === other ||
+    (self == null && other == null) ||
+    (self != null && other != null && self.equals(other))
+
+  if (!pass) {
+    return makeResult(false, `expected buffers to match:\n\n${String(diff(self, other))}`)
+  }
+
+  return makeResult(
+    true,
+    `expected buffers not to match: ${self?.toString('hex') || String(self)}`,
+  )
+}
+
+expect.extend({ toEqualBuffer: toEqualBuffer })
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toEqualBuffer(other: Buffer | null | undefined): R
+    }
+  }
+}
