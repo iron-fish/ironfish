2636fe80303fbc01127ae36ff0cbd676aca5e652
Jason Spafford <nullprogrammer@gmail.com>
2021-05-19 14:02:43 -0700

    Add file logging to ironfish.log
    This adds a new FileReporter which logs to datadir/ironfish.log and does
    not get rotated. The default data dir is ~/.ironfish
    
    This file log is off by default, so you would need to
    enable it by setting 'enableLogFile' to 'true' in your
    node config.

--- ironfish/src/fileSystems/fileSystem.ts
+++ ironfish/src/fileSystems/fileSystem.ts
@@ -1,10 +1,15 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import type fs from 'fs'
 
 export abstract class FileSystem {
   abstract init(): Promise<FileSystem>
-  abstract writeFile(path: string, data: string): Promise<void>
+  abstract writeFile(
+    path: string,
+    data: string,
+    options?: { mode?: fs.Mode; flag?: fs.OpenMode },
+  ): Promise<void>
   abstract readFile(path: string): Promise<string>
   abstract mkdir(path: string, options: { recursive?: boolean }): Promise<void>
   abstract resolve(path: string): string
