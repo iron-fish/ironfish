0c261ce3f8497dc095d284d8320132d1047bb89e
dev-null-noop <dev.null.noop@gmail.com>
2021-05-24 21:07:30 -0400

    Support interactive import in accounts:import  (#155)
    * Support interactive import in accounts:import
    
    This adds a new interactive account import when you haven't piped any
    data into the accounts import command, but also haven't given it a
    file.
    
    * refactor for 3 input methods
    
    refactored to allow for stdin input again. fixed fs import issue.
    
    * Move import methods into their own functions
    
    This is just a small cleanup, rather than a refactor
    
    Co-authored-by: Jason Spafford <nullprogrammer@gmail.com>

--- ironfish-cli/src/commands/accounts/import.ts
+++ ironfish-cli/src/commands/accounts/import.ts
@@ -4,7 +4,7 @@
 import { RemoteFlags } from '../../flags'
 import { IronfishCommand } from '../../command'
 import { JSONUtils, PromiseUtils, Account } from 'ironfish'
-import fs from 'fs'
+import { cli } from 'cli-ux'
 import { flags } from '@oclif/command'
 
 export class ImportCommand extends IronfishCommand {
@@ -34,32 +34,19 @@ export class ImportCommand extends IronfishCommand {
 
     await this.sdk.client.connect()
 
-    let data: string | null = null
-
+    let account: Account | null = null
     if (importPath) {
-      const resolved = this.sdk.fileSystem.resolve(importPath)
-      data = fs.readFileSync(resolved, 'utf8')
-    } else if (process.stdin) {
-      data = ''
-
-      const onData = (dataIn: string): void => {
-        data += dataIn
-      }
-
-      process.stdin.setEncoding('utf8')
-      process.stdin.on('data', onData)
-      while (!process.stdin.readableEnded) {
-        await PromiseUtils.sleep(100)
-      }
-      process.stdin.off('data', onData)
+      account = await this.importFile(importPath)
+    } else if (process.stdin.isTTY) {
+      account = await this.importTTY()
+    } else if (!process.stdin.isTTY) {
+      account = await this.importPipe()
     }
 
-    if (data === null) {
+    if (account === null) {
       this.log('No account to import provided')
       this.exit(1)
     }
-
-    const account = JSONUtils.parse<Account>(data)
 
     const result = await this.sdk.client.importAccount({
       account: account,
@@ -73,6 +60,62 @@ export class ImportCommand extends IronfishCommand {
       this.log(`The default account is now: ${name}`)
     } else {
       this.log(`Run "ironfish accounts:use ${name}" to set the account as default`)
+    }
+  }
+
+  async importFile(path: string): Promise<Account> {
+    const resolved = this.sdk.fileSystem.resolve(path)
+    const data = await this.sdk.fileSystem.readFile(resolved)
+    return JSONUtils.parse<Account>(data)
+  }
+
+  async importPipe(): Promise<Account> {
+    let data = ''
+
+    const onData = (dataIn: string): void => {
+      data += dataIn
+    }
+
+    process.stdin.setEncoding('utf8')
+    process.stdin.on('data', onData)
+
+    while (!process.stdin.readableEnded) {
+      await PromiseUtils.sleep(100)
+    }
+
+    process.stdin.off('data', onData)
+
+    return JSONUtils.parse<Account>(data)
+  }
+
+  async importTTY(): Promise<Account> {
+    const accountName = (await cli.prompt('Enter the account name', {
+      required: true,
+    })) as string
+
+    const spendingKey = (await cli.prompt('Enter the account spending key', {
+      required: true,
+    })) as string
+
+    const incomingViewKey = (await cli.prompt('Enter the account incoming view key', {
+      required: true,
+    })) as string
+
+    const outgoingViewKey = (await cli.prompt('Enter the account outgoing view key', {
+      required: true,
+    })) as string
+
+    const publicAddress = (await cli.prompt('Enter the account public address', {
+      required: true,
+    })) as string
+
+    return {
+      name: accountName,
+      spendingKey: spendingKey,
+      incomingViewKey: incomingViewKey,
+      outgoingViewKey: outgoingViewKey,
+      publicAddress: publicAddress,
+      rescan: null,
     }
   }
 }
