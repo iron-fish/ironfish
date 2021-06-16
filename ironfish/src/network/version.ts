67e9b659f8e28490ee88637a1600fc365f08604c
Derek Guenther <derek@ironfish.network>
2021-06-16 12:28:27 -0700

    Update network version and genesis block (#221)
    * Update network version
    
    * Add the new genesis block
    
    * Disable autoseed when creating a genesis block
    
    * Start updating fixtures
    
    * Generate new fixtures
    
    * Add perf fixture
    
    * push up slow fixtures
    
    Co-authored-by: Jason Spafford <nullprogrammer@gmail.com>

--- ironfish/src/network/version.ts
+++ ironfish/src/network/version.ts
@@ -2,5 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-export const VERSION_PROTOCOL = 5
-export const VERSION_PROTOCOL_MIN = 5
+export const VERSION_PROTOCOL = 6
+export const VERSION_PROTOCOL_MIN = 6
