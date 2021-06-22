17fa34d47c6ac3ac2f2f8efbe929446d9ebd7eaf
Derek Guenther <dguenther9@gmail.com>
2021-06-22 13:52:29 -0700

    Refactor WebRTCConnection to use node-datachannel

--- ironfish/src/network/peers/connections/webRtcConnection.ts
+++ ironfish/src/network/peers/connections/webRtcConnection.ts
@@ -2,32 +2,58 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import nodeDataChannel from 'node-datachannel'
 import type { Logger } from '../../../logger'
 import colors from 'colors/safe'
-import SimplePeer, { SignalData } from 'simple-peer'
 import { Event } from '../../../event'
 import { MetricsMonitor } from '../../../metrics'
 import { LooseMessage, NodeMessageType, parseMessage } from '../../messages'
-import { IsomorphicWebRtc } from '../../types'
 import { Connection, ConnectionDirection, ConnectionType } from './connection'
 import { NetworkError } from './errors'
+import { Assert } from '../../../assert'
+
+export type SignalData =
+  | {
+      type: nodeDataChannel.DescriptionType
+      sdp: string
+    }
+  | CandidateSignal
+
+type CandidateSignal = {
+  type: 'candidate'
+  candidate: {
+    candidate: string
+    sdpMid: string
+    sdpMLineIndex: number
+  }
+}
 
 /**
- * Light wrapper of WebRtc SimplePeer that knows how to send and receive
+ * Light wrapper of node-datachannel that knows how to send and receive
  * LooseMessages instead of strings/data.
  */
 export class WebRtcConnection extends Connection {
-  private readonly peer: SimplePeer.Instance
+  private readonly peer: nodeDataChannel.PeerConnection
+  private datachannel: nodeDataChannel.DataChannel | null = null
 
   /**
-   * Event fired when the peer wants to signal its remote peer that an offer,
-   * answer, or ice candidate is available
+   * True if we've received an SDP message from the peer.
+   */
+  private receivedDescription = false
+
+  /**
+   * Queue for ICE candidates until we've received an SDP message from the peer.
+   */
+  private candidateQueue: CandidateSignal[] = []
+
+  /**
+   * Event fired when the PeerConnection has an SDP message or ICE candidate to send to
+   * the remote peer.
    */
   onSignal = new Event<[SignalData]>()
 
   constructor(
     initiator: boolean,
-    wrtc: IsomorphicWebRtc,
     logger: Logger,
     metrics?: MetricsMonitor,
     options: { simulateLatency?: number } = {},
@@ -44,43 +70,58 @@ export class WebRtcConnection extends Connection {
       this.addLatencyWrapper()
     }
 
-    // TODO: This is using google STUN internally, we need to
-    // make it use any of the websocket peers
-    this.peer = new SimplePeer({ initiator, wrtc })
-
-    this.peer.on('close', () => {
-      this.setState({ type: 'DISCONNECTED' })
+    // TODO: Use our own STUN servers
+    this.peer = new nodeDataChannel.PeerConnection('Peer1', {
+      iceServers: ['stun:stun.l.google.com:19302', 'stun:global.stun.twilio.com:3478'],
     })
 
-    this.peer.on('error', (error: Error) => {
-      this._error = error
-      this.setState({ type: 'DISCONNECTED' })
+    this.peer.onLocalDescription((sdp, type) => {
+      // The TypeScript types for "type" in this callback might not be accurate.
+      // They should be https://www.w3.org/TR/webrtc/#dom-rtcsdptype
+      this.onSignal.emit({ type, sdp })
     })
 
-    this.peer.on('connect', () => {
+    this.peer.onLocalCandidate((candidate, mid) => {
+      this.onSignal.emit({
+        type: 'candidate',
+        candidate: {
+          candidate: candidate,
+          sdpMid: mid,
+          // sdpMLineIndex isn't used by node-datachannel, but helps compatibility
+          // with node-webrtc (and probably browser WebRTC)
+          sdpMLineIndex: 0,
+        },
+      })
+    })
+
+    this.peer.onDataChannel((dc: nodeDataChannel.DataChannel) => {
+      Assert.isNull(this.datachannel)
+      this.initializeDataChannel(dc)
+    })
+
+    if (initiator) {
+      this.initializeDataChannel(this.peer.createDataChannel(''))
+    }
+  }
+
+  initializeDataChannel(dc: nodeDataChannel.DataChannel): void {
+    this.datachannel = dc
+
+    this.datachannel.onOpen(() => {
       if (this.state.type !== 'WAITING_FOR_IDENTITY' && this.state.type !== 'CONNECTED') {
         this.setState({ type: 'WAITING_FOR_IDENTITY' })
       }
     })
 
-    this.peer.on('signal', (signal: SignalData) => {
-      if (this.state.type !== 'CONNECTED' && this.state.type !== 'WAITING_FOR_IDENTITY') {
-        this.setState({ type: 'SIGNALING' })
-      }
-
-      this.onSignal.emit(signal)
+    this.datachannel.onError((e) => {
+      this.close(new NetworkError(e))
     })
 
-    this.peer.on('data', (data: string | Uint8Array) => {
-      // simple-peer will sometimes emit data before emitting 'connect', so
-      // make sure the connection state is updated
-      if (this.state.type === 'SIGNALING') {
-        this.setState({ type: 'WAITING_FOR_IDENTITY' })
-        this.logger.debug(
-          'Received data before WebRTC connect event fired, setting peer to WAITING_FOR_IDENTITY',
-        )
-      }
+    this.datachannel.onClosed(() => {
+      this.close()
+    })
 
+    this.datachannel.onMessage((data: string | Uint8Array) => {
       let stringdata
       if (data instanceof Uint8Array) {
         stringdata = new TextDecoder().decode(data)
@@ -98,7 +139,7 @@ export class WebRtcConnection extends Connection {
         message = parseMessage(stringdata)
       } catch (error) {
         this.logger.warn('Unable to parse webrtc message', stringdata)
-        this.peer.destroy()
+        this.close(error)
         return
       }
 
@@ -118,7 +159,23 @@ export class WebRtcConnection extends Connection {
       if (this.state.type === 'DISCONNECTED' || this.state.type === 'CONNECTING') {
         this.setState({ type: 'SIGNALING' })
       }
-      this.peer.signal(data)
+
+      if (data.type === 'candidate') {
+        if (this.receivedDescription) {
+          this.peer.addRemoteCandidate(data.candidate.candidate, data.candidate.sdpMid)
+        } else {
+          this.candidateQueue.push(data)
+        }
+      } else {
+        this.peer.setRemoteDescription(data.sdp, data.type)
+        this.receivedDescription = true
+        while (this.candidateQueue.length > 0) {
+          const data = this.candidateQueue.shift()
+          if (data) {
+            this.peer.addRemoteCandidate(data.candidate.candidate, data.candidate.sdpMid)
+          }
+        }
+      }
     } catch (error) {
       const message = 'An error occurred when loading signaling data:'
       this.logger.debug(message, error)
@@ -130,7 +187,9 @@ export class WebRtcConnection extends Connection {
    * Encode the message to json and send it to the peer
    */
   send = (message: LooseMessage): boolean => {
-    if (message.type === NodeMessageType.NewBlock && this.peer.bufferSize > 0) {
+    if (!this.datachannel) return false
+
+    if (message.type === NodeMessageType.NewBlock && this.datachannel.bufferedAmount() > 0) {
       return false
     }
 
@@ -138,9 +197,15 @@ export class WebRtcConnection extends Connection {
       this.logger.debug(`${colors.yellow('SEND')} ${this.displayName}: ${message.type}`)
     }
 
+    if (!this.datachannel.isOpen()) {
+      this.logger.debug('Datachannel no longer open, closing connection')
+      this.close()
+      return false
+    }
+
     const data = JSON.stringify(message)
     try {
-      this.peer.send(data)
+      this.datachannel.sendMessage(data)
     } catch (e) {
       this.logger.debug(
         `Error occurred while sending ${message.type} message in state ${this.state.type}`,
@@ -171,6 +236,13 @@ export class WebRtcConnection extends Connection {
     }
 
     this.setState({ type: 'DISCONNECTED' })
-    this.peer.destroy()
+    this.datachannel?.close()
+    try {
+      this.peer.close()
+    } catch (e) {
+      // peer.close() may throw "It seems peer-connection is closed" if the
+      // peer connection has been disposed already
+    }
+    this.datachannel = null
   }
 }
