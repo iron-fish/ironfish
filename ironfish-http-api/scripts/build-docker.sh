41be1bd33a09a7cd43fcbca0414f4d6593bfe766
NullSoldier <nullprogrammer@gmail.com>
2021-06-29 16:27:39 -0700

    Add verify command to HTTP api to verify build
    The command just logs and creates a new server, which would have
    caught the last error we had in the build.

--- ironfish-http-api/scripts/build-docker.sh
+++ ironfish-http-api/scripts/build-docker.sh
@@ -10,3 +10,9 @@ docker build . \
     --progress plain \
     --tag ironfish-http-api:latest \
     --file ironfish-http-api/Dockerfile
+
+docker run \
+    --env DOCKER_VERIFY=1 \
+    --interactive \
+    --rm \
+    ironfish-http-api:latest start
