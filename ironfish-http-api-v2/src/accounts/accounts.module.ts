5ccad2ec1a581724bd342a7fd121322f1b21ba3d
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2021-07-13 13:58:32 -0400

    feat(api): Create controller to fetch account (#298)
    * feat(api): Create controller to fetch accounts
    
    * s/prismaService/prisma

--- ironfish-http-api-v2/src/accounts/accounts.module.ts
+++ ironfish-http-api-v2/src/accounts/accounts.module.ts
@@ -3,11 +3,13 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Module } from '@nestjs/common'
 import { PrismaModule } from '../prisma/prisma.module'
+import { AccountsController } from './accounts.controller'
 import { AccountsService } from './accounts.service'
 
 @Module({
+  controllers: [AccountsController],
+  exports: [AccountsService],
   imports: [PrismaModule],
   providers: [AccountsService],
-  exports: [AccountsService],
 })
 export class AccountsModule {}
