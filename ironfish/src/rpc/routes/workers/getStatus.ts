fd67193a69fc8c5ce1d122a3a2093ecec5e6de29
Yury Starav <yury@ironfish.network>
2021-09-14 10:45:07 -0700

    Add pool speed status to workers info command

--- ironfish/src/rpc/routes/workers/getStatus.ts
+++ ironfish/src/rpc/routes/workers/getStatus.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
+import { IronfishNode } from '../../..'
 import { MathUtils } from '../../../utils'
-import { WorkerPool } from '../../../workerPool'
 import { ApiNamespace, router } from '../router'
 
 export type GetWorkersStatusRequest =
@@ -65,45 +65,18 @@ router.register<typeof GetWorkersStatusRequestSchema, GetWorkersStatusResponse>(
   `${ApiNamespace.worker}/getStatus`,
   GetWorkersStatusRequestSchema,
   (request, node): void => {
-    const jobs = getJobs(node.workerPool)
+    const jobs = getWorkersStatus(node)
 
     if (!request.data?.stream) {
-      request.end({
-        started: node.workerPool.started,
-        workers: node.workerPool.workers.length,
-        executing: node.workerPool.executing,
-        queued: node.workerPool.queued,
-        capacity: node.workerPool.capacity,
-        change: MathUtils.round(node.workerPool.change?.rate5s ?? 0, 2),
-        speed: MathUtils.round(node.workerPool.speed?.rate5s ?? 0, 2),
-        jobs,
-      })
+      request.end(jobs)
       return
     }
 
-    request.stream({
-      started: node.workerPool.started,
-      workers: node.workerPool.workers.length,
-      executing: node.workerPool.executing,
-      queued: node.workerPool.queued,
-      capacity: node.workerPool.capacity,
-      change: MathUtils.round(node.workerPool.change?.rate5s ?? 0, 2),
-      speed: MathUtils.round(node.workerPool.speed?.rate5s ?? 0, 2),
-      jobs,
-    })
+    request.stream(jobs)
 
     const interval = setInterval(() => {
-      const jobs = getJobs(node.workerPool)
-      request.stream({
-        started: node.workerPool.started,
-        workers: node.workerPool.workers.length,
-        executing: node.workerPool.executing,
-        queued: node.workerPool.queued,
-        capacity: node.workerPool.capacity,
-        change: MathUtils.round(node.workerPool.change?.rate5s ?? 0, 2),
-        speed: MathUtils.round(node.workerPool.speed?.rate5s ?? 0, 2),
-        jobs,
-      })
+      const jobs = getWorkersStatus(node)
+      request.stream(jobs)
     }, 1000)
 
     request.onClose.on(() => {
@@ -112,21 +85,30 @@ router.register<typeof GetWorkersStatusRequestSchema, GetWorkersStatusResponse>(
   },
 )
 
-function getJobs(pool: WorkerPool): GetWorkersStatusResponse['jobs'] {
+function getWorkersStatus(node: IronfishNode): GetWorkersStatusResponse {
   const result: GetWorkersStatusResponse['jobs'] = []
 
-  for (const name of pool.stats.keys()) {
+  for (const name of node.workerPool.stats.keys()) {
     // Move control messages to top level message and not request body type
     if (name === 'jobAbort' || name === 'sleep') {
       continue
     }
 
-    const job = pool.stats.get(name)
+    const job = node.workerPool.stats.get(name)
 
     if (job) {
       result.push({ name: name, ...job })
     }
   }
-
-  return result
+  const response: GetWorkersStatusResponse = {
+    started: node.workerPool.started,
+    workers: node.workerPool.workers.length,
+    executing: node.workerPool.executing,
+    queued: node.workerPool.queued,
+    capacity: node.workerPool.capacity,
+    change: MathUtils.round(node.workerPool.change?.rate5s ?? 0, 2),
+    speed: MathUtils.round(node.workerPool.speed?.rate5s ?? 0, 2),
+    jobs: result,
+  }
+  return response
 }
