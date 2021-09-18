b0ff298f7c0d8932b9ed6ad105a17bf06af9162f
Alexander Decurnou <alex@deekerno.com>
2021-09-18 16:45:36 -0400

    IRO-1122: Fix lint error

--- ironfish-cli/src/commands/chain/sync.ts
+++ ironfish-cli/src/commands/chain/sync.ts
@@ -73,7 +73,9 @@ export default class Sync extends IronfishCommand {
     const api = new IronfishApi(apiHost, apiToken)
     const head = await api.head()
 
-    this.log(`Starting from head ${head}`)
+    if (head) {
+      this.log(`Starting from head ${head}`)
+    }
 
     const response = client.followChainStream({
       head: (args.head || head) as string | null,
