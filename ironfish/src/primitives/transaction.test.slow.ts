b8bea376037c28e5444347c2e808e8de6b59777c
yuryironfish <90644789+yuryironfish@users.noreply.github.com>
2021-10-07 15:24:26 -0700

    IRO-1170: Add isMinersFee method to transaction class (#376)
    * IRO-1170: Add isMinersFee method to transaction class

--- ironfish/src/primitives/transaction.test.slow.ts
+++ ironfish/src/primitives/transaction.test.slow.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { createNodeTest, useAccountFixture } from '../testUtilities'
+import { createNodeTest, useAccountFixture, useMinerBlockFixture } from '../testUtilities'
 
 describe('Accounts', () => {
   const nodeTest = createNodeTest()
@@ -25,5 +25,50 @@ describe('Accounts', () => {
     const hashB = transactionB.transactionHash()
 
     expect(hashA.equals(hashB)).toBe(false)
+  }, 600000)
+
+  it('check if a transaction is a miners fee', async () => {
+    const account = await useAccountFixture(nodeTest.accounts)
+
+    const transactionA = await nodeTest.strategy.createMinersFee(
+      BigInt(0),
+      1,
+      account.spendingKey,
+    )
+
+    const transactionB = await nodeTest.strategy.createMinersFee(
+      BigInt(-1),
+      1,
+      account.spendingKey,
+    )
+
+    expect(await transactionA.isMinersFee()).toBe(true)
+    expect(await transactionB.isMinersFee()).toBe(true)
+  })
+
+  it('check if a transaction is not a miners fee', async () => {
+    const nodeA = nodeTest.node
+
+    // Create an account A
+    const accountA = await useAccountFixture(nodeA.accounts, () =>
+      nodeA.accounts.createAccount('testA'),
+    )
+    const accountB = await useAccountFixture(nodeA.accounts, () =>
+      nodeA.accounts.createAccount('testB'),
+    )
+
+    // Create a block with a miner's fee
+    const block1 = await useMinerBlockFixture(nodeA.chain, 2, accountA)
+    await nodeA.chain.addBlock(block1)
+    await nodeA.accounts.updateHead()
+
+    const transaction = await nodeA.accounts.createTransaction(
+      accountA,
+      BigInt(1),
+      BigInt(1),
+      '',
+      accountB.publicAddress,
+    )
+    expect(await transaction.isMinersFee()).toBe(false)
   }, 600000)
 })
