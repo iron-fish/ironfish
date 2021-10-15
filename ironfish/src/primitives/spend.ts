c5a36a785ca696f40d65df10c486101702b707b1
Derek Guenther <derek@ironfish.network>
2021-10-15 11:12:18 -0700

    Retype spend from WasmNoteEncryptedHash to Buffer (#390)

--- ironfish/src/primitives/spend.ts
+++ ironfish/src/primitives/spend.ts
@@ -2,14 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { WasmNoteEncryptedHash } from './noteEncrypted'
 import { Nullifier } from './nullifier'
 
-/**
- * TODO: Why is a spends commitment a note hash?
- */
 export interface Spend {
   nullifier: Nullifier
-  commitment: WasmNoteEncryptedHash
+  commitment: Buffer
   size: number
 }
