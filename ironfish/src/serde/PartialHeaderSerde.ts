4c38e490a420906587ee5dd7e78f20b364d18a0b
yuryironfish <90644789+yuryironfish@users.noreply.github.com>
2021-10-19 22:45:08 -0700

    IRO-1168: Initial commit

--- ironfish/src/serde/PartialHeaderSerde.ts
+++ ironfish/src/serde/PartialHeaderSerde.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import * as bufio from '@bitrelay/bufio'
 import {
   SerializedWasmNoteEncryptedHash,
   WasmNoteEncryptedHash,
@@ -22,8 +23,35 @@ export default class PartialBlockHeaderSerde implements Serde<PartialBlockHeader
     // WHAT EVER YOU DO DO NOT REORDER THE KEYS IN THIS OBJECT
     // It will cause ALL block hashes to change. Yes that is
     // absolutely aweful, and we will fix it.
+    const bw = bufio.write()
+    bw.writeU64(header.sequence)
+    const previousBlockHash = BlockHashSerdeInstance.serialize(header.previousBlockHash)
+    bw.writeU64(previousBlockHash.length)
+    bw.writeString(previousBlockHash)
+    const commitment = this.strategy.noteHasher
+      .hashSerde()
+      .serialize(header.noteCommitment.commitment)
+    bw.writeU64(commitment.length)
+    bw.writeBytes(commitment)
+    bw.writeU64(header.noteCommitment.size)
+    const nullifierCommitment = this.strategy.nullifierHasher
+      .hashSerde()
+      .serialize(header.nullifierCommitment.commitment)
+    bw.writeU64(nullifierCommitment.length)
+    bw.writeString(nullifierCommitment)
+    bw.writeU64(header.nullifierCommitment.size)
+    const target = TargetSerdeInstance.serialize(header.target)
+    bw.writeU64(target.length)
+    bw.writeString(target)
+    bw.writeU64(header.timestamp.getTime())
+    const minersFee = header.minersFee.toString()
+    bw.writeU64(minersFee.length)
+    bw.writeString(minersFee)
+    const graffiti = GraffitiSerdeInstance.serialize(header.graffiti)
+    bw.writeU64(graffiti.length)
+    bw.writeString(graffiti)
 
-    const serialized: SerializedPartialBlockHeader = {
+    /*    const serialized: SerializedPartialBlockHeader = {
       sequence: header.sequence.toString(),
       previousBlockHash: BlockHashSerdeInstance.serialize(header.previousBlockHash),
       noteCommitment: {
@@ -43,31 +71,42 @@ export default class PartialBlockHeaderSerde implements Serde<PartialBlockHeader
       minersFee: header.minersFee.toString(),
       graffiti: GraffitiSerdeInstance.serialize(header.graffiti),
     }
-
-    return Buffer.from(IJSON.stringify(serialized))
+    return Buffer.from(IJSON.stringify(serialized))*/
+    return bw.render()
   }
 
   deserialize(data: Buffer): PartialBlockHeader {
-    const deserialized = IJSON.parse(data.toString()) as SerializedPartialBlockHeader
-
+    const br = bufio.read(data)
+    const sequence = br.readU64()
+    let len = br.readU64()
+    const previousBlockHash = br.readString(len)
+    len = br.readU64()
+    const commitment = br.readBytes(len)
+    const noteCommitmentSize = br.readU64()
+    len = br.readU64()
+    const nullifierCommitment = br.readString(len)
+    const nullifierCommitmentSize = br.readU64()
+    len = br.readU64()
+    const target = br.readString(len)
+    const timestamp = br.readU64()
+    len = br.readU64()
+    const minersFee = br.readString(len)
+    len = br.readU64()
+    const graffiti = br.readString(len)
     return {
-      sequence: Number(deserialized.sequence),
-      previousBlockHash: BlockHashSerdeInstance.deserialize(deserialized.previousBlockHash),
-      target: TargetSerdeInstance.deserialize(deserialized.target),
-      timestamp: new Date(deserialized.timestamp),
-      minersFee: BigInt(deserialized.minersFee),
-      graffiti: GraffitiSerdeInstance.deserialize(deserialized.graffiti),
+      sequence: sequence,
+      previousBlockHash: BlockHashSerdeInstance.deserialize(previousBlockHash),
+      target: TargetSerdeInstance.deserialize(target),
+      timestamp: new Date(timestamp),
+      minersFee: BigInt(minersFee),
+      graffiti: GraffitiSerdeInstance.deserialize(graffiti),
       noteCommitment: {
-        commitment: this.strategy.noteHasher
-          .hashSerde()
-          .deserialize(deserialized.noteCommitment.commitment),
-        size: deserialized.noteCommitment.size,
+        commitment: this.strategy.noteHasher.hashSerde().deserialize(commitment),
+        size: noteCommitmentSize,
       },
       nullifierCommitment: {
-        commitment: this.strategy.nullifierHasher
-          .hashSerde()
-          .deserialize(deserialized.nullifierCommitment.commitment),
-        size: deserialized.nullifierCommitment.size,
+        commitment: this.strategy.nullifierHasher.hashSerde().deserialize(nullifierCommitment),
+        size: nullifierCommitmentSize,
       },
     }
   }
