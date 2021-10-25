3ede448f5225e0cc26fdabf889e5a64db8d8fc32
yuryironfish <90644789+yuryironfish@users.noreply.github.com>
2021-10-25 13:39:02 -0700

    IRO-1185: Update to using bufio

--- ironfish/src/serde/PartialHeaderSerde.ts
+++ ironfish/src/serde/PartialHeaderSerde.ts
@@ -2,7 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import * as bufio from '@bitrelay/bufio'
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+import * as bufio from 'bufio'
 import { WasmNoteEncryptedHash } from '../primitives/noteEncrypted'
 import { NullifierHash } from '../primitives/nullifier'
 import { Target, TargetSerdeInstance } from '../primitives/target'
