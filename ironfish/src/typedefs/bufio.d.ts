4fcbfe95e064edd62c66d788e0e5a8b5b3b610a8
NullSoldier <nullprogrammer@gmail.com>
2021-10-25 14:13:34 -0700

    Started adding bufio type defs

--- ironfish/src/typedefs/bufio.d.ts
+++ ironfish/src/typedefs/bufio.d.ts
@@ -2,4 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
- declare module 'bufio';
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+declare module 'bufio' {
+  type Encoding = 'utf8' | 'ascii'
+
+  class StaticWriter {
+    writeU64(value: number): StaticWriter
+    writeVarString(value: string, enc?: Encoding | null): StaticWriter
+  }
+
+  class BufferWriter {
+    writeU64(value: number): BufferWriter
+    writeVarString(value: string, enc?: Encoding | null): BufferWriter
+  }
+
+  export function write(size?: number): StaticWriter | BufferWriter
+}
