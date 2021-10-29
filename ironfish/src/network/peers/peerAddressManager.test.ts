91b7c011658d589ce6de17964cb5d66543ace990
Alexander Decurnou <alex@deekerno.com>
2021-10-29 12:22:15 -0400

    Add getter, addAddressesFromPeerList, and tests

--- ironfish/src/network/peers/peerAddressManager.test.ts
+++ ironfish/src/network/peers/peerAddressManager.test.ts
@@ -3,6 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 jest.mock('ws')
 
+import { InternalMessageType, PeerList } from '..'
 import {
   getConnectedPeer,
   getConnectingPeer,
@@ -20,7 +21,7 @@ jest.useFakeTimers()
 describe('PeerAddressManager', () => {
   it('constructor load hosts from HostsStore', () => {
     const peerAddressManager = new PeerAddressManager(mockHostsStore())
-    expect(peerAddressManager.addrs).toMatchObject([
+    expect(peerAddressManager.addresses).toMatchObject([
       {
         address: '127.0.0.1',
         port: 9999,
@@ -28,7 +29,39 @@ describe('PeerAddressManager', () => {
     ])
   })
 
-  it('getPeerAddr should return a randomly-sampled PeerAddress', () => {
+  it('addAddressesFromPeerList should add new addresses from a peer list', () => {
+    const peerAddressManager = new PeerAddressManager(mockHostsStore())
+    expect(peerAddressManager.addresses.length).toEqual(1)
+    expect(peerAddressManager.addresses).toMatchObject([
+      {
+        address: '127.0.0.1',
+        port: 9999,
+      },
+    ])
+    const peerList: PeerList = {
+      type: InternalMessageType.peerList,
+      payload: {
+        connectedPeers: [
+          {
+            address: '1.1.1.1',
+            identity: 'blah',
+            name: 'blah',
+            port: 1111,
+          },
+        ],
+      },
+    }
+    peerAddressManager.addAddressesFromPeerList(peerList)
+    expect(peerAddressManager.addresses.length).toEqual(2)
+    expect(peerAddressManager.addresses).toContainEqual({
+      address: '1.1.1.1',
+      identity: 'blah',
+      name: 'blah',
+      port: 1111,
+    })
+  })
+
+  it('getRandomDisconnectedPeer should return a randomly-sampled disconnected peer', () => {
     const allPeerAddresses: PeerAddress[] = []
     const allPeers: Peer[] = []
     const peerAddressManager = new PeerAddressManager(mockHostsStore())
@@ -44,7 +77,7 @@ describe('PeerAddressManager', () => {
         identity: peer.state.identity,
       })
     }
-    peerAddressManager.addrs = allPeerAddresses
+    peerAddressManager.hostsStore.set('knownPeers', allPeerAddresses)
     const sample = peerAddressManager.getRandomDisconnectedPeer(allPeers)
     expect(allPeerAddresses).toContainEqual(sample)
     expect(sample.address).toEqual(disconnectedPeer.address)
--- ironfish/src/network/peers/peerAddressManager.ts
+++ ironfish/src/network/peers/peerAddressManager.ts
@@ -3,20 +3,33 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { ArrayUtils } from '../..'
 import { HostsStore } from '../../fileStores'
-import { Peer } from '..'
+import { Peer, PeerList } from '..'
 import { PeerAddress } from './peerAddress'
 
 export class PeerAddressManager {
-  addrs: Array<PeerAddress>
   hostsStore: HostsStore
 
   constructor(hostsStore: HostsStore) {
     this.hostsStore = hostsStore
-    this.addrs = this.hostsStore.getArray('knownPeers')
+  }
+
+  get addresses(): ReadonlyArray<Readonly<PeerAddress>> {
+    return this.hostsStore.getArray('knownPeers')
+  }
+
+  addAddressesFromPeerList(peerList: PeerList): void {
+    const newAddresses: PeerAddress[] = peerList.payload.connectedPeers.map((peer) => ({
+      address: peer.address,
+      port: peer.port,
+      identity: peer.identity,
+      name: peer.name,
+    }))
+
+    this.hostsStore.set('knownPeers', [...this.addresses, ...newAddresses])
   }
 
   getRandomDisconnectedPeer(peers: Peer[]): PeerAddress {
-    const addressSet = new Set(this.addrs)
+    const addressSet = new Set(this.addresses)
     const connectedPeerAdresses: Set<PeerAddress> = new Set(
       peers
         .filter((peer) => peer.state.type === 'CONNECTED' || peer.state.type === 'CONNECTING')
