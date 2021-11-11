ffc2273a46cccc309cb5f2ba22034414448543dc
Alexander Decurnou <alex@deekerno.com>
2021-11-11 18:57:04 -0500

    Switch to peer list request only

--- ironfish/src/network/peers/peerManager.ts
+++ ironfish/src/network/peers/peerManager.ts
@@ -54,12 +54,6 @@ import { Peer } from './peer'
 const MAX_WEBRTC_BROKERING_ATTEMPTS = 5
 
 /**
- * The minimum version at which the peer manager will send peer list requests
- * to a connected peer
- */
-const MIN_VERSION_FOR_PEER_LIST_REQUESTS = 9
-
-/**
  * PeerManager keeps the state of Peers and their underlying connections up to date,
  * determines how to establish a connection to a given Peer, and provides an event
  * bus for Peers, e.g. for listening to incoming messages from all connected peers.
@@ -91,7 +85,7 @@ export class PeerManager {
    * setInterval handle for distributePeerList, which sends out peer lists and
    * requests for peer lists
    */
-  private distributePeerListHandle: ReturnType<typeof setInterval> | undefined
+  private requestPeerListHandle: ReturnType<typeof setInterval> | undefined
 
   /**
    * setInterval handle for peer disposal, which removes peers from the list that we
@@ -798,7 +792,7 @@ export class PeerManager {
 
   async start(): Promise<void> {
     await Promise.allSettled([
-      (this.distributePeerListHandle = setInterval(() => this.distributePeerList(), 5000)),
+      (this.requestPeerListHandle = setInterval(() => this.requestPeerList(), 5000)),
       (this.disposePeersHandle = setInterval(() => this.disposePeers(), 2000)),
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
       (this.savePeerAddressesHandle = setInterval(async () => {
@@ -812,7 +806,7 @@ export class PeerManager {
    * outstanding connections.
    */
   async stop(): Promise<void> {
-    this.distributePeerListHandle && clearInterval(this.distributePeerListHandle)
+    this.requestPeerListHandle && clearInterval(this.requestPeerListHandle)
     this.disposePeersHandle && clearInterval(this.disposePeersHandle)
     this.savePeerAddressesHandle && clearInterval(this.savePeerAddressesHandle)
     await this.addressManager.save(this.peers)
@@ -837,44 +831,13 @@ export class PeerManager {
     }
   }
 
-  private distributePeerList() {
-    const connectedPeers = []
-
-    for (const p of this.identifiedPeers.values()) {
-      if (p.state.type !== 'CONNECTED') {
-        continue
-      }
-
-      // Worker nodes are nodes that should not be broadcast because they are
-      // meant to connect to a single node and perform one function
-      if (p.isWorker && !this.localPeer.broadcastWorkers) {
-        continue
-      }
-
-      connectedPeers.push({
-        identity: p.state.identity,
-        name: p.name || undefined,
-        address: p.address,
-        port: p.port,
-      })
-    }
-
-    const peerList: PeerList = {
-      type: InternalMessageType.peerList,
-      payload: { connectedPeers },
-    }
-
+  private requestPeerList() {
     const peerListRequest: PeerListRequest = {
       type: InternalMessageType.peerListRequest,
     }
 
     for (const peer of this.getConnectedPeers()) {
-      if (peer.version !== null && peer.version >= MIN_VERSION_FOR_PEER_LIST_REQUESTS) {
-        peer.send(peerListRequest)
-        continue
-      }
-
-      peer.send(peerList)
+      peer.send(peerListRequest)
     }
   }
 
