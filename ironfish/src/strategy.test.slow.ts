8219cfe784f1f728d947d267fb16f364304f37ac
yuryironfish <90644789+yuryironfish@users.noreply.github.com>
2021-11-17 11:13:51 -0800

    Update slow tests

--- ironfish/src/strategy.test.slow.ts
+++ ironfish/src/strategy.test.slow.ts
@@ -218,7 +218,7 @@ describe('Demonstrate the Sapling API', () => {
       }
 
       expect(decryptedNote['wasmNote']).toBeNull()
-      expect(decryptedNote.value()).toBe(BigInt(500000000))
+      expect(decryptedNote.value()).toBe(BigInt(2000000000))
       expect(decryptedNote['wasmNote']).toBeNull()
     }, 60000)
   })
