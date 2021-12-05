5d5f4061b258b7d215aca931d46cf17e9ac4c9f8
PNZ20 <42167577+PNZ20@users.noreply.github.com>
2021-12-05 22:19:03 +0100

    Revert "Update pay.ts"

--- ironfish-cli/src/commands/accounts/pay.ts
+++ ironfish-cli/src/commands/accounts/pay.ts
@@ -41,7 +41,7 @@ export class Pay extends IronfishCommand {
     }),
     fee: flags.string({
       char: 'o',
-      description: 'the fee amount in IRON',
+      description: 'the fee amount in Ore',
     }),
     memo: flags.string({
       char: 'm',
