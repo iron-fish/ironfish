bacb276ae3c5d7355b1c0831a18836d16e63317d
NullSoldier <nullprogrammer@gmail.com>
2022-01-11 00:55:28 -0800

    Bump network version to 13

--- ironfish/src/network/version.ts
+++ ironfish/src/network/version.ts
@@ -2,5 +2,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-export const VERSION_PROTOCOL = 12
-export const VERSION_PROTOCOL_MIN = 12
+export const VERSION_PROTOCOL = 13
+export const VERSION_PROTOCOL_MIN = 13
