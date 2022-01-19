44d0c15bf3d863e1ac0e00f104c9537c6d8adab7
Derek Guenther <derek@ironfish.network>
2022-01-19 13:38:23 -0800

    Remove unnecessary test

--- ironfish/src/account/accounts.test.ts
+++ ironfish/src/account/accounts.test.ts
@@ -8,48 +8,9 @@ import {
   useMinerBlockFixture,
   useTxFixture,
 } from '../testUtilities'
-import { makeBlockAfter } from '../testUtilities/helpers/blockchain'
 
 describe('Accounts', () => {
   const nodeTest = createNodeTest()
-
-  it('sync account head', async () => {
-    const { node, chain, strategy } = nodeTest
-    strategy.disableMiningReward()
-
-    const getTransactionsSpy = jest.spyOn(chain, 'iterateBlockTransactions')
-
-    // G -> A1
-    const blockA1 = await makeBlockAfter(chain, chain.genesis)
-    await expect(chain).toAddBlock(blockA1)
-
-    await node.accounts.updateHead()
-    expect(node.accounts['chainProcessor'].hash).toEqual(blockA1.header.hash)
-    expect(getTransactionsSpy).toBeCalledTimes(2)
-
-    // G -> A1 -> A2
-    const blockA2 = await makeBlockAfter(chain, blockA1)
-    await expect(chain).toAddBlock(blockA2)
-
-    await node.accounts.updateHead()
-    expect(node.accounts['chainProcessor'].hash).toEqual(blockA2.header.hash)
-    expect(getTransactionsSpy).toBeCalledTimes(3)
-
-    // Add 3 more on a heavier fork. Chain A should be removed first, then chain B added
-    // G -> A1 -> A2
-    //   -> B1 -> B2 -> B3
-    const blockB1 = await makeBlockAfter(chain, chain.genesis)
-    const blockB2 = await makeBlockAfter(chain, blockB1)
-    const blockB3 = await makeBlockAfter(chain, blockB2)
-
-    await expect(chain).toAddBlock(blockB1)
-    await expect(chain).toAddBlock(blockB2)
-    await expect(chain).toAddBlock(blockB3)
-
-    await node.accounts.updateHead()
-    expect(node.accounts['chainProcessor'].hash).toEqual(blockB3.header.hash)
-    expect(getTransactionsSpy).toBeCalledTimes(8)
-  }, 8000)
 
   it('should reset when chain processor head does not exist in chain', async () => {
     const { node, strategy } = nodeTest
