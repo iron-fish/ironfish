dbea35401e8f2ec20d9e428f9c3ab00ac9b8d6ef
NullSoldier <nullprogrammer@gmail.com>
2022-01-20 15:19:33 -0800

    Removed double license

--- ironfish/src/utils/file.ts
+++ ironfish/src/utils/file.ts
@@ -3,9 +3,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { MathUtils } from './math'
 
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 type SizeSuffix = { B: string; KB: string; MB: string; GB: string; TB: string; PB: string }
 
 const fileSizeSuffix: SizeSuffix = { B: 'B', KB: 'KB', MB: 'MB', GB: 'GB', TB: 'TB', PB: 'PB' }
