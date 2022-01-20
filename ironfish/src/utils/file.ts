25208ac2b3b381970f45c3cf4d227bb112982eef
NullSoldier <nullprogrammer@gmail.com>
2022-01-20 14:18:08 -0800

    Format miner hash rate using metric system
    This PR wil now format the hash rate in metric increments similar to
    file sizes while still printing the original value. It also adds tests
    for the FileUtils, and fixes a few bugs I saw with rounding that
    produced odd values.

--- ironfish/src/utils/file.ts
+++ ironfish/src/utils/file.ts
@@ -1,31 +1,55 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-type SizeSuffix = { B: string; KB: string; MB: string; GB: string }
+import { MathUtils } from './math'
 
-const fileSizeSuffix: SizeSuffix = { B: 'B', KB: 'KB', MB: 'MB', GB: 'GB' }
-const memorySizeSuffix: SizeSuffix = { B: 'B', KB: 'KiB', MB: 'MiB', GB: 'GiB' }
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+type SizeSuffix = { B: string; KB: string; MB: string; GB: string; TB: string; PB: string }
 
-const formatFileSize = (
-  bytes: number,
-  base = 1000,
-  suffix: SizeSuffix = fileSizeSuffix,
-): string => {
+const fileSizeSuffix: SizeSuffix = { B: 'B', KB: 'KB', MB: 'MB', GB: 'GB', TB: 'TB', PB: 'PB' }
+const hashRateSuffix: SizeSuffix = { B: 'H', KB: 'KH', MB: 'MH', GB: 'GH', TB: 'TH', PB: 'PH' }
+
+const memorySizeSuffix: SizeSuffix = {
+  B: 'B',
+  KB: 'KiB',
+  MB: 'MiB',
+  GB: 'GiB',
+  TB: 'TiB',
+  PB: 'PiB',
+}
+
+const formatSize = (bytes: number, base: number, suffix: SizeSuffix): string => {
   if (bytes < Math.pow(base, 1)) {
     return `${bytes.toFixed(0)} ${suffix.B}`
   }
   if (bytes < Math.pow(base, 2)) {
-    return (bytes / Math.pow(base, 1)).toFixed(0) + ` ${suffix.KB}`
+    return MathUtils.floor(bytes / Math.pow(base, 1), 2).toFixed(2) + ` ${suffix.KB}`
   }
   if (bytes < Math.pow(base, 3)) {
-    return (bytes / Math.pow(base, 2)).toFixed(2) + ` ${suffix.MB}`
-  } else {
-    return (bytes / Math.pow(base, 3)).toFixed(2) + ` ${suffix.GB}`
+    return MathUtils.floor(bytes / Math.pow(base, 2), 2).toFixed(2) + ` ${suffix.MB}`
   }
+  if (bytes < Math.pow(base, 4)) {
+    return MathUtils.floor(bytes / Math.pow(base, 3), 2).toFixed(2) + ` ${suffix.GB}`
+  }
+  if (bytes < Math.pow(base, 5)) {
+    return MathUtils.floor(bytes / Math.pow(base, 4), 2).toFixed(2) + ` ${suffix.TB}`
+  }
+
+  return MathUtils.floor(bytes / Math.pow(base, 5), 2).toFixed(2) + ` ${suffix.PB}`
+}
+
+const formatFileSize = (bytes: number): string => {
+  return formatSize(bytes, 1000, fileSizeSuffix)
 }
 
 const formatMemorySize = (bytes: number): string => {
-  return formatFileSize(bytes, 1024, memorySizeSuffix)
+  return formatSize(bytes, 1024, memorySizeSuffix)
 }
 
-export const FileUtils = { formatFileSize, formatMemorySize }
+const formatHashRate = (bytes: number): string => {
+  return formatSize(bytes, 1000, hashRateSuffix)
+}
+
+export const FileUtils = { formatFileSize, formatMemorySize, formatHashRate }
