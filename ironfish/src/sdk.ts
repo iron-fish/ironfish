d7e96e193a57c41fb0c1db88e1dac0015cb90541
Alexander Decurnou <alex@deekerno.com>
2022-01-24 18:32:23 -0500

    Add hostsStore to sdk.ts

--- ironfish/src/sdk.ts
+++ ironfish/src/sdk.ts
@@ -3,7 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { BoxKeyPair } from 'tweetnacl'
 import { PrivateIdentity } from '.'
-import { Config, ConfigOptions, InternalStore } from './fileStores'
+import { Config, ConfigOptions, HostsStore, InternalStore } from './fileStores'
 import { FileSystem, NodeFileProvider } from './fileSystems'
 import {
   createRootLogger,
@@ -37,6 +37,7 @@ export class IronfishSdk {
   logger: Logger
   metrics: MetricsMonitor
   internal: InternalStore
+  hosts: HostsStore
   strategyClass: typeof Strategy | null
   privateIdentity: BoxKeyPair | null | undefined
 
@@ -46,6 +47,7 @@ export class IronfishSdk {
     clientMemory: IronfishMemoryClient,
     config: Config,
     internal: InternalStore,
+    hosts: HostsStore,
     fileSystem: FileSystem,
     logger: Logger,
     metrics: MetricsMonitor,
@@ -56,6 +58,7 @@ export class IronfishSdk {
     this.clientMemory = clientMemory
     this.config = config
     this.internal = internal
+    this.hosts = hosts
     this.fileSystem = fileSystem
     this.logger = logger
     this.metrics = metrics
@@ -99,6 +102,9 @@ export class IronfishSdk {
 
     const internal = new InternalStore(fileSystem, dataDir)
     await internal.load()
+
+    const hosts = new HostsStore(fileSystem, dataDir)
+    await hosts.load()
 
     if (configOverrides) {
       Object.assign(config.overrides, configOverrides)
@@ -151,6 +157,7 @@ export class IronfishSdk {
       clientMemory,
       config,
       internal,
+      hosts,
       fileSystem,
       logger,
       metrics,
