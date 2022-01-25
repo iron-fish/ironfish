c4eb3a83ac49c46cf521daecfcfcce8d11ac862b
Alexander Decurnou <alex@deekerno.com>
2022-01-25 11:43:33 -0500

    Implement AddressManager functions; implement tests

--- ironfish/src/network/peers/addressManager.test.ts
+++ ironfish/src/network/peers/addressManager.test.ts
@@ -3,14 +3,27 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 jest.mock('ws')
 
-import { mockHostsStore } from '../testUtilities'
+import { InternalMessageType, PeerList } from '../messages'
+import {
+  getConnectedPeer,
+  getConnectingPeer,
+  getDisconnectedPeer,
+  mockFileSystem,
+  mockHostsStore,
+  mockLocalPeer,
+} from '../testUtilities'
 import { AddressManager } from './addressManager'
+import { ConnectionDirection, ConnectionType } from './connections'
+import { Peer } from './peer'
+import { PeerAddress } from './peerAddress'
+import { PeerManager } from './peerManager'
 
 jest.useFakeTimers()
 
 describe('AddressManager', () => {
   it('constructor loads addresses from HostsStore', () => {
-    const addressManager = new AddressManager(mockHostsStore())
+    const addressManager = new AddressManager(mockFileSystem())
+    addressManager.hostsStore = mockHostsStore()
     expect(addressManager.priorConnectedPeerAddresses).toMatchObject([
       {
         address: '127.0.0.1',
@@ -21,8 +34,133 @@ describe('AddressManager', () => {
       {
         address: '1.1.1.1',
         port: 1111,
-        identity: undefined,
+        identity: null,
+        name: null,
       },
     ])
+  })
+
+  it('addAddressesFromPeerList should add new addresses from a peer list', () => {
+    const addressManager = new AddressManager(mockFileSystem())
+    addressManager.hostsStore = mockHostsStore()
+    expect(addressManager.possiblePeerAddresses.length).toEqual(1)
+    expect(addressManager.possiblePeerAddresses).toMatchObject([
+      {
+        address: '1.1.1.1',
+        port: 1111,
+        identity: null,
+        name: null,
+      },
+    ])
+    const peerList: PeerList = {
+      type: InternalMessageType.peerList,
+      payload: {
+        connectedPeers: [
+          {
+            address: '2.2.2.2',
+            identity: 'blah',
+            name: 'blah',
+            port: 2222,
+          },
+        ],
+      },
+    }
+    addressManager.addAddressesFromPeerList(peerList)
+    expect(addressManager.possiblePeerAddresses.length).toEqual(2)
+    expect(addressManager.possiblePeerAddresses).toContainEqual({
+      address: '2.2.2.2',
+      identity: 'blah',
+      name: 'blah',
+      port: 2222,
+    })
+  })
+
+  it('removePeerAddress should remove a peer address', () => {
+    const addressManager = new AddressManager(mockFileSystem())
+    addressManager.hostsStore = mockHostsStore()
+    const pm = new PeerManager(mockLocalPeer(), mockFileSystem())
+    const { peer: peer1 } = getConnectedPeer(pm)
+    const allPeers: Peer[] = [peer1]
+    const allPeerAddresses: PeerAddress[] = []
+
+    for (const peer of allPeers) {
+      allPeerAddresses.push({
+        address: peer.address,
+        port: peer.port,
+        identity: peer.state.identity,
+        name: peer.name,
+      })
+    }
+    addressManager.hostsStore.set('possiblePeers', allPeerAddresses)
+    addressManager.hostsStore.set('priorPeers', allPeerAddresses)
+    addressManager.removePeerAddress(peer1)
+    expect(addressManager.possiblePeerAddresses.length).toEqual(0)
+    expect(addressManager.priorConnectedPeerAddresses.length).toEqual(0)
+  })
+
+  it('getRandomDisconnectedPeer should return a randomly-sampled disconnected peer', () => {
+    const addressManager = new AddressManager(mockFileSystem())
+    addressManager.hostsStore = mockHostsStore()
+    const pm = new PeerManager(mockLocalPeer(), mockFileSystem())
+    const { peer: connectedPeer } = getConnectedPeer(pm)
+    const { peer: connectingPeer } = getConnectingPeer(pm)
+    const { peer: disconnectedPeer } = getDisconnectedPeer(pm)
+    const nonDisconnectedPeers: Peer[] = [connectedPeer, connectingPeer]
+    const allPeerAddresses: PeerAddress[] = []
+
+    for (const peer of [...nonDisconnectedPeers, disconnectedPeer]) {
+      allPeerAddresses.push({
+        address: peer.address,
+        port: peer.port,
+        identity: 'boop',
+        name: peer.name,
+      })
+    }
+
+    addressManager.hostsStore.set('priorPeers', allPeerAddresses)
+    addressManager.hostsStore.set('possiblePeers', allPeerAddresses)
+    const sample = addressManager.getRandomDisconnectedPeerAddress(nonDisconnectedPeers)
+    expect(sample).not.toBeNull()
+    if (sample !== null) {
+      expect(allPeerAddresses).toContainEqual(sample)
+      expect(sample.address).toEqual(disconnectedPeer.address)
+      expect(sample.port).toEqual(disconnectedPeer.port)
+    }
+  })
+
+  describe('save', () => {
+    it('save should persist connected peers', async () => {
+      const addressManager = new AddressManager(mockFileSystem())
+      addressManager.hostsStore = mockHostsStore()
+      const pm = new PeerManager(mockLocalPeer(), mockFileSystem())
+      const { peer: connectedPeer } = getConnectedPeer(pm)
+      const { peer: connectingPeer } = getConnectingPeer(pm)
+      const { peer: disconnectedPeer } = getDisconnectedPeer(pm)
+      const address: PeerAddress = {
+        address: connectedPeer.address,
+        port: connectedPeer.port,
+        identity: connectedPeer.state.identity,
+        name: connectedPeer.name,
+      }
+
+      await addressManager.save([connectedPeer, connectingPeer, disconnectedPeer])
+      expect(addressManager.priorConnectedPeerAddresses).toContainEqual(address)
+    })
+
+    it('should not persist peers that will never retry connecting', async () => {
+      const addressManager = new AddressManager(mockFileSystem())
+      addressManager.hostsStore = mockHostsStore()
+      expect(addressManager.priorConnectedPeerAddresses.length).toEqual(1)
+      const pm = new PeerManager(mockLocalPeer(), mockFileSystem())
+      const { peer: connectedPeer } = getConnectedPeer(pm)
+      const { peer: connectingPeer } = getConnectingPeer(pm)
+      const { peer: disconnectedPeer } = getDisconnectedPeer(pm)
+      connectedPeer
+        .getConnectionRetry(ConnectionType.WebSocket, ConnectionDirection.Outbound)
+        ?.neverRetryConnecting()
+
+      await addressManager.save([connectedPeer, connectingPeer, disconnectedPeer])
+      expect(addressManager.priorConnectedPeerAddresses.length).toEqual(0)
+    })
   })
 })
--- ironfish/src/network/peers/addressManager.ts
+++ ironfish/src/network/peers/addressManager.ts
@@ -1,8 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Peer, PeerList } from '..'
 import { HostsStore } from '../../fileStores'
 import { FileSystem } from '../../fileSystems'
+import { ArrayUtils } from '../../utils'
+import { ConnectionDirection, ConnectionType } from './connections'
 import { PeerAddress } from './peerAddress'
 
 /**
@@ -24,5 +27,124 @@ export class AddressManager {
 
   get possiblePeerAddresses(): ReadonlyArray<Readonly<PeerAddress>> {
     return this.hostsStore.getArray('possiblePeers')
+  }
+  /**
+   * Adds addresses associated to peers received from peer list
+   */
+  addAddressesFromPeerList(peerList: PeerList): void {
+    const newAddresses: PeerAddress[] = peerList.payload.connectedPeers.map((peer) => ({
+      address: peer.address,
+      port: peer.port,
+      identity: peer.identity ?? null,
+      name: peer.name ?? null,
+    }))
+
+    const possiblePeerStrings = this.possiblePeerAddresses
+      .filter((peerAddress) => !(peerAddress.address == null) && !(peerAddress.port == null))
+      .map(
+        (filteredPeerAddress) =>
+          //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          `${filteredPeerAddress.address!}:${filteredPeerAddress.port!}`,
+      )
+    const possiblePeerSet = new Set(possiblePeerStrings)
+
+    const dedupedAddresses = newAddresses.filter(
+      (newAddress) =>
+        !(newAddress.address == null) &&
+        !(newAddress.port == null) &&
+        !possiblePeerSet.has(`${newAddress.address}:${newAddress.port}`),
+    )
+
+    if (dedupedAddresses.length) {
+      this.hostsStore.set('possiblePeers', [...this.possiblePeerAddresses, ...dedupedAddresses])
+    }
+  }
+
+  /**
+   * Returns a peer address for a disconnected peer by using current peers to
+   * filter out peer addresses. It attempts to find a previously-connected
+   * peer address that is not part of an active connection. If there are none,
+   * then it attempts to find a previously-known peer address.
+   */
+  getRandomDisconnectedPeerAddress(peers: Peer[]): PeerAddress | null {
+    if (
+      this.possiblePeerAddresses.length === 0 &&
+      this.priorConnectedPeerAddresses.length === 0
+    ) {
+      return null
+    }
+
+    const currentPeerAddresses = new Set(
+      //eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      peers.filter((peer) => peer.state.identity !== null).map((peer) => peer.state.identity!),
+    )
+
+    const priorConnectedAddressSet = new Set([...this.priorConnectedPeerAddresses])
+
+    const disconnectedPriorAddresses = this.filterConnectedAddresses(
+      priorConnectedAddressSet,
+      currentPeerAddresses,
+    )
+    if (disconnectedPriorAddresses.length) {
+      return ArrayUtils.sampleOrThrow(disconnectedPriorAddresses)
+    } else {
+      const possibleAddressSet = new Set([...this.possiblePeerAddresses])
+      const disconnectedPossibleAddresses = this.filterConnectedAddresses(
+        possibleAddressSet,
+        currentPeerAddresses,
+      )
+      if (disconnectedPossibleAddresses.length) {
+        return ArrayUtils.sampleOrThrow(disconnectedPossibleAddresses)
+      } else {
+        return null
+      }
+    }
+  }
+
+  private filterConnectedAddresses(
+    addressSet: Set<Readonly<PeerAddress>>,
+    connectedPeerAdresses: Set<string>,
+  ): PeerAddress[] {
+    const disconnectedAddresses = [...addressSet].filter(
+      (address) => address.identity !== null && !connectedPeerAdresses.has(address.identity),
+    )
+
+    return disconnectedAddresses
+  }
+
+  /**
+   * Removes address associated with a peer from address stores
+   */
+  removePeerAddress(peer: Peer): void {
+    const filteredPossibles = this.possiblePeerAddresses.filter(
+      (possible) => possible.identity !== peer.state.identity,
+    )
+    const filteredPriorConnected = this.priorConnectedPeerAddresses.filter(
+      (prior) => prior.identity !== peer.state.identity,
+    )
+
+    this.hostsStore.set('possiblePeers', filteredPossibles)
+    this.hostsStore.set('priorPeers', filteredPriorConnected)
+  }
+
+  /**
+   * Persist all currently connected peers and unused peer addresses to disk
+   */
+  async save(peers: Peer[]): Promise<void> {
+    const inUsePeerAddresses = peers
+      .filter(
+        (peer) =>
+          peer.state.type === 'CONNECTED' &&
+          !peer.getConnectionRetry(ConnectionType.WebSocket, ConnectionDirection.Outbound)
+            ?.willNeverRetryConnecting,
+      )
+      .map((peer) => ({
+        address: peer.address,
+        port: peer.port,
+        identity: peer.state.identity ?? null,
+        name: peer.name ?? null,
+      }))
+    this.hostsStore.set('priorPeers', [...inUsePeerAddresses])
+    await this.hostsStore.save()
   }
 }
