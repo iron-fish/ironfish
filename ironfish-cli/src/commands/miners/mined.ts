551d69ae436bb3c3357520bf5e2b28a3fc492c39
Kupuyc <kupuyc@gmail.com>
2022-02-07 22:42:18 +0300

    Migrate miners:mined command from cli-ux to @oclif/core

--- ironfish-cli/src/commands/miners/mined.ts
+++ ironfish-cli/src/commands/miners/mined.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import cli from 'cli-ux'
+import { CliUx } from '@oclif/core'
 import {
   AsyncUtils,
   GENESIS_BLOCK_SEQUENCE,
@@ -56,7 +56,7 @@ export class MinedCommand extends IronfishCommand {
 
     const speed = new Meter()
 
-    const progress = cli.progress({
+    const progress = CliUx.ux.progress({
       format:
         'Scanning blocks: [{bar}] {value}/{total} {percentage}% | ETA: {estimate} | SEQ {sequence}',
     }) as ProgressBar
