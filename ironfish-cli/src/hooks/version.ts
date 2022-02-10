a4b4619597c686a8930753325ec3e7a3161981e6
Kupuyc <kupuyc@gmail.com>
2022-02-10 21:48:17 +0300

    Migrate version hook from @oclif/config to @oclif/core

--- ironfish-cli/src/hooks/version.ts
+++ ironfish-cli/src/hooks/version.ts
@@ -3,11 +3,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 /* eslint-disable no-console */
-import { Hook } from '@oclif/config'
+import { Hook } from '@oclif/core'
 import { Platform } from 'ironfish'
 import { IronfishCliPKG } from '../package'
 
-const VersionHook: Hook<'init'> = (): void => {
+const VersionHook: Hook<'init'> = async () => {
   const isVersionCmd = process.argv[2] === 'version'
   const hasDashVersion = process.argv.some((a) => a === '--version')
   const showVersion = isVersionCmd || hasDashVersion
