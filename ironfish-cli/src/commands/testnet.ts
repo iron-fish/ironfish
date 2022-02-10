cebd322afa7e204f3e904a109a5ae865a77eb9e6
Kupuyc <kupuyc@gmail.com>
2022-02-10 23:02:30 +0300

    Remove unused import

--- ironfish-cli/src/commands/testnet.ts
+++ ironfish-cli/src/commands/testnet.ts
@@ -1,8 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Flags, CliUx } from '@oclif/core'
-import { Assert, WebApi } from 'ironfish'
+import { WebApi } from 'ironfish'
 import { IronfishCommand } from '../command'
 import { DataDirFlag, DataDirFlagKey, VerboseFlag, VerboseFlagKey } from '../flags'
 
