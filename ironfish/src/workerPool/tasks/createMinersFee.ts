2ae4309515278f1c163f706d5d9cc777fd5babd0
Alexander Decurnou <alex@deekerno.com>
2022-02-11 14:45:54 -0500

    Lint fixes

--- ironfish/src/workerPool/tasks/createMinersFee.ts
+++ ironfish/src/workerPool/tasks/createMinersFee.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { generateNewPublicAddress, Note, NoteBuilder, Transaction } from 'ironfish-rust-nodejs'
 import bufio from 'bufio'
+import { generateNewPublicAddress, Note, NoteBuilder, Transaction } from 'ironfish-rust-nodejs'
 import { BigIntUtils } from '../../utils'
 
 export type CreateMinersFeeRequest = {
