587c64c4581b2062bedce785188b659ad3962d97
NullSoldier <nullprogrammer@gmail.com>
2022-02-14 13:45:33 -0800

    Convert telemetry types to strict union types
    The interfaces before did not ensure that if you put "string" that you
    then passed a string in the value. These union types now ensure that the
    value matches the type that is specified.

--- ironfish/src/telemetry/interfaces/field.ts
+++ ironfish/src/telemetry/interfaces/field.ts
@@ -1,8 +1,19 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-export interface Field {
-  name: string
-  type: 'string' | 'boolean' | 'float' | 'integer'
-  value: string | boolean | number
-}
+export type Field =
+  | {
+      name: string
+      type: 'string'
+      value: string
+    }
+  | {
+      name: string
+      type: 'boolean'
+      value: boolean
+    }
+  | {
+      name: string
+      type: 'float' | 'integer'
+      value: number
+    }
