99c916af475a47131f33583089e6a11abd0ab01d
Alexander Decurnou <alex@deekerno.com>
2022-02-23 12:27:20 -0600

    Fix parse error for hosts.json (#1056)

--- ironfish/src/fileStores/hosts.ts
+++ ironfish/src/fileStores/hosts.ts
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { createRootLogger, Logger, ParseJsonError } from '..'
 import { FileSystem } from '../fileSystems'
 import { PeerAddress } from '../network/peers/peerAddress'
 import { KeyStore } from './keyStore'
@@ -16,7 +17,23 @@ export const HostOptionsDefaults: HostsOptions = {
 }
 
 export class HostsStore extends KeyStore<HostsOptions> {
+  logger: Logger
+
   constructor(files: FileSystem, dataDir?: string, configName?: string) {
     super(files, configName || 'hosts.json', HostOptionsDefaults, dataDir)
+    this.logger = createRootLogger()
+  }
+
+  async load(): Promise<void> {
+    try {
+      await super.load()
+    } catch (e) {
+      if (e instanceof ParseJsonError) {
+        this.logger.warn(`Error: Could not parse JSON at ${this.storage.configPath}`)
+        await super.save()
+      } else {
+        throw e
+      }
+    }
   }
 }
