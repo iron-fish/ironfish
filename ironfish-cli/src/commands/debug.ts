6a5b8362ef2ce11d847fceb0ff2161ecd923ee1c
Mat <mat@ironfish.network>
2022-02-24 10:10:41 -0700

    Refactor debug command to be more dynamic
    - Dynamically figures out how big to make the spaces between keys and values so we don't need to manually add a bunch of spaces if we add a bigger label
    - Prints as much data as it can if the database is locked, instead of just pausing and waiting for the DB to open

--- ironfish-cli/src/commands/debug.ts
+++ ironfish-cli/src/commands/debug.ts
@@ -1,10 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { FileUtils, IronfishPKG, NodeUtils } from 'ironfish'
+import { DatabaseIsLockedError, FileUtils, IronfishNode, IronfishPKG } from 'ironfish'
 import os from 'os'
 import { IronfishCommand } from '../command'
 import { LocalFlags } from '../flags'
+
+const SPACE_BUFFER = 8
 
 export default class Debug extends IronfishCommand {
   static description = 'Show debug information to help locate issues'
@@ -16,15 +18,27 @@ export default class Debug extends IronfishCommand {
 
   async start(): Promise<void> {
     const node = await this.sdk.node({ autoSeed: false })
-    await NodeUtils.waitForOpen(node, null, { upgrade: false })
 
-    const accountsMeta = await node.accounts.db.loadAccountsMeta()
-    const accountsHeadHash = accountsMeta.headHash !== null ? accountsMeta.headHash : ''
+    let dbOpen = true
+    try {
+      await node.openDB({ upgrade: false })
+    } catch (err) {
+      if (err instanceof DatabaseIsLockedError) {
+        this.log('Database in use, skipping output that requires database.')
+        this.log('Stop the node and run the debug command again to show full output.\n')
+        dbOpen = false
+      }
+    }
 
-    const accountsBlockHeader = await node.chain.getHeader(Buffer.from(accountsHeadHash, 'hex'))
-    const accountsHeadInChain = !!accountsBlockHeader
-    const accountsHeadSequence = accountsBlockHeader?.sequence || 'null'
+    let output = this.baseOutput(node)
+    if (dbOpen) {
+      output = new Map([...output, ...(await this.outputRequiringDB(node))])
+    }
 
+    this.display(output)
+  }
+
+  baseOutput(node: IronfishNode): Map<string, string> {
     const cpus = os.cpus()
     const cpuNames = [...new Set(cpus.map((c) => c.model))]
     const cpuThreads = cpus.length
@@ -33,18 +47,47 @@ export default class Debug extends IronfishCommand {
 
     const telemetryEnabled = this.sdk.config.get('enableTelemetry').toString()
 
-    this.log(`
-Iron Fish version       ${node.pkg.version} @ ${node.pkg.git}
-Iron Fish library       ${IronfishPKG.version} @ ${IronfishPKG.git}
-Operating system        ${os.type()} ${process.arch}
-CPU model(s)            ${cpuNames.toString()}
-CPU threads             ${cpuThreads}
-RAM total               ${memTotal}
-Node version            ${process.version}
-Telemetry enabled       ${telemetryEnabled}
-Accounts head hash      ${accountsHeadHash}
-Accounts head in chain  ${accountsHeadInChain.toString()}
-Accounts head sequence  ${accountsHeadSequence}
-    `)
+    return new Map<string, string>([
+      ['Iron Fish version', `${node.pkg.version} @ ${node.pkg.git}`],
+      ['Iron Fish library', `${IronfishPKG.version} @ ${IronfishPKG.git}`],
+      ['Operating system', `${os.type()} ${process.arch}`],
+      ['CPU model(s)', `${cpuNames.toString()}`],
+      ['CPU threads', `${cpuThreads}`],
+      ['RAM total', `${memTotal}`],
+      ['Node version', `${process.version}`],
+      ['Telemetry enabled', `${telemetryEnabled}`],
+    ])
+  }
+
+  async outputRequiringDB(node: IronfishNode): Promise<Map<string, string>> {
+    const accountsMeta = await node.accounts.db.loadAccountsMeta()
+    const accountsHeadHash = accountsMeta.headHash !== null ? accountsMeta.headHash : ''
+
+    const accountsBlockHeader = await node.chain.getHeader(Buffer.from(accountsHeadHash, 'hex'))
+    const accountsHeadInChain = !!accountsBlockHeader
+    const accountsHeadSequence = accountsBlockHeader?.sequence || 'null'
+
+    return new Map<string, string>([
+      ['Accounts head hash', `${accountsHeadHash}`],
+      ['Accounts head in chain', `${accountsHeadInChain.toString()}`],
+      ['Accounts head sequence', `${accountsHeadSequence}`],
+    ])
+  }
+
+  display(output: Map<string, string>): void {
+    // Get the longest key length to determine how big to make the space buffer
+    let longestStringLength = 0
+    for (const key of output.keys()) {
+      if (key.length > longestStringLength) {
+        longestStringLength = key.length
+      }
+    }
+
+    const maxKeyWidth = longestStringLength + SPACE_BUFFER
+    output.forEach((value, key) => {
+      const spaceWidth = maxKeyWidth - key.length
+      const spaceString = new Array(spaceWidth).join(' ')
+      this.log(`${key}${spaceString}${value}`)
+    })
   }
 }
