dcbfbc7e76a3ec633f5fdec5501b5ec508afe547
AmberKiso <96642073+AmberKiso@users.noreply.github.com>
2022-02-24 01:00:47 -0700

    Update messages.test.ts

--- ironfish/src/network/messages.test.ts
+++ ironfish/src/network/messages.test.ts
@@ -1,11 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { string } from 'yup'
+ 
 import { Assert } from '..'
 import {
   DisconnectingMessage,
