402487ecf76c081f0fb8a557c998f644a1dd4f83
AmberKiso <96642073+AmberKiso@users.noreply.github.com>
2022-03-02 09:02:38 -0700

    Update blockutil.test.ts

--- ironfish/src/utils/blockutil.test.ts
+++ ironfish/src/utils/blockutil.test.ts
@@ -3,14 +3,22 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Blockchain } from '../blockchain'
 import { GENESIS_BLOCK_SEQUENCE } from '../consensus/consensus'
- 
 import { Strategy } from '../strategy'
 import { makeDbPath } from '../testUtilities/helpers/storage'
 import { WorkerPool } from '../workerPool'
 import { getBlockRange } from './blockchain'
 
+// Strategy for testing:
+// Consider a line of numbers to choose for the input parameters
+// <-----N1---N2----0G---P1---P2---H---X1---X2----->
+// Where N1, N2 are negative
+// G is the genesis block (1)
+// P1 and P2 are positive numbers in the range of blocks
+// H is the height of the chain
+// X1 and X2 exceed the height of the chain
 
-describe('getBlockRange', () => {const workerPool = new WorkerPool()
+describe('getBlockRange', () => {
+  const workerPool = new WorkerPool()
   const strategy = new Strategy(workerPool)
   const chain = new Blockchain({ location: makeDbPath(), strategy })
 
@@ -19,22 +27,19 @@ describe('getBlockRange', () => {const workerPool = new WorkerPool()
     chain.latest.sequence = 10000
   })
 
-  it('prototype', async () => {
-    const param = {start: 2000, stop: 200}
+  it('prototype', () => {
+    const param = { start: 2000, stop: 200 }
 
-    //const {start, stop } = getBlockRange(chain, {start: 2000, stop: 200 })
-    const {start, stop } = getBlockRange(chain, param)
-      
-    expect(start).toEqual(2000)    
-    expect(stop).toEqual(2000)
+    const { start, stop } = getBlockRange(chain, param)
+    expect(start).toEqual(param.start)
+    expect(stop).toEqual(param.start)
   })
 
-  it('G < b < e < M', async () => {
-    const param = {start: 200, stop: 2000}
+  it('G < b < e < M', () => {
+    const param = { start: 200, stop: 2000 }
 
-    const {start, stop } = getBlockRange(chain, param)
-      
-    expect(start).toEqual(200)    
-    expect(stop).toEqual(2000)
+    const { start, stop } = getBlockRange(chain, param)
+    expect(start).toEqual(param.start)
+    expect(stop).toEqual(param.stop)
   })
 })
