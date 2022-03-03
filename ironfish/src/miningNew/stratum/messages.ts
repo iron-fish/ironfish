ddb05b3d88c4e47ba058cc940647256d8e25c725
NullSoldier <nullprogrammer@gmail.com>
2022-03-02 17:35:17 -0800

    Simplify message type

--- ironfish/src/miningNew/stratum/messages.ts
+++ ironfish/src/miningNew/stratum/messages.ts
@@ -1,15 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
- import * as yup from 'yup'
+import * as yup from 'yup'
 
-export type StratumMessage<
-  TType extends string = string,
-  TBody extends unknown | undefined = unknown | undefined,
-> = {
+export type StratumMessage = {
   id: number
-  method: TType
-  body: TBody
+  method: string
+  body?: unknown
 }
 
 export type MiningSubscribeMessage = {
