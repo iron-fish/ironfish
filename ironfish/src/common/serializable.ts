5ba77df4175011a633ecb5a2fe96dee09ad94811
Alexander Decurnou <alex@deekerno.com>
2022-03-07 21:34:48 -0700

    Fix lint

--- ironfish/src/common/serializable.ts
+++ ironfish/src/common/serializable.ts
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import bufio from 'bufio'
-
 export interface Serializable {
   serialize(): Buffer
   deserialize(buffer: Buffer): Serializable
