9a3e1ffe322cb5cc43f7b635803ee4f2230cf529
NullSoldier <nullprogrammer@gmail.com>
2022-03-10 15:03:54 -0800

    Simplify transaction tests
    Combined some tests and made it easier to figure out where to put tests

--- ironfish/src/rpc/routes/transactions/sendTransaction.test.ts
+++ ironfish/src/rpc/routes/transactions/sendTransaction.test.ts
@@ -51,112 +51,97 @@ describe('Transactions sendTransaction', () => {
   })
 
   it('throws if not connected to network', async () => {
+    routeTest.node.peerNetwork['_isReady'] = false
+
     await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
       'Your node must be connected to the Iron Fish network to send a transaction',
     )
   })
 
-  describe('Connected to the network', () => {
-    it('throws if the chain is outdated', async () => {
-      routeTest.node.peerNetwork['_isReady'] = true
+  it('throws if the chain is outdated', async () => {
+    routeTest.node.peerNetwork['_isReady'] = true
+    routeTest.chain.synced = false
 
-      await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
-        'Your node must be synced with the Iron Fish network to send a transaction. Please try again later',
-      )
-    })
-
-    it('throws if not enough funds', async () => {
-      routeTest.node.peerNetwork['_isReady'] = true
-      routeTest.chain.synced = true
-
-      await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
-        'Your balance is too low. Add funds to your account first',
-      )
-    })
-
-    it('throws if the confirmed balance is too low', async () => {
-      routeTest.node.peerNetwork['_isReady'] = true
-      routeTest.chain.synced = true
-
-      jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
-        Promise.resolve({
-          unconfirmed: BigInt(11),
-          confirmed: BigInt(0),
-        }),
-      )
-
-      await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
-        'Please wait a few seconds for your balance to update and try again',
-      )
-    })
-
-    it('calls the pay method on the node', async () => {
-      routeTest.node.peerNetwork['_isReady'] = true
-      routeTest.chain.synced = true
-      routeTest.node.accounts.pay = jest.fn()
-
-      const account = await useAccountFixture(routeTest.node.accounts, 'account')
-      const tx = await useMinersTxFixture(routeTest.node.accounts, account)
-
-      jest.spyOn(routeTest.node.accounts, 'pay').mockResolvedValue(tx)
-
-      jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
-        Promise.resolve({
-          unconfirmed: BigInt(11),
-          confirmed: BigInt(11),
-        }),
-      )
-
-      const result = await routeTest.client.sendTransaction(TEST_PARAMS)
-      expect(result.content.hash).toEqual(tx.hash().toString('hex'))
-    }, 30000)
-
-    describe('with multiple recipients', () => {
-      it('throws if not enough funds', async () => {
-        routeTest.node.peerNetwork['_isReady'] = true
-        routeTest.chain.synced = true
-
-        await expect(routeTest.client.sendTransaction(TEST_PARAMS_MULTI)).rejects.toThrowError(
-          'Your balance is too low. Add funds to your account first',
-        )
-      })
-
-      it('throws if the confirmed balance is too low', async () => {
-        routeTest.node.peerNetwork['_isReady'] = true
-        routeTest.chain.synced = true
-
-        jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
-          Promise.resolve({
-            unconfirmed: BigInt(21),
-            confirmed: BigInt(0),
-          }),
-        )
-
-        await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
-          'Please wait a few seconds for your balance to update and try again',
-        )
-      })
-
-      it('calls the pay method on the node', async () => {
-        routeTest.node.peerNetwork['_isReady'] = true
-        routeTest.chain.synced = true
-        routeTest.node.accounts.pay = jest.fn()
-
-        const account = await useAccountFixture(routeTest.node.accounts, 'account_multi-output')
-        const tx = await useMinersTxFixture(routeTest.node.accounts, account)
-
-        jest.spyOn(routeTest.node.accounts, 'pay').mockResolvedValue(tx)
-
-        jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
-          Promise.resolve({
-            unconfirmed: BigInt(21),
-            confirmed: BigInt(21),
-          }),
-        )
-
-        const result = await routeTest.client.sendTransaction(TEST_PARAMS_MULTI)
-        expect(result.content.hash).toEqual(tx.hash().toString('hex'))
-      }, 30000)
-    })
+    await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
+      'Your node must be synced with the Iron Fish network to send a transaction. Please try again later',
+    )
   })
+
+  it('throws if not enough funds', async () => {
+    routeTest.node.peerNetwork['_isReady'] = true
+    routeTest.chain.synced = true
+
+    await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
+      'Your balance is too low. Add funds to your account first',
+    )
+
+    await expect(routeTest.client.sendTransaction(TEST_PARAMS_MULTI)).rejects.toThrowError(
+      'Your balance is too low. Add funds to your account first',
+    )
+  })
+
+  it('throws if the confirmed balance is too low', async () => {
+    routeTest.node.peerNetwork['_isReady'] = true
+    routeTest.chain.synced = true
+
+    jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
+      Promise.resolve({
+        unconfirmed: BigInt(11),
+        confirmed: BigInt(0),
+      }),
+    )
+
+    await expect(routeTest.client.sendTransaction(TEST_PARAMS)).rejects.toThrowError(
+      'Please wait a few seconds for your balance to update and try again',
+    )
+
+    jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
+      Promise.resolve({
+        unconfirmed: BigInt(21),
+        confirmed: BigInt(0),
+      }),
+    )
+
+    await expect(routeTest.client.sendTransaction(TEST_PARAMS_MULTI)).rejects.toThrowError(
+      'Please wait a few seconds for your balance to update and try again',
+    )
+  })
+
+  it('calls the pay method on the node with single recipient', async () => {
+    routeTest.node.peerNetwork['_isReady'] = true
+    routeTest.chain.synced = true
+
+    const account = await useAccountFixture(routeTest.node.accounts, 'account')
+    const tx = await useMinersTxFixture(routeTest.node.accounts, account)
+
+    jest.spyOn(routeTest.node.accounts, 'pay').mockResolvedValue(tx)
+    jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
+      Promise.resolve({
+        unconfirmed: BigInt(11),
+        confirmed: BigInt(11),
+      }),
+    )
+
+    const result = await routeTest.client.sendTransaction(TEST_PARAMS)
+    expect(result.content.hash).toEqual(tx.hash().toString('hex'))
+  }, 30000)
+
+  it('calls the pay method on the node with multiple recipient', async () => {
+    routeTest.node.peerNetwork['_isReady'] = true
+    routeTest.chain.synced = true
+
+    const account = await useAccountFixture(routeTest.node.accounts, 'account_multi-output')
+    const tx = await useMinersTxFixture(routeTest.node.accounts, account)
+
+    jest.spyOn(routeTest.node.accounts, 'pay').mockResolvedValue(tx)
+    jest.spyOn(routeTest.node.accounts, 'getBalance').mockReturnValueOnce(
+      Promise.resolve({
+        unconfirmed: BigInt(21),
+        confirmed: BigInt(21),
+      }),
+    )
+
+    const result = await routeTest.client.sendTransaction(TEST_PARAMS_MULTI)
+    expect(result.content.hash).toEqual(tx.hash().toString('hex'))
+  }, 30000)
 })
