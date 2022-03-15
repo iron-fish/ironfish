7bcce7a90953b69c7de15063e77be536c3dda362
Derek Guenther <dguenther9@gmail.com>
2022-03-15 16:35:36 -0700

    Convert Sleep task to serialized form (#1126)

--- ironfish/src/workerPool/tasks/handlers.ts
+++ ironfish/src/workerPool/tasks/handlers.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { CreateMinersFeeTask } from './createMinersFee'
+import { SleepTask } from './sleep'
 import { SubmitTelemetryTask } from './submitTelemetry'
 import { VerifyTransactionTask } from './verifyTransaction'
 import { WorkerMessageType } from './workerMessage'
@@ -10,6 +11,7 @@ import { WorkerTask } from './workerTask'
 export const handlers: Record<WorkerMessageType, WorkerTask | undefined> = {
   [WorkerMessageType.CreateMinersFee]: CreateMinersFeeTask.getInstance(),
   [WorkerMessageType.JobError]: undefined,
+  [WorkerMessageType.Sleep]: SleepTask.getInstance(),
   [WorkerMessageType.SubmitTelemetry]: SubmitTelemetryTask.getInstance(),
   [WorkerMessageType.VerifyTransaction]: VerifyTransactionTask.getInstance(),
 }
