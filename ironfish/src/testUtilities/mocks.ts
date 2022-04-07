c893337e12cafd8b6ee64b4267965e7d267d1c92
Mat <mat@ironfish.network>
2022-04-07 13:34:05 -0700

    Lint fix

--- ironfish/src/testUtilities/mocks.ts
+++ ironfish/src/testUtilities/mocks.ts
@@ -4,6 +4,8 @@
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
 export function mockEvent(): any {
   return { on: jest.fn() }
