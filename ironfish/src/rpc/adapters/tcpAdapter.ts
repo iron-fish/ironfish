9a790267efdcf5412596159e4b0ed6bb5f6684f7
Daniel <daniel@Daniels-MacBook-Pro.local>
2022-04-15 10:34:33 -0700

    close TCP server on tcpAdapter stop()

--- ironfish/src/rpc/adapters/tcpAdapter.ts
+++ ironfish/src/rpc/adapters/tcpAdapter.ts
@@ -89,7 +89,15 @@ export class TcpAdapter implements IAdapter {
       })
       sock.destroy()
     })
-    return Promise.resolve()
+    return new Promise((resolve, reject) => {
+      this.server?.close((error) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve()
+        }
+      })
+    })
   }
 
   attach(server: RpcServer): void {
