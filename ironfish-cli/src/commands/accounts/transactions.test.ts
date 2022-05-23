3c8b3fcfaab6151c0b7698199d1944448ee94fce
wd021 <dannywitters@gmail.com>
2022-05-23 15:37:19 +0100

    fix linting

--- ironfish-cli/src/commands/accounts/transactions.test.ts
+++ ironfish-cli/src/commands/accounts/transactions.test.ts
@@ -1,8 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { expect as expectCli, test } from '@oclif/test'
 import { GetTransactionsResponse } from '@ironfish/sdk'
+import { expect as expectCli, test } from '@oclif/test'
 
 describe('accounts:transactions', () => {
   const responseContent: GetTransactionsResponse = {
