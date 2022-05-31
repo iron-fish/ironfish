ff3dca53a9e6d42d02679e59ba0f577bdfcb9f64
Alexander <alex@deekerno.com>
2022-05-31 13:04:33 -0400

    Reset chainProcessor hash if index head not found in database (#1555)

--- ironfish/src/node.ts
+++ ironfish/src/node.ts
@@ -313,7 +313,7 @@ export class IronfishNode {
       await this.rpc.start()
     }
 
-    this.minedBlocksIndexer.start()
+    await this.minedBlocksIndexer.start()
     this.telemetry.submitNodeStarted()
   }
 
