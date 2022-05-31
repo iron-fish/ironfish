66065f923f75ff70fd92bc343734f521a51e48bf
Hugh Cunningham <hugh@ironfish.network>
2022-05-31 16:05:05 -0700

    adds the option to specify a sign key
    the 'signKeyPath' argument could be used to create a series of certs signed with
    the same key, i.e., using that key as a CA

--- ironfish-cli/scripts/generate-tls-keys.sh
+++ ironfish-cli/scripts/generate-tls-keys.sh
@@ -9,12 +9,15 @@ fi
 keyPath=${keyPath:-"node-key.pem"}
 certPath=${certPath:-"node-cert.pem"}
 certSubject=${certSubject:-"/O=Iron Fish"}
+# generates self-signed certificates by default
+signKeyPath=${signKeyPath:-$keyPath}
 
 while [ $# -gt 0 ]; do
   case $1 in
     --keyPath) declare keyPath=$2;;
     --certPath) declare certPath=$2;;
     --certSubject) declare certSubject=$2;;
+    --signKeyPath) declare signKeyPath=$2;;
     *) break;
   esac;
   shift
@@ -24,7 +27,7 @@ echo "Generating node server private key at $keyPath"
 openssl genrsa -out $keyPath 2048 &> /dev/null
 # generate certificate signing request
 openssl req -new -key $keyPath -out csr.pem -subj "$certSubject"
-echo "Generating self-signed node server certificate at $certPath"
-openssl x509 -req -in csr.pem -signkey $keyPath -out $certPath &> /dev/null
+echo "Generating node server certificate at $certPath, signing with sign key at $signKeyPath"
+openssl x509 -req -in csr.pem -signkey $signKeyPath -out $certPath &> /dev/null
 # dispose of request
 rm csr.pem
