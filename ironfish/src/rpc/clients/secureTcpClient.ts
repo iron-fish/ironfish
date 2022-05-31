947f67a959e88c22459ec2be3605775bcaba0e73
Hugh Cunningham <hugh@ironfish.network>
2022-05-31 15:48:11 -0700

    removes unused logger imports

--- ironfish/src/rpc/clients/secureTcpClient.ts
+++ ironfish/src/rpc/clients/secureTcpClient.ts
@@ -2,13 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import tls from 'tls'
-import { createRootLogger, Logger } from '../../logger'
 import { ErrorUtils } from '../../utils'
 import { ConnectionRefusedError } from './errors'
 import { IronfishTcpClient } from './tcpClient'
 
 export class IronfishSecureTcpClient extends IronfishTcpClient {
-
   async connectClient(): Promise<void> {
     return new Promise((resolve, reject): void => {
       const onSecureConnect = () => {
