262009f3e52d0a5dbd40d022e76dde624330b6c3
Hugh Cunningham <hugh@ironfish.network>
2022-06-01 10:10:49 -0700

    adds usage/help text, extends cert duration to 365 days

--- ironfish-cli/scripts/generate-tls-keys.sh
+++ ironfish-cli/scripts/generate-tls-keys.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+print_usage() {
+  cat << EOF
+
+  generate-tls-keys.sh
+
+  generates a private key and a x509 public certificate for enabling TLS
+
+    options:
+      --keyPath      the output path for the private key. default value 'node-key.pem'
+      --certPath     the output path for the public certificate. default value 'node-cert.pem'
+      --certSubject  the subject for the x509 public certificate. default value '/O=Iron Fish'
+      --signKeyPath  the input path of the certificate signing key. uses the generated private key to self-sign by default
+
+EOF
+}
+
 if [ ! command -v openssl &> /dev/null ]; then
   echo "openssl is not installed but is required"
   exit 1
@@ -18,7 +34,7 @@ while [ $# -gt 0 ]; do
     --certPath) declare certPath=$2;;
     --certSubject) declare certSubject=$2;;
     --signKeyPath) declare signKeyPath=$2;;
-    *) break;
+    *) print_usage; exit 0;;
   esac;
   shift
 done
@@ -26,8 +42,8 @@ done
 echo "Generating node server private key at $keyPath"
 openssl genrsa -out $keyPath 2048 &> /dev/null
 # generate certificate signing request
-openssl req -new -key $keyPath -out csr.pem -subj "$certSubject"
+openssl req -new -days 365 -key $keyPath -out csr.pem -subj "$certSubject"
 echo "Generating node server certificate at $certPath, signing with sign key at $signKeyPath"
 openssl x509 -req -in csr.pem -signkey $signKeyPath -out $certPath &> /dev/null
-# dispose of request
+# dispose of request file
 rm csr.pem
