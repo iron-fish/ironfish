07d8cdbf37c4e70e0483562868a6b555c90786f5
Hugh Cunningham <hugh@ironfish.network>
2022-06-17 13:07:52 -0700

    removes await or miner start in miner start cli command

--- ironfish-cli/src/commands/miners/start.ts
+++ ironfish-cli/src/commands/miners/start.ts
@@ -96,7 +96,7 @@ export class Miner extends IronfishCommand {
         port: port,
       })
 
-      await miner.start()
+      miner.start()
       if (flags.richOutput) {
         this.displayHashrate(miner)
       }
