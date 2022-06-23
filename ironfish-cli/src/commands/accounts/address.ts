224f153f22037aae47e097690975551e9b736bce
Jason Spafford <nullprogrammer@gmail.com>
2022-06-23 12:19:06 -0400

    Rename accounts:publickey to accounts:address (#1658)
    I'm doing this because I think this is a more common terminology for
    this concept. Yes, it is a public key but most users in our community
    know this as the address. I created an alias so the old command is still
    usable.
    
    Co-authored-by: Jason Spafford <jasonspafford@Jasons-MacBook-Pro.local>

--- ironfish-cli/src/commands/accounts/address.ts
+++ ironfish-cli/src/commands/accounts/address.ts
@@ -5,15 +5,18 @@ import { Flags } from '@oclif/core'
 import { IronfishCommand } from '../../command'
 import { RemoteFlags } from '../../flags'
 
-export class PublicKeyCommand extends IronfishCommand {
-  static description = `Display or regenerate the account public key`
+export class AddressCommand extends IronfishCommand {
+  static aliases = ['accounts:publickey']
+  static description = `Display or regenerate your account address
+
+  The address for an account is the accounts public key, see more here: https://ironfish.network/docs/whitepaper/5_account`
 
   static flags = {
     ...RemoteFlags,
     generate: Flags.boolean({
       char: 'g',
       default: false,
-      description: 'generate the public key',
+      description: 'generate a new address',
     }),
   }
 
@@ -22,12 +25,12 @@ export class PublicKeyCommand extends IronfishCommand {
       name: 'account',
       parse: (input: string): Promise<string> => Promise.resolve(input.trim()),
       required: false,
-      description: 'name of the account to get a public key',
+      description: 'name of the account to get the address for',
     },
   ]
 
   async start(): Promise<void> {
-    const { args, flags } = await this.parse(PublicKeyCommand)
+    const { args, flags } = await this.parse(AddressCommand)
     const account = args.account as string | undefined
 
     const client = await this.sdk.connectRpc()
