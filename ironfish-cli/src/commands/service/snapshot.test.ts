7307d2450a451c3904a8e1b6631b0bdd862ad6a2
Alexander <alex@deekerno.com>
2022-07-13 17:06:46 -0400

    Add chain snapshot export functionality (#1655)
    * Add route for returning serialized blocks for snapshots
    
    * Basic working implementation. Needs cleanup
    
    * Clean up payload and names
    
    * Add checksum generation; upload manifest.json to S3
    
    * Serialize groups of blocks; fix tests
    
    * UX improvements
    
    * Add .then() to manifest file write; add descriptive names for buffer writers
    
    * Remove start and stop parameters
    
    * Simplify flow for bucket flag
    
    * Use forceLocal flag
    
    * Add AWS SDK for uploading multipart upload of files
    
    * Minimize imports; remove superfluous start and stop params
    
    * Simplify S3 upload
    
    * Replace raw tar command call with node-tar
    
    * Add CLI test
    
    * Fix test
    
    * Add additional test to RPC; fix yup schema
    
    * Add more tests to CLI test
    
    * Update yarn.lock
    
    * Move aws-sdk to ironfish-cli package.json
    
    * Adjust package.json and yarn.lock
    
    * Move to modularized S3 client
    
    * Add more sensible multipart uploading
    
    * Clean up intermediate block files
    
    * Fix tests
    
    * Add back newline
    
    * Update yarn.lock
    
    Co-authored-by: Derek Guenther <dguenther9@gmail.com>

---
+++ ironfish-cli/src/commands/service/snapshot.test.ts
@@ -0,0 +1,147 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { expect as expectCli, test } from '@oclif/test'
+import path from 'path'
+import { Readable } from 'stream'
+
+describe('service:snapshot', () => {
+  jest.spyOn(Date, 'now').mockReturnValue(123456789)
+  const mockedFileSize = 10 * 1024 * 1024
+
+  const manifestContent = {
+    block_height: 3,
+    checksum: 'e5b844cc57f57094ea4585e235f36c78c1cd222262bb89d53c94dcb4d6b3e55d',
+    file_name: `ironfish_snapshot_123456789.tar.gz`,
+    file_size: mockedFileSize,
+    timestamp: 123456789,
+  }
+
+  beforeAll(() => {
+    jest.doMock('@ironfish/sdk', () => {
+      const originalModule = jest.requireActual('@ironfish/sdk')
+
+      const response = {
+        contentStream: jest.fn(async function* () {
+          const stream = [
+            { start: 1, stop: 3 },
+            { start: 1, stop: 3, seq: 3, buffer: Buffer.from('foo') },
+          ]
+
+          for await (const value of stream) {
+            yield value
+          }
+        }),
+      }
+
+      const client = {
+        connect: jest.fn(),
+        snapshotChainStream: jest.fn().mockReturnValue(response),
+      }
+
+      const mockFileSystem = {
+        mkdir: jest.fn(),
+        resolve: jest.fn().mockImplementation((path: string) => path),
+        join: jest.fn().mockImplementation((...paths: string[]) => path.join(...paths)),
+      }
+
+      const module: typeof jest = {
+        ...originalModule,
+        IronfishSdk: {
+          init: jest.fn().mockReturnValue({
+            connectRpc: jest.fn().mockResolvedValue(client),
+            client,
+            fileSystem: mockFileSystem,
+          }),
+          response,
+        },
+      }
+
+      return module
+    })
+
+    jest.mock('@aws-sdk/client-s3', () => {
+      const mockS3Client = {
+        send: jest
+          .fn()
+          .mockReturnValue(Promise.resolve({ UploadId: 'foobar', ETag: 'barbaz' })),
+      }
+
+      return {
+        S3Client: jest.fn(() => mockS3Client),
+        AbortMultipartUploadCommand: jest.fn(),
+        CompleteMultipartUploadCommand: jest.fn(),
+        CreateMultipartUploadCommand: jest.fn(),
+        UploadPartCommand: jest.fn(),
+      }
+    })
+
+    jest.mock('tar', () => {
+      return { create: jest.fn() }
+    })
+
+    jest.mock('fs/promises', () => {
+      const mockFileHandle = {
+        createReadStream: jest
+          .fn()
+          .mockImplementation(() => Readable.from(Buffer.alloc(10 * 1024 * 1024))),
+      }
+
+      const mockStats = {
+        size: mockedFileSize,
+      }
+
+      return {
+        FileHandle: jest.fn(() => mockFileHandle),
+        open: jest.fn().mockReturnValue(Promise.resolve(mockFileHandle)),
+        mkdir: jest.fn().mockImplementation((tempdir: string) => tempdir),
+        rm: jest.fn(),
+        rmdir: jest.fn(),
+        stat: jest.fn(() => mockStats),
+        writeFile: jest.fn(() => Promise.resolve()),
+      }
+    })
+  })
+
+  afterAll(() => {
+    jest.dontMock('@ironfish/sdk')
+  })
+
+  describe('given a bucket, exports a snapshot of the chain and uploads it', () => {
+    test
+      .stdout()
+      .command(['service:snapshot', '--bucket=testbucket'])
+      .exit(0)
+      .it('outputs the contents of manifest.json', (ctx) => {
+        expectCli(ctx.stdout).include(JSON.stringify(manifestContent, undefined, '  '))
+      })
+  })
+
+  describe('given a path, exports a snapshot of the chain to that path', () => {
+    test
+      .stdout()
+      .command(['service:snapshot', '--path=foobar'])
+      .exit(0)
+      .it('exports blocks and snapshot to correct path', (ctx) => {
+        expectCli(ctx.stdout).include(
+          `Zipping\n    SRC foobar/blocks\n    DST foobar/${manifestContent.file_name}\n\n`,
+        )
+      })
+  })
+
+  describe('given a path and bucket, exports a snapshot of the chain to that path and uploads it', () => {
+    test
+      .stdout()
+      .command(['service:snapshot', '--path=foobar', '--bucket=testbucket'])
+      .exit(0)
+      .it(
+        'exports blocks and snapshot to correct path, and outputs the contents of manifest.json',
+        (ctx) => {
+          expectCli(ctx.stdout).include(
+            `Zipping\n    SRC foobar/blocks\n    DST foobar/${manifestContent.file_name}\n\n`,
+          )
+          expectCli(ctx.stdout).include(JSON.stringify(manifestContent, undefined, '  '))
+        },
+      )
+  })
+})
