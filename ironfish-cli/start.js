af57baaac8287912c4fb9a770f8aeba1b8263294
hamza-siddiq <92462002+hamza-siddiq@users.noreply.github.com>
2022-07-17 00:27:32 +0300

    Fixed bug 1794
    Fixed the following error: "Node randomly crashes with "error Command failed with exit code 1."

--- ironfish-cli/start.js
+++ ironfish-cli/start.js
@@ -6,6 +6,7 @@ const execSync = require('child_process').execSync
 
 const arg = process.argv.slice(2) || ''
 
-execSync(`tsc-watch --build --onSuccess "yarn run start:js ${arg.join(' ')}"`, {
+execSync(`tsc-watch --build --onSuccess " while true do yarn run start:js ${arg.join(' ')} done"`, {
   stdio: [0, 1, 2],
 })
+
