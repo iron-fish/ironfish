50c795bbdfd4a8509d9fdfcecfbbf42ce9d2521e
danield9tqh <danield9tqh@gmail.com>
2022-07-21 14:50:40 -0400

    fix tests and add test for same transaction

--- ironfish/src/testUtilities/mocks.ts
+++ ironfish/src/testUtilities/mocks.ts
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { SerializedTransaction } from '../primitives/transaction'
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
