5f604ed18ed34032b6feb66ef519d253440d970a
Hugh Cunningham <hugh@ironfish.network>
2022-08-02 11:57:13 -0700

    removes unused snapshots code
    - removes rpc routes
    
    intent is to removie unused code before merging `snapshots` into `staging` but
    preserve commit history.

--- ironfish/src/rpc/routes/chain/index.ts
+++ ironfish/src/rpc/routes/chain/index.ts
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-export * from './importChain'
 export * from './exportChain'
 export * from './followChain'
 export * from './getBlock'
@@ -10,4 +9,3 @@ export * from './getBlockInfo'
 export * from './getChainInfo'
 export * from './getTransactionStream'
 export * from './showChain'
-export * from './snapshotChain'
