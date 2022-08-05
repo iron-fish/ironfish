e5300c9ee414885a417ab03e1ba64bed1db691e4
hamza-siddiq <92462002+hamza-siddiq@users.noreply.github.com>
2022-08-06 02:47:30 +0500

    Revert "Fix a bug where tests were timing out"
    This reverts commit 5d2188653378d536321a6c1f54a82ca0c4871b34.

--- config/jest.config.base.js
+++ config/jest.config.base.js
@@ -6,9 +6,4 @@ module.exports = {
   coverageProvider: "v8",
   coverageReporters: ["text-summary", "json", "clover", "text"],
   testPathIgnorePatterns: ['.*\\.test\\.slow\\.ts$', '.*\\.test\\.perf\\.ts$'],
-
-  // set timeout to a higher value than the default (5000), in order to avoid 
-  // timing out when running tests. 32 seconds should suffice for the existing
-  // tests.
-  testTimeout: 32000
 };
