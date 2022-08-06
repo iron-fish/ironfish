514103447da409232b0795f299225b5226fa6c03
Mat <mat@ironfish.network>
2022-08-05 21:43:41 -0700

    Remove last few tweetnacl references

--- ironfish/src/sdk.ts
+++ ironfish/src/sdk.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { BoxKeyPair } from 'tweetnacl'
+import { BoxKeyPair } from '@ironfish/rust-nodejs'
 import { Config, ConfigOptions, DEFAULT_DATA_DIR, InternalStore } from './fileStores'
 import { FileSystem, NodeFileProvider } from './fileSystems'
 import {
