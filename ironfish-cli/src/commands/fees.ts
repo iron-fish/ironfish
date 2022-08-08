e9f4d0bd6c2e35375313fe0f0941d1bd36c195f3
wd021 <dannywitters@gmail.com>
2022-08-08 13:50:36 +0200

    update assert if blocks to fetch < head height

--- ironfish-cli/src/commands/fees.ts
+++ ironfish-cli/src/commands/fees.ts
@@ -1,16 +1,15 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
-import { RpcRequestError } from '@ironfish/sdk'
-import { CliUx, Flags } from '@oclif/core'
+import { Assert } from '@ironfish/sdk'
+import { BlockHeader } from '@ironfish/sdk'
 import { IronfishCommand } from '../command'
 import { RemoteFlags } from '../flags'
 
 const DEFAULT_BLOCKS_TO_FETCH = 10
 
 export class FeeCommand extends IronfishCommand {
-  static description = ``
+  static description = `Get fee distribution for most recent blocks`
 
   static flags = {
     ...RemoteFlags,
@@ -21,7 +20,7 @@ export class FeeCommand extends IronfishCommand {
       name: 'blocks',
       parse: (input: string): Promise<string> => Promise.resolve(input.trim()),
       required: false,
-      description: 'latest number of blocks to get fee distribution for',
+      description: 'latest number of blocks to calculate fees for',
     },
   ]
 
@@ -29,13 +28,26 @@ export class FeeCommand extends IronfishCommand {
     const { args } = await this.parse(FeeCommand)
     const blocks = args.blocks as number | null
 
+    const numOfBlocks = blocks || DEFAULT_BLOCKS_TO_FETCH
+
     const client = await this.sdk.connectRpc()
 
-    const numOfBlocks = blocks || DEFAULT_BLOCKS_TO_FETCH
-    const response = await client.getFees({ numOfBlocks })
+    try {
+      const response = await client.getFees({ numOfBlocks })
 
-    this.log(
-      `Fee distribution for last ${numOfBlocks} block${numOfBlocks > 1 ? 's' : ''}: (${response.content.startBlock} - ${response.content.endBlock})\np25: ${response.content.p25} ORE\np50: ${response.content.p50} ORE\np75: ${response.content.p75} ORE`,
-    )
+      this.log(
+        `Fee distribution for last ${numOfBlocks} block${numOfBlocks > 1 ? 's' : ''}: (${
+          response.content.startBlock
+        } - ${response.content.endBlock})\np25: ${response.content.p25 || '-'} ORE\np50: ${
+          response.content.p50 || '-'
+        } ORE\np75: ${response.content.p75 || '-'} ORE`,
+      )
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        this.error(error.message)
+      }
+
+      this.exit(1)
+    }
   }
 }
