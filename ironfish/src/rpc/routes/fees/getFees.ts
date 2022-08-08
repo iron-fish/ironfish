028a59ea70e8191bc1be47a0ab839a7b80c9bd83
wd021 <dannywitters@gmail.com>
2022-08-08 12:42:29 +0200

    add getFees method to rpc route

---
+++ ironfish/src/rpc/routes/fees/getFees.ts
@@ -0,0 +1,79 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import * as yup from 'yup'
+import { Assert } from '../../../assert'
+import percentile from '../../../utils/percentile'
+import { ApiNamespace, router } from '../router'
+
+export type GetFeesRequest = { numOfBlocks: number }
+export type GetFeesResponse = {
+  startBlock: number
+  endBlock: number
+  fees: string
+  p25: number | null
+  p50: number | null
+  p75: number | null
+}
+
+export const GetFeesRequestSchema: yup.ObjectSchema<GetFeesRequest> = yup
+  .object({
+    numOfBlocks: yup.number().defined(),
+  })
+  .defined()
+
+export const GetFeesResponseSchema: yup.ObjectSchema<GetFeesResponse> = yup
+  .object({
+    startBlock: yup.number().defined(),
+    endBlock: yup.number().defined(),
+    fees: yup.string(),
+    p25: yup.number().nullable().defined(),
+    p50: yup.number().nullable().defined(),
+    p75: yup.number().nullable().defined(),
+  })
+  .defined()
+
+router.register<typeof GetFeesRequestSchema, GetFeesResponse>(
+  `${ApiNamespace.fees}/getFees`,
+  GetFeesRequestSchema,
+  async (request, node): Promise<void> => {
+    const numOfBlocks = request.data.numOfBlocks
+
+    const latestBlockHeader = node.chain.latest
+    let latestBlock = await node.chain.getBlock(latestBlockHeader.hash)
+    Assert.isNotNull(latestBlock, 'No block found')
+
+    const fees: number[] = []
+
+    let startBlock: number = latestBlockHeader.sequence
+    const endBlock: number = latestBlockHeader.sequence
+
+    latestBlock.transactions.forEach((transaction) => {
+      if (!transaction.isMinersFee()) {
+        fees.push(Number(transaction.fee()))
+      }
+    })
+
+    for (let i = 0; i < numOfBlocks; i++) {
+      latestBlock = await node.chain.getBlock(latestBlock.header.previousBlockHash)
+      Assert.isNotNull(latestBlock, 'No block found')
+
+      startBlock = latestBlock.header.sequence
+
+      latestBlock.transactions.forEach((transaction) => {
+        if (!transaction.isMinersFee()) {
+          fees.push(Number(transaction.fee()))
+        }
+      })
+    }
+
+    request.end({
+      startBlock,
+      endBlock,
+      fees: JSON.stringify(fees),
+      p25: percentile(fees, 25),
+      p50: percentile(fees, 50),
+      p75: percentile(fees, 75),
+    })
+  },
+)
