a4e3f7cd98d0991624c5f5eaf8cadfbbf43b493a
Ayman Al-Qadhi <alqd@protonmail.com>
2022-08-12 01:21:12 +0200

    Fix import lint warning

--- ironfish/src/merkletree/merkletree.ts
+++ ironfish/src/merkletree/merkletree.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { VerifyError } from '../blockchain'
 import { Assert } from '../assert'
+import { VerifyError } from '../blockchain'
 import { VerificationResultReason } from '../consensus'
 import { JsonSerializable } from '../serde'
 import {
