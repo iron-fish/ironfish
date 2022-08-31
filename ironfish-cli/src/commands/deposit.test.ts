ba04da6425d6d64b5c8df3fb3d153de7b95c37a2
Lawrence Wisne <lawrence@ironfish.network>
2022-08-31 11:24:53 -0400

    tweaking test

--- ironfish-cli/src/commands/deposit.test.ts
+++ ironfish-cli/src/commands/deposit.test.ts
@@ -6,14 +6,13 @@ import { expect as expectCli, test } from '@oclif/test'
 
 describe('deposit command', () => {
   const fee = 1
-  const amount = 0.000001
+  const amount = 0.1
   const to =
     '997c586852d1b12da499bcff53595ba37d04e4909dbdb1a75f3bfd90dd7212217a1c2c0da652d187fc52ed'
   const from = 'test_account'
   const memo = 'test memo for a transaction'
   const hash =
     'aaaa586852d1b12da499bcff53595ba37d04e4909dbdb1a75f3bfd90dd7212217a1c2c0da652d187fc52ed'
-  const confirmationString = `$IRON 0.00001000 ($ORE 1,000) plus a transaction fee of $IRON 0.00000001 ($ORE 1) to ${to} from the account ${from}`
 
   const getAccountBalance = jest.fn().mockResolvedValue({ content: { confirmed: 10000001 } })
   IronfishSdk.init = jest.fn().mockImplementation(() => {
