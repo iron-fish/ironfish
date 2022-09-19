e27d35adcc4e2c98e4435f424529c0542776a0e0
Derek Guenther <derek@ironfish.network>
2022-09-19 16:41:50 -0400

    Fix lint

--- ironfish-cli/src/commands/chain/benchmark.ts
+++ ironfish-cli/src/commands/chain/benchmark.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { BenchUtils, ErrorUtils, IronfishSdk } from '@ironfish/sdk'
+import { BenchUtils, IronfishSdk } from '@ironfish/sdk'
 import { CliUx, Flags } from '@oclif/core'
 import fs from 'fs/promises'
 import path from 'path'
