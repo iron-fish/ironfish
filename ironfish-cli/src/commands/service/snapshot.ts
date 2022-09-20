30b38dd455bbcc4254bfb951965322f06e531dc6
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2022-09-20 15:07:17 -0700

    removes snapshot cli options for aws keys (#2219)
    the AWS SDK can automatically read credentials from IAM roles or from
    environment variables. the cli options require these keys to either be set on
    the command line or in environment variables.
    
    automatically reading credentials from IAM roles attached to an EC2 instance is
    very convenient for automating service:snapshot since it allows us to run the
    command without setting keys on a remote instance.
    
    users of service:snapshot can still set each of these variables as environment
    variables when running the command
    
    - removes options for aws_access_key_id, aws_secret_access_key, region
    - updates description for upload option to indicate that AWS credentials must be
      set in order to upload

--- ironfish-cli/src/commands/service/snapshot.ts
+++ ironfish-cli/src/commands/service/snapshot.ts
@@ -24,6 +24,8 @@ export default class CreateSnapshot extends IronfishCommand {
     upload: Flags.boolean({
       default: false,
       allowNo: true,
+      description:
+        'Upload the snapshot to an S3 bucket. AWS credentials and region must be configured or set in the environment',
     }),
     bucket: Flags.string({
       char: 'b',
@@ -32,29 +34,11 @@ export default class CreateSnapshot extends IronfishCommand {
       description: 'S3 bucket to upload snapshot to',
       default: 'ironfish-snapshots',
     }),
-    accessKeyId: Flags.string({
-      char: 'a',
-      parse: (input: string) => Promise.resolve(input.trim()),
-      required: false,
-      description: 'S3 access key ID',
-    }),
-    secretAccessKey: Flags.string({
-      char: 's',
-      parse: (input: string) => Promise.resolve(input.trim()),
-      required: false,
-      description: 'S3 secret access key',
-    }),
-    region: Flags.string({
-      char: 'r',
-      parse: (input: string) => Promise.resolve(input.trim()),
-      required: false,
-      description: 'AWS region where bucket is contained',
-    }),
     path: Flags.string({
       char: 'p',
       parse: (input: string): Promise<string> => Promise.resolve(input.trim()),
       required: false,
-      description: 'The path where the snapshot should be saved',
+      description: 'The local path where the snapshot will be saved',
     }),
   }
 
@@ -62,13 +46,6 @@ export default class CreateSnapshot extends IronfishCommand {
     const { flags } = await this.parse(CreateSnapshot)
 
     const bucket = flags.bucket
-    const accessKeyId = (flags.accessKeyId || process.env.AWS_ACCESS_KEY_ID || '').trim()
-    const secretAccessKey = (
-      flags.secretAccessKey ||
-      process.env.AWS_SECRET_ACCESS_KEY ||
-      ''
-    ).trim()
-    const region = (flags.region || process.env.AWS_REGION || '').trim()
 
     let exportDir
 
@@ -111,13 +88,7 @@ export default class CreateSnapshot extends IronfishCommand {
       const snapshotBaseName = path.basename(SNAPSHOT_FILE_NAME, '.tar.gz')
       const snapshotKeyName = `${snapshotBaseName}_${timestamp}.tar.gz`
 
-      const s3 = new S3Client({
-        credentials: {
-          accessKeyId,
-          secretAccessKey,
-        },
-        region,
-      })
+      const s3 = new S3Client({})
 
       CliUx.ux.action.start(`Uploading to ${bucket}`)
       await S3Utils.uploadToBucket(
