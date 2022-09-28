7fdfc9b9b3c663fcf2b4d0baebbe27f755ef9cda
Jason Spafford <nullprogrammer@gmail.com>
2022-09-28 14:32:02 -0400

    Change to abstract into streamTransaction()
    Then we can short circuit when we know we're filtering on one
    transaction.

--- ironfish/src/rpc/routes/accounts/getTransactions.ts
+++ ironfish/src/rpc/routes/accounts/getTransactions.ts
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
+import { IronfishNode } from '../../../node'
+import { Account } from '../../../wallet/account'
+import { TransactionValue } from '../../../wallet/database/transactionValue'
+import { RpcRequest } from '../../request'
 import { ApiNamespace, router } from '../router'
 import { serializeRpcAccountTransaction } from './types'
 import { getAccount, getTransactionStatus } from './utils'
@@ -54,44 +58,60 @@ router.register<typeof GetAccountTransactionsRequestSchema, GetAccountTransactio
   GetAccountTransactionsRequestSchema,
   async (request, node): Promise<void> => {
     const account = getAccount(node, request.data.account)
-    const hash = request.data.hash ? Buffer.from(request.data.hash, 'hex') : null
+
+    if (request.data.hash) {
+      const hash = Buffer.from(request.data.hash, 'hex')
+      const transaction = await account.getTransaction(hash)
+
+      if (transaction) {
+        await streamTransaction(request, node, account, transaction)
+      }
+
+      request.end()
+      return
+    }
 
     for await (const transaction of account.getTransactions()) {
-      if (hash && !hash.equals(transaction.transaction.hash())) {
-        continue
-      }
-
-      const serializedTransaction = serializeRpcAccountTransaction(transaction)
-
-      let creator = false
-      for (const spend of transaction.transaction.spends()) {
-        const noteHash = await account.getNoteHash(spend.nullifier)
-
-        if (noteHash) {
-          creator = true
-          break
-        }
-      }
-
-      const status = await getTransactionStatus(
-        node,
-        transaction.blockHash,
-        transaction.sequence,
-        transaction.transaction.expirationSequence(),
-      )
-
-      const serialized = {
-        ...serializedTransaction,
-        creator,
-        status,
-      }
-
-      request.stream({
-        account: account.name,
-        transaction: serialized,
-      })
+      await streamTransaction(request, node, account, transaction)
     }
 
     request.end()
   },
 )
+
+const streamTransaction = async (
+  request: RpcRequest<GetAccountTransactionsRequest, GetAccountTransactionsResponse>,
+  node: IronfishNode,
+  account: Account,
+  transaction: TransactionValue,
+): Promise<void> => {
+  const serializedTransaction = serializeRpcAccountTransaction(transaction)
+
+  let creator = false
+  for (const spend of transaction.transaction.spends()) {
+    const noteHash = await account.getNoteHash(spend.nullifier)
+
+    if (noteHash) {
+      creator = true
+      break
+    }
+  }
+
+  const status = await getTransactionStatus(
+    node,
+    transaction.blockHash,
+    transaction.sequence,
+    transaction.transaction.expirationSequence(),
+  )
+
+  const serialized = {
+    ...serializedTransaction,
+    creator,
+    status,
+  }
+
+  request.stream({
+    account: account.name,
+    transaction: serialized,
+  })
+}
