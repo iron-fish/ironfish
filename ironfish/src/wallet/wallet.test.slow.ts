74eb4293363881191f2cad74004e44f8de9f81e2
Hugh Cunningham <hugh@ironfish.network>
2022-10-04 12:23:19 -0700

    removes unused import to fix lint

--- ironfish/src/wallet/wallet.test.slow.ts
+++ ironfish/src/wallet/wallet.test.slow.ts
@@ -3,7 +3,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { generateKey } from '@ironfish/rust-nodejs'
 import { Target } from '../primitives/target'
-import { ValidationError } from '../rpc/adapters/errors'
 import {
   createNodeTest,
   useAccountFixture,
