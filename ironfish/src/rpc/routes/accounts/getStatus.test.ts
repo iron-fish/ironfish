cc0d38bbbae45f0e36ffdcb433cde008ecbff610
Joseph Parks <jowparks@gmail.com>
2022-10-17 15:37:51 -0700

    feat: add missing files and test

---
+++ ironfish/src/rpc/routes/accounts/getStatus.test.ts
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import * as native from '@ironfish/rust-nodejs'
+import { v4 as uuid } from 'uuid'
+import { createRouteTest } from '../../../testUtilities/routeTest'
+import { Account } from '../../../wallet'
+
+describe('Route account/getPublicKey', () => {
+  const routeTest = createRouteTest(true)
+  let account = {} as Account
+
+  beforeAll(async () => {
+    account = await routeTest.node.wallet.createAccount(uuid())
+    await routeTest.node.wallet.setDefaultAccount(account.name)
+  })
+
+  it('should return account status information', async () => {
+    const response = await routeTest.client
+      .request<any>('account/getAccountsStatus', {})
+      .waitForEnd()
+
+    expect(response.status).toBe(200)
+    expect(response.content).toMatchObject({
+      account: account.name,
+      id: account.id, 
+    //   headHash: headHash ? headHash.toString('hex') : 'NULL', 
+    //   headInChain: headInChain,
+    //   sequence: headSequence,
+    })
+  })
+
+})
