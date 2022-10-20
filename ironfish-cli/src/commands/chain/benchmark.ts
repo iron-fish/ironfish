79f6e94b0459d77bdc89fcdd6fd642f8a7850311
danield9tqh <danield9tqh@gmail.com>
2022-10-20 10:43:44 -0700

    update benchmark to take a target directory and build on top

--- ironfish-cli/src/commands/chain/benchmark.ts
+++ ironfish-cli/src/commands/chain/benchmark.ts
@@ -3,6 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { BenchUtils, IronfishSdk, NodeUtils, TimeUtils } from '@ironfish/sdk'
 import { CliUx, Flags } from '@oclif/core'
+import blessed from 'blessed'
 import fs from 'fs/promises'
 import path from 'path'
 import { IronfishCommand } from '../../command'
@@ -19,7 +20,8 @@ export default class Benchmark extends IronfishCommand {
 
   static flags = {
     ...LocalFlags,
-    tempdir: Flags.string({
+    targetdir: Flags.string({
+      char: 't',
       parse: (input: string) => Promise.resolve(input.trim()),
       required: false,
       description: 'Path to the temporary directory to use to test',
@@ -43,19 +45,23 @@ export default class Benchmark extends IronfishCommand {
     await NodeUtils.waitForOpen(node)
     CliUx.ux.action.stop('done.')
 
-    if (!flags.tempdir) {
+    let targetDirectory
+    if (!flags.targetdir) {
       await fs.mkdir(node.config.tempDir, { recursive: true })
-      flags.tempdir = node.config.tempDir
+      targetDirectory = path.join(node.config.tempDir)
+    } else {
+      targetDirectory = path.join(flags.targetdir)
     }
 
-    const tempDataDir = await fs.mkdtemp(path.join(flags.tempdir, 'benchmark-'))
+    CliUx.ux.action.start(`Opening node in ${targetDirectory}`)
 
-    CliUx.ux.action.start(`Opening temp node in ${tempDataDir}`)
+    const noLoggingConfig = Object.assign({}, this.sdk.config.overrides)
+    noLoggingConfig.logLevel = '*:error'
     const tmpSdk = await IronfishSdk.init({
       pkg: IronfishCliPKG,
-      configOverrides: this.sdk.config.overrides,
+      configOverrides: noLoggingConfig,
       configName: undefined,
-      dataDir: tempDataDir,
+      dataDir: targetDirectory,
       logger: this.logger,
     })
     const tempNode = await tmpSdk.node()
@@ -63,14 +69,34 @@ export default class Benchmark extends IronfishCommand {
     tempNode.workerPool.start()
     CliUx.ux.action.stop('done.')
 
-    const header = await node.chain.getHeaderAtSequence(blocks)
-    if (header === null) {
+    const startingSequence = tempNode.chain.head.sequence
+    const startingHeader = await node.chain.getHeaderAtSequence(startingSequence)
+
+    const endingSequence = startingSequence + blocks
+    const endingHeader = await node.chain.getHeaderAtSequence(endingSequence)
+
+    if (startingHeader === null) {
+      throw new Error(`Target chain is longer than source chain`)
+    }
+
+    if (endingHeader === null) {
       throw new Error(`Chain must have at least ${blocks} blocks`)
     }
 
-    let totalMs = 0
+    if (!tempNode.chain.head.hash.equals(startingHeader?.hash)) {
+      throw new Error(`The two chains do not match at sequence ${startingSequence}`)
+    }
 
-    for await (const currentHeader of node.chain.iterateTo(node.chain.genesis, header)) {
+    let totalMs = 0
+    let totalSpends = 0
+    let totalNotes = 0
+    let totalTransactions = 0
+
+    const screen = blessed.screen({ smartCSR: true, fullUnicode: true })
+    const statusText = blessed.text()
+    screen.append(statusText)
+
+    for await (const currentHeader of node.chain.iterateTo(startingHeader, endingHeader)) {
       const block = await node.chain.getBlock(currentHeader)
       if (block === null) {
         throw new Error('Should have block if we have header')
@@ -78,19 +104,44 @@ export default class Benchmark extends IronfishCommand {
       const startTime = BenchUtils.start()
       await tempNode.chain.addBlock(block)
       totalMs += BenchUtils.end(startTime)
+      totalSpends += block.transactions.reduce((count, tx) => {
+        return count + [...tx.spends()].length
+      }, 0)
+      totalNotes += block.transactions.reduce((count, tx) => {
+        return count + [...tx.notes()].length
+      }, 0)
+      totalTransactions += block.transactions.length
+
+      if (block.header.sequence % 5 === 0) {
+        const status = [
+          `Block: ${block.header.sequence.toString()}`,
+          `Blocks/sec: ${blocks / (totalMs / 1000)} `,
+          `Transactions/sec ${totalTransactions / (totalMs / 1000)} `,
+          `Spends/sec: ${totalSpends / (totalMs / 1000)} `,
+          `Notes/sec: ${totalNotes / (totalMs / 1000)} `,
+        ].join('\n')
+
+        statusText.setContent(status)
+        screen.render()
+      }
     }
 
+    screen.destroy()
+
     this.log(`Total time to import ${blocks} blocks: ${TimeUtils.renderSpan(totalMs)}`)
+    this.log(`Average ${blocks / (totalMs / 1000)} blocks/sec`)
+    this.log(`Average ${totalTransactions / (totalMs / 1000)} transactions/sec`)
+    this.log(`Average ${totalSpends / (totalMs / 1000)} spends/sec`)
 
     // Check that data is consistent
-    const nodeNotesHash = await node.chain.notes.pastRoot(header.noteCommitment.size)
+    const nodeNotesHash = await node.chain.notes.pastRoot(endingHeader.noteCommitment.size)
     const tempNodeNotesHash = await tempNode.chain.notes.rootHash()
     if (!nodeNotesHash.equals(tempNodeNotesHash)) {
       throw new Error('/!\\ Note tree hashes were not consistent /!\\')
     }
 
     const nodeNullifiersHash = await node.chain.nullifiers.pastRoot(
-      header.nullifierCommitment.size,
+      endingHeader.nullifierCommitment.size,
     )
     const tempNodeNullifiersHash = await tempNode.chain.nullifiers.rootHash()
     if (!nodeNullifiersHash.equals(tempNodeNullifiersHash)) {
@@ -99,6 +150,8 @@ export default class Benchmark extends IronfishCommand {
 
     // Clean up the temporary node
     await tempNode.shutdown()
-    await fs.rm(tempDataDir, { recursive: true })
+    if (!flags.targetdir) {
+      await fs.rm(targetDirectory, { recursive: true })
+    }
   }
 }
