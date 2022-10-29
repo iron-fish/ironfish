7d20fe0bb7289fbb1f2e23bd98545bf3d18c0602
AmberKiso <96642073+AmberKiso@users.noreply.github.com>
2022-10-29 00:40:31 -0600

    testcase 1

--- ironfish/src/rpc/routes/chain/getBlockInfo.test.ts
+++ ironfish/src/rpc/routes/chain/getBlockInfo.test.ts
@@ -4,85 +4,39 @@
 import { Assert } from '../../../assert'
 import { makeBlockAfter } from '../../../testUtilities/helpers/blockchain'
 import { createRouteTest } from '../../../testUtilities/routeTest'
-import { ERROR_CODES } from '../../adapters'
+import { GetBlockInfoResponse } from './getBlockInfo'
 
-describe('Route chain/getBlockInfo',  () => {
+describe('Route chain/getBlockInfo', () => {
   const routeTest = createRouteTest()
-    
-    it('Processes hash input', async () => {
+
+  it('Processes hash input', async () => {
     // Create a 3 block chain
-      const { chain, strategy } = routeTest
-      await chain.open()
-      strategy.disableMiningReward()
+    const { chain, strategy } = routeTest
+    await chain.open()
+    strategy.disableMiningReward()
 
-      const genesis = await chain.getBlock(chain.genesis)
-      Assert.isNotNull(genesis)
+    const genesis = await chain.getBlock(chain.genesis)
+    Assert.isNotNull(genesis)
 
-      const blockA1 = await makeBlockAfter(chain, genesis)
-      await expect(chain).toAddBlock(blockA1)
+    const blockA1 = await makeBlockAfter(chain, genesis)
+    await expect(chain).toAddBlock(blockA1)
 
-      const blockA2 = await makeBlockAfter(chain, blockA1)
-      await expect(chain).toAddBlock(blockA2)
+    const blockA2 = await makeBlockAfter(chain, blockA1)
+    await expect(chain).toAddBlock(blockA2)
 
-      //Get hash of blocks
-      const hash0 = genesis.header.hash.toString('hex') // 69e
-      const hash1 = blockA1.header.hash.toString('hex') // 589
-      const hash2 = blockA2.header.hash.toString('hex') // cd9
+    //Get hash of blocks
+    const hash0 = genesis.header.hash.toString('hex') // 69e
+    const hash1 = blockA1.header.hash.toString('hex') // 589
+    const hash2 = blockA2.header.hash.toString('hex') // cd9
 
-      //Find block matching hash
+    //Find block matching hash
+    const response = await routeTest.client
+      .request<GetBlockInfoResponse>('chain/getBlockInfo', { search: hash0 })
+      .waitForEnd()
 
-      const response = await routeTest.client
-        .request('chain/getBlockInfo', { hash0 })
-        .waitForRoute()
+    expect(response.status).toBe(200)
+    expect(response.content.block.hash).toEqual(hash0)
+  })
 
-
-
-        let value = await response.contentStream().next()
-        expect.objectContaining({
-            message: expect.stringContaining(
-              'No block with header',
-            ),
-            status: 400,
-            code: ERROR_CODES.VALIDATION,
-          })
-        /*
-      //Receive and discard the first response (from getBlockRange)
-      let value = await response.contentStream().next()
-      //expect(response.status).toBe(200) //code = ERROR_CODES.VALIDATION
-
-      expect.objectContaining({ 
-        message: expect.stringContaining(
-          'Your balance is too low. Add funds to your account first',
-        ),
-        status: 400,
-        code: ERROR_CODES.VALIDATION,
-      })
-
-
-
-
-
-      // Receive and discard the genesis block
-      value = await response.contentStream().next()
-      expect(response.status).toBe(200)
-
-      expect(value).toMatchObject({
-        value: {
-          block: {
-            head: false,
-            hash: hash0,
-            seq: 0,
-          },
-        },
-      })
-
-
-
-      //Don't find block with non-matching hash
-*/
-
-    })
-    
-    //it('Processes sequence input', async () => {
-  
+  //it('Processes sequence input', async () => {
 })
