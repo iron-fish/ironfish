575937d2c345318148b1467e8139711a858becbd
Mat <mat@ironfish.network>
2022-11-15 15:10:33 -0700

    refactor(rust,zkp): Add value commitment to mint circuit, sign mint descriptions

--- ironfish-zkp/src/circuits/mint_asset.rs
+++ ironfish-zkp/src/circuits/mint_asset.rs
@@ -2,10 +2,11 @@ use bellman::{
     gadgets::{boolean, multipack},
     Circuit,
 };
+use zcash_primitives::sapling::ValueCommitment;
 use zcash_proofs::circuit::{ecc, pedersen_hash};
 
 use crate::{
-    circuits::util::asset_info_preimage,
+    circuits::util::{asset_info_preimage, expose_randomized_public_key, expose_value_commitment},
     constants::{proof::ASSET_KEY_GENERATOR, ASSET_IDENTIFIER_PERSONALIZATION},
 };
 
@@ -23,6 +24,10 @@ pub struct MintAsset {
     /// Private keys associated with the public key used to create the
     /// identifier
     pub asset_authorization_key: Option<jubjub::Fr>,
+
+    pub value_commitment: Option<ValueCommitment>,
+
+    pub public_key_randomness: Option<jubjub::Fr>,
 }
 
 impl Circuit<bls12_381::Scalar> for MintAsset {
@@ -47,10 +52,10 @@ impl Circuit<bls12_381::Scalar> for MintAsset {
         // Create the Asset Info pre-image
         let identifier_preimage = asset_info_preimage(
             &mut cs.namespace(|| "asset info preimage"),
-            self.name,
-            self.metadata,
-            asset_public_key,
-            self.nonce,
+            &self.name,
+            &self.metadata,
+            &asset_public_key,
+            &self.nonce,
         )?;
 
         // Computed identifier bits from the given asset info
@@ -68,6 +73,16 @@ impl Circuit<bls12_381::Scalar> for MintAsset {
 
         multipack::pack_into_inputs(cs.namespace(|| "pack identifier"), &asset_identifier)?;
 
+        // Witness and expose the value commitment
+        expose_value_commitment(cs.namespace(|| "value commitment"), self.value_commitment)?;
+
+        // Witness and expose the randomized public key
+        expose_randomized_public_key(
+            cs.namespace(|| "randomized public key"),
+            self.public_key_randomness,
+            &asset_public_key,
+        )?;
+
         Ok(())
     }
 }
@@ -81,9 +96,10 @@ mod test {
         Circuit,
     };
     use ff::Field;
-    use group::GroupEncoding;
+    use group::{Curve, GroupEncoding};
+    use jubjub::ExtendedPoint;
     use rand::{rngs::StdRng, SeedableRng};
-    use zcash_primitives::sapling::pedersen_hash;
+    use zcash_primitives::sapling::{pedersen_hash, redjubjub, ValueCommitment};
 
     use crate::constants::{ASSET_IDENTIFIER_PERSONALIZATION, ASSET_KEY_GENERATOR};
 
@@ -118,7 +134,28 @@ mod test {
         let identifier = identifier_point.to_bytes();
 
         let identifier_bits = multipack::bytes_to_bits_le(&identifier);
-        let public_inputs = multipack::compute_multipacking(&identifier_bits);
+        let identifier_inputs = multipack::compute_multipacking(&identifier_bits);
+
+        let value_commitment = ValueCommitment {
+            value: 5,
+            randomness: jubjub::Fr::random(&mut rng),
+        };
+
+        let value_commitment_point = ExtendedPoint::from(value_commitment.commitment()).to_affine();
+
+        let public_key_randomness = jubjub::Fr::random(&mut rng);
+        let randomized_public_key = redjubjub::PublicKey(asset_public_key.into())
+            .randomize(public_key_randomness, ASSET_KEY_GENERATOR);
+        let randomized_public_key_point = randomized_public_key.0.to_affine();
+
+        let public_inputs = vec![
+            identifier_inputs[0],
+            identifier_inputs[1],
+            value_commitment_point.get_u(),
+            value_commitment_point.get_v(),
+            randomized_public_key_point.get_u(),
+            randomized_public_key_point.get_v(),
+        ];
 
         // Mint proof
         let circuit = MintAsset {
@@ -126,11 +163,13 @@ mod test {
             metadata,
             nonce,
             asset_authorization_key: Some(asset_auth_key),
+            value_commitment: Some(value_commitment),
+            public_key_randomness: Some(public_key_randomness),
         };
         circuit.synthesize(&mut cs).unwrap();
 
         assert!(cs.is_satisfied());
         assert!(cs.verify(&public_inputs));
-        assert_eq!(cs.num_constraints(), 5356);
+        assert_eq!(cs.num_constraints(), 7631);
     }
 }
