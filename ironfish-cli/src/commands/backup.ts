fdf298b3357568259ba777c6c72ca21a96053295
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2022-12-01 12:48:37 -0800

    fixes backup command multipart uploads (#2633)
    * fixes backup command multipart uploads
    
    the backup command uses an S3 multipart upload to copy a zip of the chain and/or
    accounts databases to an S3 bucket.
    
    this command fails if the AWS credentials aren't set properly in the S3Client.
    if there are no credentials set, we need not pass a 'credentials' object to the
    constructor.
    
    makes accessKeyId and secretAccessKey flags interdependent. makes both flags
    automatically read from env variables if not passed in.
    
    fixes parsing of credentials to pass no credentials to the S3Client constructor
    if no credentials are passed to the command or set in the environment.
    
    this won't allow uploads without access keys, however, as S3Client requires some
    credentials. if none are passed in it will read them from AWS credentials
    configured on the machine.
    
    * uses aws cognito to allow unauthenticated uploads
    
    if aws credentials are not set or passed in aws aws cognito to create an
            identity and fetch credentials for it.
    
    created a cognito identity pool in aws and use the id of that pool here for
    creating identities.
    
    adds @aws-sdk/client-cognito-identity to dependencies
    
    * adds default bucket to backup command
    
    * Revert "adds default bucket to backup command"
    
    This reverts commit 2d1249ceb5285c7f1fbdda8161fc650c5bc838c6.

--- ironfish-cli/src/commands/backup.ts
+++ ironfish-cli/src/commands/backup.ts
@@ -1,8 +1,10 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { CognitoIdentity } from '@aws-sdk/client-cognito-identity'
 import { S3Client } from '@aws-sdk/client-s3'
-import { FileUtils, NodeUtils } from '@ironfish/sdk'
+import { Credentials } from '@aws-sdk/types/dist-types/credentials'
+import { Assert, FileUtils, NodeUtils } from '@ironfish/sdk'
 import { CliUx, Flags } from '@oclif/core'
 import fsAsync from 'fs/promises'
 import os from 'os'
@@ -44,12 +46,16 @@ export default class Backup extends IronfishCommand {
       parse: (input: string) => Promise.resolve(input.trim()),
       required: false,
       description: 'S3 access key ID',
+      env: 'AWS_ACCESS_KEY_ID',
+      dependsOn: ['secretAccessKey'],
     }),
     secretAccessKey: Flags.string({
       char: 's',
       parse: (input: string) => Promise.resolve(input.trim()),
       required: false,
       description: 'S3 secret access key',
+      env: 'AWS_SECRET_ACCESS_KEY',
+      dependsOn: ['accessKeyId'],
     }),
   }
 
@@ -65,13 +71,8 @@ export default class Backup extends IronfishCommand {
     const { flags, args } = await this.parse(Backup)
     const bucket = (args.bucket as string).trim()
 
-    const accessKeyId = (flags.accessKeyId || process.env.AWS_ACCESS_KEY_ID || '').trim()
-    const secretAccessKey = (
-      flags.secretAccessKey ||
-      process.env.AWS_SECRET_ACCESS_KEY ||
-      ''
-    ).trim()
-    const region = 'us-east-1'
+    const accessKeyId = flags.accessKeyId
+    const secretAccessKey = flags.secretAccessKey
 
     let id = uuid().slice(0, 5)
     const name = this.sdk.config.get('nodeName')
@@ -113,13 +114,7 @@ export default class Backup extends IronfishCommand {
 
     CliUx.ux.action.start(`Uploading to ${bucket}`)
 
-    const s3 = new S3Client({
-      credentials: {
-        accessKeyId,
-        secretAccessKey,
-      },
-      region,
-    })
+    const s3 = await this.getS3Client(accessKeyId, secretAccessKey)
 
     await S3Utils.uploadToBucket(
       s3,
@@ -134,5 +129,54 @@ export default class Backup extends IronfishCommand {
     CliUx.ux.action.start(`Removing backup dir ${destDir}`)
     await fsAsync.rm(destDir, { recursive: true })
     CliUx.ux.action.stop(`done`)
+  }
+
+  private async getS3Client(accessKeyId?: string, secretAccessKey?: string): Promise<S3Client> {
+    const region = 'us-east-1'
+
+    if (accessKeyId && secretAccessKey) {
+      return new S3Client({
+        credentials: {
+          accessKeyId,
+          secretAccessKey,
+        },
+        region,
+      })
+    }
+
+    const credentials = await this.getCognitoIdentityCredentials()
+
+    return new S3Client({
+      credentials,
+      region,
+    })
+  }
+
+  private async getCognitoIdentityCredentials(): Promise<Credentials> {
+    const identityPoolId = 'us-east-1:3ebc542a-6ac4-4c5d-9558-1621eadd2382'
+
+    const cognito = new CognitoIdentity({})
+
+    const identityResponse = await cognito.getId({ IdentityPoolId: identityPoolId })
+
+    const identityId = identityResponse.IdentityId
+
+    const credentialsResponse = await cognito.getCredentialsForIdentity({
+      IdentityId: identityId,
+    })
+
+    const cognitoAccessKeyId = credentialsResponse.Credentials?.AccessKeyId
+    const cognitoSecretAccessKey = credentialsResponse.Credentials?.SecretKey
+    const cognitoSessionToken = credentialsResponse.Credentials?.SessionToken
+
+    Assert.isNotUndefined(cognitoAccessKeyId)
+    Assert.isNotUndefined(cognitoSecretAccessKey)
+    Assert.isNotUndefined(cognitoSessionToken)
+
+    return {
+      accessKeyId: cognitoAccessKeyId,
+      secretAccessKey: cognitoSecretAccessKey,
+      sessionToken: cognitoSessionToken,
+    }
   }
 }
