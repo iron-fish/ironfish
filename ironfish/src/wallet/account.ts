4e2ee1e0e5ed20064b01fb17d005b9259ef6cc89
Hugh Cunningham <hugh@ironfish.network>
2022-12-16 16:24:32 -0800

    fixes lint issue

--- ironfish/src/wallet/account.ts
+++ ironfish/src/wallet/account.ts
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Asset } from '@ironfish/rust-nodejs'
 import { BufferMap } from 'buffer-map'
 import MurmurHash3 from 'imurmurhash'
 import { Assert } from '../assert'
