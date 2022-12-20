059532070fe68036ae430651a85872ee106efcb6
Jason Spafford <nullprogrammer@gmail.com>
2022-12-20 01:02:26 -0500

    Add tests for creating transactions (#2777)
    This adds test for posting a raw tranaction, and the raw transaction
    itself.

--- ironfish/src/primitives/rawTransaction.test.ts
+++ ironfish/src/primitives/rawTransaction.test.ts
@@ -2,17 +2,87 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Asset, generateKey, Note as NativeNote } from '@ironfish/rust-nodejs'
+import { BufferMap } from 'buffer-map'
+import { Assert } from '../assert'
 import { Witness } from '../merkletree'
 import { NoteHasher } from '../merkletree/hasher'
 import { Side } from '../merkletree/merkletree'
 import { IsNoteWitnessEqual } from '../merkletree/witness'
-import { useAccountFixture } from '../testUtilities/fixtures'
+import {
+  useAccountFixture,
+  useMinerBlockFixture,
+  useRawTxFixture,
+} from '../testUtilities/fixtures'
 import { createNodeTest } from '../testUtilities/nodeTest'
 import { Note } from './note'
 import { RawTransaction, RawTransactionSerde } from './rawTransaction'
 
 describe('RawTransaction', () => {
   const nodeTest = createNodeTest()
+
+  it('should post', async () => {
+    const account = await useAccountFixture(nodeTest.wallet)
+    const asset = new Asset(account.spendingKey, 'test', '')
+
+    const block = await useMinerBlockFixture(
+      nodeTest.chain,
+      undefined,
+      account,
+      nodeTest.wallet,
+    )
+    await expect(nodeTest.chain).toAddBlock(block)
+    await nodeTest.wallet.updateHead()
+    const balance = await account.getUnconfirmedBalance(Asset.nativeIdentifier())
+
+    const burn = {
+      assetIdentifier: Asset.nativeIdentifier(),
+      value: 2n,
+    }
+
+    const mint = {
+      asset,
+      value: 1n,
+    }
+
+    const raw = await useRawTxFixture({
+      wallet: nodeTest.wallet,
+      from: account,
+      to: account,
+      amount: 1n,
+      fee: 5n,
+      expiration: 10,
+      burns: [burn],
+      mints: [mint],
+    })
+
+    const posted = raw.post()
+    expect(posted.takeReference().verify()).toBe(true)
+    expect(posted.fee()).toEqual(5n)
+    expect(posted.expirationSequence()).toEqual(10)
+    expect(posted.notes.length).toEqual(3)
+    expect(posted.spends.length).toEqual(1)
+    expect(posted.mints.length).toEqual(1)
+    expect(posted.burns.length).toEqual(1)
+
+    const valuesByAsset = new BufferMap<bigint>()
+
+    for (const note of posted.notes) {
+      const decryptedNote = note.decryptNoteForOwner(account.incomingViewKey)
+      Assert.isNotUndefined(decryptedNote)
+
+      const identifier = decryptedNote.assetIdentifier()
+      const value = valuesByAsset.get(identifier) || 0n
+      valuesByAsset.set(identifier, value + decryptedNote.value())
+    }
+
+    const nativeValue = valuesByAsset.get(Asset.nativeIdentifier())
+    Assert.isNotUndefined(nativeValue)
+    expect(nativeValue).toEqual(balance - raw.fee - mint.value - 1n)
+
+    const mintedValue = valuesByAsset.get(asset.identifier())
+    Assert.isNotUndefined(mintedValue)
+    expect(mintedValue).toEqual(1n)
+  })
 
   describe('RawTransactionSerde', () => {
     it('serializes and deserializes a block', async () => {
