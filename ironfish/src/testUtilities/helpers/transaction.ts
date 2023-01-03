479baab28aae2f6d513349c43d11762fc8689e19
Jason Spafford <nullprogrammer@gmail.com>
2023-01-03 12:07:27 -0500

    Remove mintAsset test function

--- ironfish/src/testUtilities/helpers/transaction.ts
+++ ironfish/src/testUtilities/helpers/transaction.ts
@@ -3,15 +3,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import { Asset } from '@ironfish/rust-nodejs'
-import { Assert } from '../../assert'
-import { Blockchain } from '../../blockchain'
 import { BurnDescription } from '../../primitives/burnDescription'
 import { MintDescription } from '../../primitives/mintDescription'
-import { NoteEncrypted } from '../../primitives/noteEncrypted'
 import { RawTransaction } from '../../primitives/rawTransaction'
 import { Transaction } from '../../primitives/transaction'
 import { Account, Wallet } from '../../wallet'
-import { WorkerPool } from '../../workerPool'
 
 export function isTransactionMine(transaction: Transaction, account: Account): boolean {
   for (const note of transaction.notes) {
@@ -27,44 +23,6 @@ export function isTransactionMine(transaction: Transaction, account: Account): b
   }
 
   return false
-}
-
-export async function buildRawTransaction(
-  chain: Blockchain,
-  pool: WorkerPool,
-  sender: Account,
-  notesToSpend: NoteEncrypted[],
-  receives: { publicAddress: string; amount: bigint; memo: string; assetIdentifier: Buffer }[],
-  mints: MintDescription[],
-  burns: BurnDescription[],
-): Promise<Transaction> {
-  const spends = await Promise.all(
-    notesToSpend.map(async (n) => {
-      const note = n.decryptNoteForOwner(sender.incomingViewKey)
-      Assert.isNotUndefined(note)
-      const treeIndex = await chain.notes.leavesIndex.get(n.merkleHash())
-      Assert.isNotUndefined(treeIndex)
-      const witness = await chain.notes.witness(treeIndex)
-      Assert.isNotNull(witness)
-
-      return {
-        note,
-        treeSize: witness.treeSize(),
-        authPath: witness.authenticationPath,
-        rootHash: witness.rootHash,
-      }
-    }),
-  )
-
-  return pool.createTransaction(
-    sender.spendingKey,
-    spends,
-    receives,
-    mints,
-    burns,
-    BigInt(0),
-    0,
-  )
 }
 
 export async function createRawTransaction(options: {
