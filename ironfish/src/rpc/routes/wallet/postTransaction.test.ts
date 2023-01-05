e0dd7dff72b0433bde6557a8cf6b166bdbba58c9
Evan Richard <evan@ironfish.network>
2023-01-05 16:51:45 -0500

    Init branch
    add postTransaction.ts and corresponding test file, for now they are copies of getStatus

---
+++ ironfish/src/rpc/routes/wallet/postTransaction.test.ts
@@ -0,0 +1,33 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { v4 as uuid } from 'uuid'
+import { createRouteTest } from '../../../testUtilities/routeTest'
+
+describe('Route wallet/status', () => {
+  const routeTest = createRouteTest(true)
+
+  it('should return account status information', async () => {
+    const account = await routeTest.node.wallet.createAccount(uuid(), true)
+    const response = await routeTest.client
+      .request<any>('wallet/getAccountsStatus', {})
+      .waitForEnd()
+
+    expect(response.status).toBe(200)
+    expect(response.content).toMatchObject({
+      accounts: [
+        {
+          name: account.name,
+          id: account.id,
+          headHash: 'NULL',
+          headInChain: false,
+          sequence: 'NULL',
+        },
+      ],
+    })
+  })
+})
