ace7d9b5f5c59d57e30eadec33bb85a325d7ac63
Evan Richard <evan@ironfish.network>
2023-01-05 16:58:56 -0500

    Update postTransaction.ts
    Add first draft of implementation. No error handling is yet present.

--- ironfish/src/rpc/routes/wallet/postTransaction.ts
+++ ironfish/src/rpc/routes/wallet/postTransaction.ts
@@ -2,65 +2,37 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
+import { RawTransactionSerde } from '../../../primitives/rawTransaction'
 import { ApiNamespace, router } from '../router'
 
-export type GetAccountStatusRequest = { account?: string }
+export type PostTransactionRequest = { transaction: string }
 
-export type GetAccountStatusResponse = {
-  accounts: Array<{
-    name: string
-    id: string
-    headHash: string
-    headInChain: boolean
-    sequence: string | number
-  }>
+export type PostTransactionResponse = {
+  transaction: string
 }
 
-export const GetAccountStatusRequestSchema: yup.ObjectSchema<GetAccountStatusRequest> = yup
-  .object({})
-  .defined()
-
-export const GetAccountStatusResponseSchema: yup.ObjectSchema<GetAccountStatusResponse> = yup
+export const PostTransactionRequestSchema: yup.ObjectSchema<PostTransactionRequest> = yup
   .object({
-    accounts: yup
-      .array(
-        yup
-          .object({
-            name: yup.string().defined(),
-            id: yup.string().defined(),
-            headHash: yup.string().defined(),
-            headInChain: yup.boolean().defined(),
-            sequence: yup.string().defined(),
-          })
-          .defined(),
-      )
-      .defined(),
+    transaction: yup.string().defined(),
   })
   .defined()
 
-router.register<typeof GetAccountStatusRequestSchema, GetAccountStatusResponse>(
-  `${ApiNamespace.wallet}/getAccountsStatus`,
-  GetAccountStatusRequestSchema,
-  async (request, node): Promise<void> => {
-    const headHashes = new Map<string, Buffer | null>()
-    for await (const { accountId, headHash } of node.wallet.walletDb.loadHeadHashes()) {
-      headHashes.set(accountId, headHash)
-    }
-    const accountsInfo: GetAccountStatusResponse['accounts'] = []
-    for (const account of node.wallet.listAccounts()) {
-      const headHash = headHashes.get(account.id)
-      const blockHeader = headHash ? await node.chain.getHeader(headHash) : null
-      const headInChain = !!blockHeader
-      const headSequence = blockHeader?.sequence || 'NULL'
-      accountsInfo.push({
-        name: account.name,
-        id: account.id,
-        headHash: headHash ? headHash.toString('hex') : 'NULL',
-        headInChain: headInChain,
-        sequence: headSequence,
-      })
-    }
+export const PostTransactionResponseSchema: yup.ObjectSchema<PostTransactionResponse> = yup
+  .object({
+    transaction: yup.string().defined(),
+  })
+  .defined()
 
-    request.end({ accounts: accountsInfo })
+router.register<typeof PostTransactionRequestSchema, PostTransactionResponse>(
+  `${ApiNamespace.wallet}/postTransaction`,
+  PostTransactionRequestSchema,
+  async (request, node): Promise<void> => {
+
+    const rawTransactionBytes = Buffer.from(request.data.transaction, 'hex')
+    const rawTransaction = RawTransactionSerde.deserialize(rawTransactionBytes)
+    const postedTransaction = await node.wallet.postTransaction(rawTransaction)
+    const postedTransactionBytes = postedTransaction.serialize()
+
+    request.end({ transaction: postedTransactionBytes.toString('hex') })
   },
 )
