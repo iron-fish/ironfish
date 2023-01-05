9df4b8e3702ebaad3c5bd0eb21e1db94dcc686fb
Rohan Jadvani <rohan@hey.com>
2023-01-05 09:42:27 -0500

    feat(ironfish): Add RPC route for `wallet/burnAsset`

--- ironfish/src/rpc/routes/wallet/index.ts
+++ ironfish/src/rpc/routes/wallet/index.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+export * from './burnAsset'
 export * from './create'
 export * from './exportAccount'
 export * from './getAccounts'
@@ -12,9 +13,9 @@ export * from './getPublicKey'
 export * from './getStatus'
 export * from './getTransaction'
 export * from './getTransactions'
-export * from './sendTransaction'
 export * from './importAccount'
 export * from './removeAccount'
 export * from './rescanAccount'
+export * from './sendTransaction'
 export * from './sendTransaction'
 export * from './useAccount'
