2f532343720e5b7817b6da0bf0c8d62e66f2b159
jowparks <jowparks@gmail.com>
2023-01-06 12:35:11 -0800

    feat: wire up multi asset syncer to API (2/2) (#2795)
    * feat: wire up assets to masp syncer cli command
    
    * feat: fix typo
    
    chore: renaming to multi asset
    
    feat: add tests
    
    feat: cleanup logic more readable

--- ironfish/src/webApi.ts
+++ ironfish/src/webApi.ts
@@ -15,9 +15,10 @@ type FaucetTransaction = {
   completed_at: string | null
 }
 
-export type MaspTransactionTypes = 'MASP_TRANSFER' | 'MASP_BURN' | 'MASP_MINT'
+export const MaspTransactionTypes = ['MASP_TRANSFER', 'MASP_BURN', 'MASP_MINT']
+export type MultiAssetTypes = typeof MaspTransactionTypes[number]
 
-export type ApiMaspUpload = {
+export type ApiMultiAssetUpload = {
   type: 'connected' | 'disconnected' | 'fork'
   block: {
     hash: string
@@ -27,8 +28,10 @@ export type ApiMaspUpload = {
   }
   transactions: {
     hash: string
-    type: MaspTransactionTypes
-    assetName: string
+    multiAssets: {
+      type: MultiAssetTypes
+      assetName: string
+    }[]
   }[]
 }
 
@@ -78,7 +81,7 @@ export class WebApi {
   }
   async headMaspTransactions(): Promise<string | null> {
     const response = await axios
-      .get<{ block_hash: string }>(`${this.host}/masp/head`)
+      .get<{ block_hash: string }>(`${this.host}/multi_asset/head`)
       .catch((e) => {
         // The API returns 404 for no head
         if (IsAxiosError(e) && e.response?.status === 404) {
@@ -106,11 +109,11 @@ export class WebApi {
     return response?.data.hash || null
   }
 
-  async uploadMaspTransactions(maspTransactions: ApiMaspUpload[]): Promise<void> {
+  async uploadMaspTransactions(multiAssets: ApiMultiAssetUpload[]): Promise<void> {
     this.requireToken()
 
     const options = this.options({ 'Content-Type': 'application/json' })
-    await axios.post(`${this.host}/masp`, { operations: maspTransactions }, options)
+    await axios.post(`${this.host}/multi_asset`, { operations: multiAssets }, options)
   }
 
   async blocks(blocks: FollowChainStreamResponse[]): Promise<void> {
