ea718ebc0145f8aef1ecc2214782f8eeadbcbd9c
jowparks <jowparks@gmail.com>
2023-01-06 10:56:07 -0800

    feat: update get transaction stream (1/2) (#2835)
    * feat: wire up assets to masp syncer cli command
    
    * feat: fix typo
    
    chore: renaming to multi asset
    
    feat: add tests
    
    feat: cleanup logic more readable
    
    * feat: update get transaction stream to handle multi asset
    
    * feat: review updates
    
    * feat: renaming and asset value change
    
    * feat: rename back to assetId since PR was merged for renaming

---
+++ ironfish/src/rpc/routes/chain/getTransactionStream.test.ts
@@ -0,0 +1,95 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import '../../../testUtilities/matchers'
+import { Asset } from '@ironfish/rust-nodejs'
+import { BlockHashSerdeInstance } from '../../../serde'
+import {
+  useAccountFixture,
+  useBurnBlockFixture,
+  useMintBlockFixture,
+} from '../../../testUtilities/fixtures'
+import { createRouteTest } from '../../../testUtilities/routeTest'
+import { GetTransactionStreamResponse } from './getTransactionStream'
+
+describe('Route chain.getTransactionStream', () => {
+  const routeTest = createRouteTest()
+
+  it('should fail if no incoming view key is specified', async () => {
+    await expect(
+      routeTest.client.request('chain/getTransactionStream', {}).waitForEnd(),
+    ).rejects.toThrow('Request failed (400) validation: incomingViewKey is a required field')
+  })
+
+  it(`should fail if block can't be found with hash`, async () => {
+    const hash = BlockHashSerdeInstance.serialize(Buffer.alloc(32, 'blockhashnotfound'))
+    const wallet = routeTest.node.wallet
+    const account = await useAccountFixture(wallet)
+
+    await expect(
+      routeTest.client
+        .request('chain/getTransactionStream', {
+          incomingViewKey: account.incomingViewKey,
+          head: hash,
+        })
+        .waitForEnd(),
+    ).rejects.toThrow(
+      `Request failed (400) validation: Block with hash ${hash} was not found in the chain`,
+    )
+  })
+
+  it('returns expected mints and burns', async () => {
+    const wallet = routeTest.node.wallet
+    const account = await useAccountFixture(wallet)
+    const asset = new Asset(account.spendingKey, 'customasset', 'metadata')
+    const response = routeTest.client.request<GetTransactionStreamResponse>(
+      'chain/getTransactionStream',
+      { incomingViewKey: account.incomingViewKey },
+    )
+    await response.contentStream().next()
+    // Mint so we have an existing asset
+    const mintValue = BigInt(10)
+
+    const mintBlock = await useMintBlockFixture({
+      node: routeTest.node,
+      account,
+      asset,
+      value: mintValue,
+    })
+    await expect(routeTest.node.chain).toAddBlock(mintBlock)
+    // validate mint block
+    expect(await (await response.contentStream().next()).value).toEqual(
+      expect.objectContaining({
+        transactions: expect.arrayContaining([
+          expect.objectContaining({
+            mints: expect.arrayContaining([expect.objectContaining({ value: '10' })]),
+          }),
+        ]),
+      }),
+    )
+
+    // update wallet so it sees newly added asset
+    await wallet.updateHead()
+
+    // now burn
+    const burnBlock = await useBurnBlockFixture({
+      node: routeTest.node,
+      account,
+      asset,
+      value: mintValue,
+    })
+    await expect(routeTest.node.chain).toAddBlock(burnBlock)
+    // validate burn block
+    expect(await (await response.contentStream().next()).value).toEqual(
+      expect.objectContaining({
+        transactions: expect.arrayContaining([
+          expect.objectContaining({
+            burns: expect.arrayContaining([expect.objectContaining({ value: '10' })]),
+          }),
+        ]),
+      }),
+    )
+    response.end()
+  })
+})
