06bc8817cd3de322e83b9b095f20afb9dacd7770
Jason Spafford <nullprogrammer@gmail.com>
2023-01-06 14:27:23 -0500

    WIP (#2844)

---
+++ ironfish/src/testUtilities/fixtures/account.ts
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Account, AccountValue, Wallet } from '../../wallet'
+import { FixtureGenerate, useFixture } from './fixture'
+
+export function useAccountFixture(
+  wallet: Wallet,
+  generate: FixtureGenerate<Account> | string = 'test',
+): Promise<Account> {
+  if (typeof generate === 'string') {
+    const name = generate
+    generate = () => wallet.createAccount(name)
+  }
+
+  return useFixture(generate, {
+    serialize: (account: Account): AccountValue => {
+      return account.serialize()
+    },
+
+    deserialize: async (accountData: AccountValue): Promise<Account> => {
+      const account = await wallet.importAccount(accountData)
+      await wallet.updateHeadHash(account, wallet.chainProcessor.hash)
+      return account
+    },
+  })
+}
