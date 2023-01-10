a8d0a497d13dceb7d6acd71b9a0c434d817d71ab
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-01-10 16:22:23 -0500

    test(ironfish): Clean up fixtures

--- ironfish/src/rpc/routes/wallet/getBalances.test.ts
+++ ironfish/src/rpc/routes/wallet/getBalances.test.ts
@@ -2,13 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Asset } from '@ironfish/rust-nodejs'
-import { BufferMap } from 'buffer-map'
-import {
-  useAccountFixture,
-  useBurnBlockFixture,
-  useMinerBlockFixture,
-  useMintBlockFixture,
-} from '../../../testUtilities'
+import { useAccountFixture } from '../../../testUtilities'
 import { createRouteTest } from '../../../testUtilities/routeTest'
 
 describe('getBalances', () => {
@@ -31,75 +25,50 @@ describe('getBalances', () => {
       const node = routeTest.node
       const wallet = node.wallet
       const account = await useAccountFixture(wallet)
-
-      const mined = await useMinerBlockFixture(node.chain, 2, account)
-      await expect(node.chain).toAddBlock(mined)
-      await node.wallet.updateHead()
-
       const asset = new Asset(account.spendingKey, 'mint-asset', 'metadata')
       const assetId = asset.id()
-      const mintValue = BigInt(10)
-      const mintBlock = await useMintBlockFixture({
-        node,
-        account,
-        asset,
-        value: mintValue,
-        sequence: 3,
-      })
-      await expect(node.chain).toAddBlock(mintBlock)
-      await node.wallet.updateHead()
 
-      const burnValue = BigInt(2)
-      const burnBlock = await useBurnBlockFixture({
-        node: node,
-        account,
-        asset,
-        value: burnValue,
-        sequence: 4,
-      })
-      await expect(node.chain).toAddBlock(burnBlock)
-      await node.wallet.updateHead()
+      const mockBalances = [
+        {
+          assetId: Asset.nativeId(),
+          confirmed: BigInt(2000000000),
+          unconfirmed: BigInt(2000000000),
+          unconfirmedCount: 0,
+          blockHash: null,
+          sequence: null,
+        },
+        {
+          assetId,
+          confirmed: BigInt(8),
+          unconfirmed: BigInt(8),
+          unconfirmedCount: 0,
+          blockHash: null,
+          sequence: null,
+        },
+      ]
+
+      const getBalances = jest
+        .spyOn(wallet, 'getBalances')
+        .mockImplementationOnce(async function* () {
+          for (const balance of mockBalances) {
+            // eslint-disable-next-line @typescript-eslint/require-await
+            yield balance
+          }
+        })
 
       const response = await routeTest.client.getAccountBalances({
         account: account.name,
       })
 
-      const balances = new BufferMap<{
-        confirmed: bigint
-        unconfirmed: bigint
-        unconfirmedCount: number
-      }>()
-      for (const balance of response.content.balances) {
-        balances.set(Buffer.from(balance.assetId, 'hex'), {
-          confirmed: BigInt(balance.confirmed),
-          unconfirmed: BigInt(balance.unconfirmed),
-          unconfirmedCount: balance.unconfirmedCount,
-        })
-      }
-
-      const expectedBalances = new BufferMap<{
-        confirmed: bigint
-        unconfirmed: bigint
-        unconfirmedCount: number
-      }>([
-        [
-          Asset.nativeId(),
-          {
-            confirmed: BigInt(2000000000),
-            unconfirmed: BigInt(2000000000),
-            unconfirmedCount: 0,
-          },
-        ],
-        [
-          assetId,
-          {
-            confirmed: mintValue - burnValue,
-            unconfirmed: mintValue - burnValue,
-            unconfirmedCount: 0,
-          },
-        ],
-      ])
-      expect(balances).toEqual(expectedBalances)
+      expect(getBalances).toHaveBeenCalledTimes(1)
+      expect(response.content.balances).toEqual(
+        mockBalances.map((mockBalance) => ({
+          ...mockBalance,
+          assetId: mockBalance.assetId.toString('hex'),
+          confirmed: mockBalance.confirmed.toString(),
+          unconfirmed: mockBalance.unconfirmed.toString(),
+        })),
+      )
     })
   })
 })
