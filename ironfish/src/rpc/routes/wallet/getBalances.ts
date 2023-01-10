89222d22a99aa59d6ca08a0792b5628893cebc49
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-01-10 18:03:12 -0500

    feat(cli,ironfish): Add `wallet:balances` command

--- ironfish/src/rpc/routes/wallet/getBalances.ts
+++ ironfish/src/rpc/routes/wallet/getBalances.ts
@@ -2,15 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
-import { ValidationError } from '../../adapters'
 import { ApiNamespace, router } from '../router'
+import { getAccount } from './utils'
 
 export interface GetBalancesRequest {
-  account: string
+  account?: string
   minimumBlockConfirmations?: number
 }
 
 export interface GetBalancesResponse {
+  account: string
   balances: {
     assetId: string
     confirmed: string
@@ -23,13 +24,14 @@ export interface GetBalancesResponse {
 
 export const GetBalancesRequestSchema: yup.ObjectSchema<GetBalancesRequest> = yup
   .object({
-    account: yup.string().required(),
+    account: yup.string().optional(),
     minimumBlockConfirmations: yup.number().optional(),
   })
   .defined()
 
 export const GetBalancesResponseSchema: yup.ObjectSchema<GetBalancesResponse> = yup
   .object({
+    account: yup.string().defined(),
     balances: yup
       .array()
       .of(
@@ -53,10 +55,7 @@ router.register<typeof GetBalancesRequestSchema, GetBalancesResponse>(
   `${ApiNamespace.wallet}/getBalances`,
   GetBalancesRequestSchema,
   async (request, node): Promise<void> => {
-    const account = node.wallet.getAccountByName(request.data.account)
-    if (!account) {
-      throw new ValidationError(`No account found with name '${request.data.account}'`)
-    }
+    const account = getAccount(node, request.data.account)
 
     const balances = []
     for await (const {
@@ -81,6 +80,6 @@ router.register<typeof GetBalancesRequestSchema, GetBalancesResponse>(
       })
     }
 
-    request.end({ balances })
+    request.end({ account: account.name, balances })
   },
 )
