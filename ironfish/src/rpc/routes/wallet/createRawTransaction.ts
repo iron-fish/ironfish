b87ff229e6ff3170337775c70b31b4045cf2c63e
Evan Richard <evan@ironfish.network>
2023-01-11 16:02:59 -0500

    Update createRawTransaction.ts
    Start to fill things out.

--- ironfish/src/rpc/routes/wallet/createRawTransaction.ts
+++ ironfish/src/rpc/routes/wallet/createRawTransaction.ts
@@ -2,18 +2,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
-import { RawTransactionSerde } from '../../../primitives/rawTransaction'
 import { ApiNamespace, router } from '../router'
 
-export type CreateRawTransactionRequest = { transaction: string }
+export type CreateRawTransactionRequest = { 
+  options: string  // TODO change fields to match the request
+}
 
 export type CreateRawTransactionResponse = {
   transaction: string
 }
 
+// TODO fixme
 export const CreateRawTransactionRequestSchema: yup.ObjectSchema<CreateRawTransactionRequest> = yup
   .object({
-    transaction: yup.string().defined(),
+    options: yup.string().defined(),
   })
   .defined()
 
@@ -27,11 +29,29 @@ router.register<typeof CreateRawTransactionRequestSchema, CreateRawTransactionRe
   `${ApiNamespace.wallet}/createRawTransaction`,
   CreateRawTransactionRequestSchema,
   async (request, node): Promise<void> => {
-    const rawTransactionBytes = Buffer.from(request.data.transaction, 'hex')
-    const rawTransaction = RawTransactionSerde.deserialize(rawTransactionBytes)
-    const postedTransaction = await node.wallet.postTransaction(rawTransaction)
-    const postedTransactionBytes = postedTransaction.serialize()
+    // const rawTransactionBytes = Buffer.from(request.data.transaction, 'hex')
+    // const rawTransaction = RawTransactionSerde.deserialize(rawTransactionBytes)
+    // const postedTransaction = await node.wallet.postTransaction(rawTransaction)
+    // const postedTransactionBytes = postedTransaction.serialize()
 
-    request.end({ transaction: postedTransactionBytes.toString('hex') })
+    // request.end({ transaction: postedTransactionBytes.toString('hex') })
+    // wallet.createTransaction takes:
+    // sender: Account, // this'll be a stringified public key
+    // receives: {
+    //   publicAddress: string 
+    //   amount: bigint
+    //   memo: string
+    //   assetId: Buffer
+    // }[],
+    // mints: MintDescription[],
+    // burns: BurnDescription[],
+    // fee: bigint,
+    // expiration: number,
+
+    // maybe start with a version that just supplies [] and [] for mints and burns
+    // and then add the ability to specify them later
+    // const transaction = await node.wallet.createTransaction(request.options.sender, request.options.receives, request.options.fee, request.options.expiration)
+    // const transactionBytes = transaction.serialize()
+    // request.end({ transaction: transactionBytes.toString('hex') })
   },
 )
