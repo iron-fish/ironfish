808e1ed66cbff17e3c5ee24d56b5026725db2e61
Hugh Cunningham <hugh@ironfish.network>
2023-01-10 16:29:51 -0800

    fixes lint

--- ironfish-cli/src/commands/wallet/repair.ts
+++ ironfish-cli/src/commands/wallet/repair.ts
@@ -1,7 +1,15 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Account, Assert, Blockchain, isExpiredSequence, NodeUtils, Wallet, WalletDB } from '@ironfish/sdk'
+import {
+  Account,
+  Assert,
+  Blockchain,
+  isExpiredSequence,
+  NodeUtils,
+  Wallet,
+  WalletDB,
+} from '@ironfish/sdk'
 import { IronfishCommand } from '../../command'
 import { LocalFlags } from '../../flags'
 
