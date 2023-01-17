846e8759d7ebc589c7774fb5b59072dd7aed3982
Evan Richard <evan@ironfish.network>
2023-01-17 16:20:53 -0500

    Update errors.ts
    Use renderIron instead of dumping raw strings.

--- ironfish/src/wallet/errors.ts
+++ ironfish/src/wallet/errors.ts
@@ -2,13 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { CurrencyUtils } from "../utils"
+
 export class NotEnoughFundsError extends Error {
   name = this.constructor.name
 
   constructor(assetId: Buffer, amount: bigint, amountNeeded: bigint) {
     super()
-    this.message = `Insufficient funds: Needed ${amountNeeded.toString()} but have ${amount.toString()} for asset '${assetId.toString(
-      'hex',
-    )}'`
+    this.message = `Insufficient funds: Needed ${CurrencyUtils.renderIron(amountNeeded.toString())} but have '${CurrencyUtils.renderIron(amount, true, assetId.toString('hex',))}'`
   }
 }
