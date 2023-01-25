b1bee72d5174338ea0472746cf5293a231a2fb2c
Hugh Cunningham <hugh@ironfish.network>
2023-01-25 13:01:58 -0800

    fixes lint

--- ironfish/src/wallet/account.ts
+++ ironfish/src/wallet/account.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { BufferMap, BufferSet } from 'buffer-map'
+import { BufferMap } from 'buffer-map'
 import MurmurHash3 from 'imurmurhash'
 import { Assert } from '../assert'
 import { BlockHeader, Transaction } from '../primitives'
