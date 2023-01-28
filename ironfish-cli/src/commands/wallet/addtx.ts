cbddcb8d4a0e2bb677d4b19a388f3a47e62adcfd
Jason Spafford <nullprogrammer@gmail.com>
2023-01-27 23:56:14 -0500

    Add broadcast flag

--- ironfish-cli/src/commands/wallet/addtx.ts
+++ ironfish-cli/src/commands/wallet/addtx.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { CliUx } from '@oclif/core'
+import { CliUx, Flags } from '@oclif/core'
 import { IronfishCommand } from '../../command'
 import { RemoteFlags } from '../../flags'
 
@@ -10,6 +10,11 @@ export class AddTxCommand extends IronfishCommand {
 
   static flags = {
     ...RemoteFlags,
+    broadcast: Flags.boolean({
+      default: true,
+      allowNo: true,
+      description: 'Broadcast the transaction to the network after adding',
+    }),
   }
 
   static args = [
@@ -21,12 +26,12 @@ export class AddTxCommand extends IronfishCommand {
   ]
 
   async start(): Promise<void> {
-    const { args } = await this.parse(AddTxCommand)
+    const { flags, args } = await this.parse(AddTxCommand)
     const transaction = args.transaction as string
 
     CliUx.ux.action.start(`Adding transaction`)
     const client = await this.sdk.connectRpc()
-    const response = await client.addTransaction({ transaction })
+    const response = await client.addTransaction({ transaction, broadcast: flags.broadcast })
     CliUx.ux.action.stop()
 
     this.log(`Transaction added for account ${response.content.account}`)
