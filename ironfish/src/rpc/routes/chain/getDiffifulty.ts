dabda2a9b97937be69c53fbf4ff5be8f7e2c8b95
Jason Spafford <nullprogrammer@gmail.com>
2023-01-30 21:54:37 -0500

    Update ironfish/src/rpc/routes/chain/getDiffifulty.ts

--- ironfish/src/rpc/routes/chain/getDiffifulty.ts
+++ ironfish/src/rpc/routes/chain/getDiffifulty.ts
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
-import { BlockHashSerdeInstance } from '../../../serde'
 import { ValidationError } from '../../adapters'
 import { ApiNamespace, router } from '../router'
 
@@ -52,7 +51,7 @@ router.register<typeof GetDifficultyRequestSchema, GetDifficultyResponse>(
 
     request.end({
       sequence,
-      hash: BlockHashSerdeInstance.serialize(block.hash),
+      hash: block.hash.toString('hex'),
       difficulty: block.target.toDifficulty().toString(),
     })
   },
