7220fc848a3fdc8c52373aa016350fc12427665a
mat-if <97762857+mat-if@users.noreply.github.com>
2023-02-07 10:38:51 -0700

    test(ironfish): introduce some test scaffolding for the mining pool (#3265)
    * test(ironfish): introduce some test scaffolding for the mining pool
    
    This introduces some basic scaffolding for the poolDatabase and
    MiningPoolShares classes. While this change doesn't introduce any meaningful unit
    tests, it will enable testing with the refactor PRs, so that we can be
    more confident in the changes being made.
    
    * fix: poolShares test with an in-memory sqlite db

---
+++ ironfish/src/mining/poolShares.test.ts
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { createRootLogger } from '../logger'
+import { createRouteTest } from '../testUtilities/routeTest'
+import { MiningPoolShares } from './poolShares'
+
+describe('poolShares', () => {
+  const routeTest = createRouteTest()
+  let shares: MiningPoolShares
+
+  beforeEach(async () => {
+    shares = await MiningPoolShares.init({
+      rpc: routeTest.client,
+      config: routeTest.sdk.config,
+      logger: createRootLogger().withTag('test'),
+      enablePayouts: false,
+      dbPath: ':memory:',
+    })
+
+    await shares.start()
+  })
+
+  afterEach(async () => {
+    await shares.stop()
+  })
+
+  // TODO(mat): This is an example, new tests will come with the refactor PRs
+  it('submitShare', async () => {
+    const address = 'fakeAddress'
+    await shares.submitShare(address)
+
+    const shareCount = await shares.sharesPendingPayout(address)
+    expect(shareCount).toEqual(1)
+  })
+})
