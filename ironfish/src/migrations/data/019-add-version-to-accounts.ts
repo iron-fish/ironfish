98a0929febd49ad79b1b02a6d17036db47385992
Evan Richard <evan@ironfish.network>
2023-02-06 19:10:49 -0500

    Create 019-add-version-to-accounts.ts
    Begin to populate migration.

---
+++ ironfish/src/migrations/data/019-add-version-to-accounts.ts
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { Logger } from '../../logger'
+import { IronfishNode } from '../../node'
+import {
+  BufferEncoding,
+  DatabaseSchema,
+  IDatabase,
+  IDatabaseStore,
+  IDatabaseTransaction,
+  NULL_ENCODING,
+  PrefixEncoding,
+  U32_ENCODING_BE,
+} from '../../storage'
+import { Account } from '../../wallet'
+import { Migration } from '../migration'
+
+export class Migration017 extends Migration {
+  path = __filename
+
+  prepare(node: IronfishNode): IDatabase {
+    return node.wallet.walletDb.db
+  }
+
+  async forward(
+    node: IronfishNode,
+    logger: Logger,
+  ): Promise<void> {
+    // TODO something that does the migration
+    const accounts = []
+    for await (const accountValue of node.wallet.walletDb.loadAccounts()) {
+      accounts.push(
+        new Account({
+          ...accountValue,
+          version: 1
+        }),
+      )
+    }  
+  }
+
+  async backward(node: IronfishNode, db: IDatabase): Promise<void> {
+    // TODO something that undoes the migration
+    throw new Error('Not implemented')
+  }
+
+}
