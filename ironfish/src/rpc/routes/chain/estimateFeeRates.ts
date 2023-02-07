cb6bf9e2223e3ad6b2c798461afb66e7355b294f
ygao76 <yajun@ironfish.network>
2023-02-06 16:20:24 -0800

    Revert "Revert "rename fee rate options""
    This reverts commit 14b7cf25d0b04494ba297075c534054f6f3bb62b.

--- ironfish/src/rpc/routes/chain/estimateFeeRates.ts
+++ ironfish/src/rpc/routes/chain/estimateFeeRates.ts
@@ -7,9 +7,9 @@ import { ApiNamespace, router } from '../router'
 
 export type EstimateFeeRatesRequest = { priority?: PriorityLevel } | undefined
 export type EstimateFeeRatesResponse = {
-  low?: string
-  medium?: string
-  high?: string
+  slow?: string
+  average?: string
+  fast?: string
 }
 
 export const EstimateFeeRatesRequestSchema: yup.ObjectSchema<EstimateFeeRatesRequest> = yup
@@ -20,9 +20,9 @@ export const EstimateFeeRatesRequestSchema: yup.ObjectSchema<EstimateFeeRatesReq
 
 export const EstimateFeeRatesResponseSchema: yup.ObjectSchema<EstimateFeeRatesResponse> = yup
   .object({
-    low: yup.string(),
-    medium: yup.string(),
-    high: yup.string(),
+    slow: yup.string(),
+    average: yup.string(),
+    fast: yup.string(),
   })
   .defined()
 
@@ -44,9 +44,9 @@ router.register<typeof EstimateFeeRatesRequestSchema, EstimateFeeRatesResponse>(
       const feeRates = feeEstimator.estimateFeeRates()
 
       request.end({
-        low: feeRates.low > 0 ? feeRates.low.toString() : '1',
-        medium: feeRates.medium > 0 ? feeRates.medium.toString() : '1',
-        high: feeRates.high > 0 ? feeRates.high.toString() : '1',
+        slow: feeRates.low > 0 ? feeRates.low.toString() : '1',
+        average: feeRates.medium > 0 ? feeRates.medium.toString() : '1',
+        fast: feeRates.high > 0 ? feeRates.high.toString() : '1',
       })
     }
   },
