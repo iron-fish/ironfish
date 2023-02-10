0b95429e6ba75184a47c0deef9ca35ed7d62b998
jowparks <jowparks@gmail.com>
2023-02-10 13:07:11 -0800

    feat: optional spending key migration (#3367)
    * feat: make spending key optional for account construction
    
    * feat: make spending key optional for account construction
    
    * feat: make spending key optional for account construction
    
    * feat: optional spending key migration

--- ironfish/src/wallet/walletdb/walletdb.ts
+++ ironfish/src/wallet/walletdb/walletdb.ts
@@ -34,7 +34,7 @@ import { HeadValue, NullableHeadValueEncoding } from './headValue'
 import { AccountsDBMeta, MetaValue, MetaValueEncoding } from './metaValue'
 import { TransactionValue, TransactionValueEncoding } from './transactionValue'
 
-export const VERSION_DATABASE_ACCOUNTS = 19
+export const VERSION_DATABASE_ACCOUNTS = 20
 
 const getAccountsDBMetaDefaults = (): AccountsDBMeta => ({
   defaultAccountId: null,
@@ -143,7 +143,7 @@ export class WalletDB {
     })
 
     this.accounts = this.db.addStore({
-      name: 'a',
+      name: 'a0',
       keyEncoding: new StringEncoding(),
       valueEncoding: new AccountValueEncoding(),
     })
