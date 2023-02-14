65f10a469a3b87eefd961d9c29b9f7f595dbf870
ygao76 <4500784+ygao76@users.noreply.github.com>
2023-02-14 14:26:31 -0800

    wallet prune (#3413)
    * Create wallet:prune
    
    * Remove db open
    
    * Close db

---
+++ ironfish-cli/src/commands/wallet/prune.ts
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { NodeUtils } from '@ironfish/sdk'
+import { CliUx, Flags } from '@oclif/core'
+import { IronfishCommand } from '../../command'
+import { LocalFlags } from '../../flags'
+
+export default class PruneCommand extends IronfishCommand {
+  static description = 'Removes expired transactions from the wallet'
+
+  static hidden = false
+
+  static flags = {
+    ...LocalFlags,
+    compact: Flags.boolean({
+      char: 'c',
+      default: true,
+      allowNo: true,
+      description: 'Compact the database',
+    }),
+  }
+
+  async start(): Promise<void> {
+    const { flags } = await this.parse(PruneCommand)
+
+    CliUx.ux.action.start(`Opening node`)
+    const node = await this.sdk.node()
+    await NodeUtils.waitForOpen(node)
+    CliUx.ux.action.stop('Done.')
+
+    if (flags.compact) {
+      CliUx.ux.action.start(`Compacting wallet database`)
+      await node.wallet.walletDb.db.compact()
+      CliUx.ux.action.stop()
+    }
+
+    await node.closeDB()
+  }
+}
