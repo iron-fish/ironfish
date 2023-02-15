863fb31f0f48dc004de29eb6ed95d581a416670e
Mat <mat@ironfish.network>
2023-02-15 12:23:01 -0700

    feat(ironfish): improve the limits of GetBlockHeaders
    This introduces a limit of 1024 headers that can be requested per
    GetBlockHeaders request. A block header is currently ~180 bytes, which
    would make the max message size around 185kb. This also introduces a
    limit to the number of total block headers that can be looked up from
    disk, given that GetBlockHeaders can use a `skip` amount to potentially
    use an excessive amount of disk usage.

--- ironfish/src/network/peerNetwork.ts
+++ ironfish/src/network/peerNetwork.ts
@@ -65,7 +65,7 @@ import { PeerManager } from './peers/peerManager'
 import { TransactionFetcher } from './transactionFetcher'
 import { IsomorphicWebSocketConstructor } from './types'
 import { parseUrl } from './utils/parseUrl'
-import { MAX_REQUESTED_BLOCKS, VERSION_PROTOCOL } from './version'
+import { MAX_REQUESTED_BLOCKS, MAX_REQUESTED_HEADERS, VERSION_PROTOCOL } from './version'
 import { WebSocketServer } from './webSocketServer'
 
 /**
@@ -1035,12 +1035,14 @@ export class PeerNetwork {
       return new GetBlockHeadersResponse([], rpcId)
     }
 
-    if (request.message.limit > MAX_REQUESTED_BLOCKS) {
+    if (request.message.limit > MAX_REQUESTED_HEADERS) {
       peer.punish(
         BAN_SCORE.MAX,
         `Peer sent GetBlockHeaders with limit of ${request.message.limit}`,
       )
-      const error = new CannotSatisfyRequestError(`Requested more than ${MAX_REQUESTED_BLOCKS}`)
+      const error = new CannotSatisfyRequestError(
+        `Requested more than ${MAX_REQUESTED_HEADERS}`,
+      )
       throw error
     }
 
@@ -1058,6 +1060,9 @@ export class PeerNetwork {
     const headers = []
     let skipCounter = skip
 
+    // Limit the total number of lookups to avoid excessive disk usage
+    let remainingLookups = MAX_REQUESTED_HEADERS * 2
+
     // If `reverse` is true, we iterate in descending order, using `start` as the
     // highest sequence.  Otherwise, we iterate in ascending order, using
     // `start` as the lowest sequence.
@@ -1066,6 +1071,11 @@ export class PeerNetwork {
       : (from: BlockHeader) => this.chain.iterateTo(from)
 
     for await (const header of iterationFunction(from)) {
+      remainingLookups -= 1
+      if (remainingLookups === 0) {
+        break
+      }
+
       if (skip) {
         if (skipCounter < skip) {
           skipCounter += 1
