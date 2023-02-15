1b80012dad047045feb7d4dd8b73f33a9bfda8d2
mat-if <97762857+mat-if@users.noreply.github.com>
2023-02-15 11:08:44 -0700

    feat(ironfish): use GetBlockHeaders if peer has the appropriate version (#3425)
    This allows us to use the new GetBlockHeaders message without requiring
    a minimum version bump, as syncing from a peer on an older version will
    default to the previously expected behavior of using GetBlockHashes.

--- ironfish/src/network/version.ts
+++ ironfish/src/network/version.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-export const VERSION_PROTOCOL = 19
+export const VERSION_PROTOCOL = 20
 export const VERSION_PROTOCOL_MIN = 19
 
 export const MAX_REQUESTED_BLOCKS = 50
