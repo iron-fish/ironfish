254f1fb76998675d8a9d1a750efc0257f7cb2cb9
Jason Spafford <nullprogrammer@gmail.com>
2023-02-16 20:19:29 -0500

    Add config:unset command
    This new command will unset a config variable

---
+++ ironfish/src/rpc/routes/config/unsetConfig.ts
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import * as yup from 'yup'
+import { ConfigOptions, ConfigOptionsSchema } from '../../../fileStores/config'
+import { ApiNamespace, router } from '../router'
+import { setUnknownConfigValue } from './uploadConfig'
+
+export type UnsetConfigRequest = { name: string }
+export type UnsetConfigResponse = Partial<ConfigOptions>
+
+export const UnsetConfigRequestSchema: yup.ObjectSchema<UnsetConfigRequest> = yup
+  .object({
+    name: yup.string().defined(),
+  })
+  .defined()
+
+export const UnsetConfigResponseSchema: yup.ObjectSchema<UnsetConfigResponse> =
+  ConfigOptionsSchema
+
+router.register<typeof UnsetConfigRequestSchema, UnsetConfigResponse>(
+  `${ApiNamespace.config}/unsetConfig`,
+  UnsetConfigRequestSchema,
+  async (request, node): Promise<void> => {
+    setUnknownConfigValue(node.config, request.data.name)
+    await node.config.save()
+    request.end()
+  },
+)
