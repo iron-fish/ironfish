83446c1433f79eb2364dcf1c53086c8cab6bc5dd
Evan Richard <evan@ironfish.network>
2023-02-21 15:53:57 -0500

    Create importViewAccount.test.ts

---
+++ ironfish/src/rpc/routes/wallet/importViewAccount.test.ts
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { v4 as uuid } from 'uuid'
+import { createRouteTest } from '../../../testUtilities/routeTest'
+
+describe('Route wallet/importViewAccount', () => {
+  const routeTest = createRouteTest(true)
+
+  it('should import a view only account that has no spending key', async () => {
+    const account = await routeTest.node.wallet.createAccount(uuid(), true)
+
+    const importRequest = {
+      account: {
+        name: account.name,
+        viewKey: account.viewKey,
+        publicAddress: account.publicAddress,
+        incomingViewKey: account.incomingViewKey,
+        outgoingViewKey: account.outgoingViewKey,
+        version: account.version,
+      },
+      rescan: false,
+    }
+    // delete the account or else the import will fail
+    await routeTest.node.wallet.removeAccount(account)
+
+    const response = await routeTest.client
+      .request<any>('wallet/importViewAccount', {
+        importRequest,
+      })
+      .waitForEnd()
+
+    expect(response.status).toBe(200)
+    expect(response.content).toMatchObject({
+      name: account.name,
+      isDefaultAccount: true,
+    })
+  })
+})
