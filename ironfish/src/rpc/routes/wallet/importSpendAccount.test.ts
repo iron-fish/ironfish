b57ac94611a353e722b9f5ebbf4e2f2e0dd5b329
Evan Richard <evan@ironfish.network>
2023-02-21 15:52:09 -0500

    Create importSpendAccount.test.ts

---
+++ ironfish/src/rpc/routes/wallet/importSpendAccount.test.ts
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { v4 as uuid } from 'uuid'
+import { createRouteTest } from '../../../testUtilities/routeTest'
+
+describe('Route wallet/importSpendAccount', () => {
+  const routeTest = createRouteTest(true)
+
+  it('should import a spending account', async () => {
+    const account = await routeTest.node.wallet.createAccount(uuid(), true)
+
+    const importRequest = {
+      account: {
+        name: account.name,
+        spendingKey: account.spendingKey,
+        version: account.version,
+      },
+      rescan: false,
+    }
+    // delete the account or else the import will fail
+    await routeTest.node.wallet.removeAccount(account)
+
+    const response = await routeTest.client
+      .request<any>('wallet/importSpendAccount', {
+        importRequest,
+      })
+      .waitForEnd()
+
+    expect(response.status).toBe(200)
+    expect(response.content).toMatchObject({
+      name: account.name,
+      isDefaultAccount: true,
+    })
+  })
+})
