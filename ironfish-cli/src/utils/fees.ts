5a2fa82c73838a636f90643458bc3c8785ea533a
Jason Spafford <nullprogrammer@gmail.com>
2023-02-22 16:18:49 -0500

    WIP

--- ironfish-cli/src/utils/fees.ts
+++ ironfish-cli/src/utils/fees.ts
@@ -22,65 +22,30 @@ export async function selectFee(options: {
   account?: string
   confirmations?: number
 }): Promise<RawTransaction> {
-  const getTxWithFee = async (
-    feeRate: bigint,
-  ): Promise<{
-    transaction: RawTransaction | null
-    fee: bigint | null
-  }> => {
-    const promise = options.client.createTransaction({
-      ...options.transaction,
-      feeRate: CurrencyUtils.encode(feeRate),
-    })
-
-    const response = await promise.catch((e) => {
-      if (e instanceof RpcRequestError && e.code === ERROR_CODES.INSUFFICIENT_BALANCE) {
-        return null
-      } else {
-        throw e
-      }
-    })
-
-    if (response === null) {
-      return {
-        transaction: null,
-        fee: null,
-      }
-    }
-
-    const bytes = Buffer.from(response.content.transaction, 'hex')
-    const raw = RawTransactionSerde.deserialize(bytes)
-
-    return {
-      transaction: raw,
-      fee: feeRate,
-    }
-  }
-
   const feeRates = await options.client.estimateFeeRates()
 
   const [slow, average, fast] = await Promise.all([
-    getTxWithFee(CurrencyUtils.decode(feeRates.content.slow)),
-    getTxWithFee(CurrencyUtils.decode(feeRates.content.average)),
-    getTxWithFee(CurrencyUtils.decode(feeRates.content.fast)),
+    getTxWithFee(
+      options.client,
+      options.transaction,
+      CurrencyUtils.decode(feeRates.content.slow),
+    ),
+    getTxWithFee(
+      options.client,
+      options.transaction,
+      CurrencyUtils.decode(feeRates.content.average),
+    ),
+    getTxWithFee(
+      options.client,
+      options.transaction,
+      CurrencyUtils.decode(feeRates.content.fast),
+    ),
   ])
 
-  const choices: {
-    name: string
-    disabled?: string | boolean
-    value: {
-      transaction: RawTransaction | null
-      fee: bigint
-    } | null
-  }[] = [
-    {
-      name: `Slow ${CurrencyUtils.renderIron(slow.fee)}`,
-      disabled: 'Not enough $IRON',
-      value: {
-        transaction: slow.transaction,
-        fee: slow.fee,
-      },
-    },
+  const choices = [
+    getChoiceFromTx('Slow', slow),
+    getChoiceFromTx('Average', average),
+    getChoiceFromTx('Fast', fast),
     {
       name: 'Enter your own',
       value: null,
@@ -108,7 +73,7 @@ export async function selectFee(options: {
     })
 
     const input = await CliUx.ux.prompt(
-      `Enter the fee amount in $IRON (blance: ${CurrencyUtils.renderIron(
+      `Enter the fee amount in $IRON (balance: ${CurrencyUtils.renderIron(
         response.content.confirmed,
       )})`,
       {
@@ -121,4 +86,50 @@ export async function selectFee(options: {
 
   Assert.isNotNull(result.selection.transaction)
   return result.selection.transaction
+}
+
+async function getTxWithFee(
+  client: RpcClient,
+  params: CreateTransactionRequest,
+  feeRate: bigint,
+): Promise<RawTransaction | null> {
+  const promise = client.createTransaction({
+    ...params,
+    feeRate: CurrencyUtils.encode(feeRate),
+  })
+
+  const response = await promise.catch((e) => {
+    if (e instanceof RpcRequestError && e.code === ERROR_CODES.INSUFFICIENT_BALANCE) {
+      return null
+    } else {
+      throw e
+    }
+  })
+
+  if (Math.random() < 1) {
+    return null
+  }
+
+  if (response === null) {
+    return null
+  }
+
+  const bytes = Buffer.from(response.content.transaction, 'hex')
+  const raw = RawTransactionSerde.deserialize(bytes)
+  return raw
+}
+
+function getChoiceFromTx(
+  name: string,
+  transaction: RawTransaction | null,
+): {
+  name: string
+  disabled?: string | boolean
+  value: RawTransaction | null
+} {
+  return {
+    name: `${name} ${transaction ? CurrencyUtils.renderIron(transaction.fee) : ''}`,
+    disabled: transaction ? false : 'Not enough $IRON',
+    value: transaction,
+  }
 }
