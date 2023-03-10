78139ab1a763468ffad318517b8dd26f94f1fb9b
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2023-03-10 12:51:42 -0800

    changes timestampToTransactionHash to one-to-many (#3625)
    * changes timestampToTransactionHash to one-to-many
    
    it's possible for multiple transactions to have the same timestamp in a wallet.
    
    for example, if an account receives two transactions that are on the same block,
    but did not sync those transactions until they were on the chain, then both
    transactions will have the block header timestamp.
    
    this example might occur in practice if a node was offline when the transactions
    were first created and/or did not receive the transactions in gossip messages
    before they were added to the chain.
    
    this might also happen for an account that is imported to a wallet after the
    transactions were added to the chain. when rescanning the chain for the imported
    account all transactions will use the timestamp of the block header of the block
    they are found on.
    
    we use the 'timestampToTransactionHash' index to load transactions in order for
    the 'getAccountTransactions' RPC. the impact of the one-to-one datastore is that
    the RPC would miss transactions that the account has.
    
    * adds more expectations to test
    
    ensure that the right transactions are returned from getTransactionsByTime in
    the expected order
    
    * fixes order of transactions in test
    
    do not expect on ordering for accountA transactions - fixture generation sets
            the timestamps for pending transactions to be equal to the time of the
            test run, so we cannot maintain ordering on these timestamps right now

---
+++ ironfish/src/migrations/data/026-timestamp-to-transactions.ts
@@ -0,0 +1,98 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Logger } from '../../logger'
+import { IronfishNode } from '../../node'
+import { IDatabase, IDatabaseTransaction } from '../../storage'
+import { createDB } from '../../storage/utils'
+import { Account } from '../../wallet'
+import { Migration } from '../migration'
+import { GetStores } from './026-timestamp-to-transactions/stores'
+
+export class Migration026 extends Migration {
+  path = __filename
+
+  prepare(node: IronfishNode): IDatabase {
+    return createDB({ location: node.config.walletDatabasePath })
+  }
+
+  async forward(
+    node: IronfishNode,
+    db: IDatabase,
+    _tx: IDatabaseTransaction | undefined,
+    logger: Logger,
+  ): Promise<void> {
+    const accounts = []
+    const stores = GetStores(db)
+
+    for await (const account of stores.old.accounts.getAllValuesIter()) {
+      accounts.push(
+        new Account({
+          ...account,
+          createdAt: null,
+          walletDb: node.wallet.walletDb,
+        }),
+      )
+    }
+
+    const accountsString =
+      accounts.length === 1 ? `${accounts.length} account` : `${accounts.length} accounts`
+    logger.info(`Indexing transaction timestamps for ${accountsString}`)
+
+    for (const account of accounts) {
+      logger.info('')
+      logger.info(`  Indexing transaction timestamps for account ${account.name}`)
+
+      let transactionCount = 0
+      for await (const { timestamp, transaction } of stores.old.transactions.getAllValuesIter(
+        undefined,
+        account.prefixRange,
+      )) {
+        await stores.new.timestampToTransactionHash.put(
+          [account.prefix, [timestamp.getTime(), transaction.hash()]],
+          null,
+        )
+
+        transactionCount++
+      }
+
+      const transactionsString =
+        transactionCount === 1
+          ? `${transactionCount} transaction`
+          : `${transactionCount} transactions`
+      logger.info(`  Completed indexing ${transactionsString} for account ${account.name}`)
+    }
+
+    await stores.old.timestampToTransactionHash.clear()
+    logger.info('')
+  }
+
+  async backward(node: IronfishNode, db: IDatabase): Promise<void> {
+    const accounts = []
+    const stores = GetStores(db)
+
+    for await (const account of stores.old.accounts.getAllValuesIter()) {
+      accounts.push(
+        new Account({
+          ...account,
+          createdAt: null,
+          walletDb: node.wallet.walletDb,
+        }),
+      )
+    }
+
+    for (const account of accounts) {
+      for await (const { timestamp, transaction } of stores.old.transactions.getAllValuesIter(
+        undefined,
+        account.prefixRange,
+      )) {
+        await stores.old.timestampToTransactionHash.put(
+          [account.prefix, timestamp.getTime()],
+          transaction.hash(),
+        )
+      }
+
+      await stores.new.timestampToTransactionHash.clear()
+    }
+  }
+}
