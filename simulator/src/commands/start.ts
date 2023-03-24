bf1d46a5b940e715ef810d89b681ae79e2e0e88f
danield9tqh <danield9tqh@gmail.com>
2023-03-24 12:54:43 -0700

    change start command to take argument not flag

--- simulator/src/commands/start.ts
+++ simulator/src/commands/start.ts
@@ -5,34 +5,52 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { createRootLogger } from '@ironfish/sdk'
-import { Command, Flags } from '@oclif/core'
+import { Command } from '@oclif/core'
 import { Config } from '@oclif/core'
 import { SIMULATIONS } from '../simulations'
 
 export abstract class Start extends Command {
   static description = 'Start a simulation'
-  static flags = {
-    simulation: Flags.integer({ char: 's', required: true }),
-  }
+
+  static args = [
+    {
+      name: 'simulation',
+      parse: (input: string): Promise<number | null> => Promise.resolve(parseNumber(input)),
+      required: true,
+      description: 'The simulation to run',
+    },
+  ]
 
   constructor(argv: string[], config: Config) {
     super(argv, config)
   }
 
   async run(): Promise<void> {
-    const { flags } = await this.parse(Start)
+    const { args } = await this.parse(Start)
+    const simulation = args.simulation as number | null
     const logger = createRootLogger()
 
-    const toRun = SIMULATIONS.at(flags.simulation - 1)
-    if (!toRun) {
-      logger.log(`could not find simulation ${flags.simulation}`)
+    if (simulation === null) {
+      logger.log(`simulation argument is invalid`)
       this.exit()
       return
     }
 
-    logger.log(`starting simulation ${flags.simulation}`)
+    const toRun = SIMULATIONS.at(simulation - 1)
+    if (!toRun || simulation < 1) {
+      logger.log(`could not find simulation ${simulation}`)
+      this.exit()
+      return
+    }
+
+    logger.log(`starting simulation ${simulation}`)
     await toRun.run(logger)
     logger.log('simulation ended')
     this.exit()
   }
+}
+
+function parseNumber(input: string): number | null {
+  const parsed = Number(input)
+  return isNaN(parsed) ? null : parsed
 }
