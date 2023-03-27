0ce9c652645c17988648d189e609d4be3fc7ce3c
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2023-03-27 16:09:53 -0700

    migrates account createdAt from Date to HeadValue (#3656)
    * migrates account createdAt from Date to HeadValue
    
    to support block-based account birthdays we will store the chain head (block
    hash and sequence) at the time the account was created.
    
    adds migration to set createdAt to null for all existing accounts
    
    * implements backward migration
    
    * adds comments for numeric literals in encodings

---
+++ ironfish/src/migrations/data/027-account-created-at-block/new/index.ts
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { IDatabase, IDatabaseStore, StringEncoding } from '../../../../storage'
+import { AccountValue, AccountValueEncoding } from './accountValue'
+
+export function GetNewStores(db: IDatabase): {
+  accounts: IDatabaseStore<{ key: string; value: AccountValue }>
+} {
+  const accounts: IDatabaseStore<{ key: string; value: AccountValue }> = db.addStore(
+    {
+      name: 'a',
+      keyEncoding: new StringEncoding(),
+      valueEncoding: new AccountValueEncoding(),
+    },
+    false,
+  )
+
+  return { accounts }
+}
