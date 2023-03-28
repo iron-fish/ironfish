bcf12ea012e271001e18337713d9ffe88a033cfe
Evan Richard <evan@ironfish.network>
2023-03-28 16:23:00 -0400

    wip

--- ironfish-cli/scripts/import-export-test.sh
+++ ironfish-cli/scripts/import-export-test.sh
@@ -2,7 +2,7 @@
 #start a clean test node
 
 TEST_VECTOR_LOCATION='./import-export-test-vector/'
-# FORMAT_ARRAY=( blob json mnemonic )
+# FORMAT_ARRAY=( blob json mnemonic ) #TODO mnemonic case needs to respond to name request too
 FORMAT_ARRAY=( blob )
 #import each account
 # for VERSION in {65..72}
@@ -11,30 +11,34 @@ for VERSION in {65..65}
     for FORMAT in "${FORMAT_ARRAY[@]}"
         do
         ACCOUNT_NAME=0p1p${VERSION}_${FORMAT}
-        TEST_INPUT=${TEST_VECTOR_LOCATION}${ACCOUNT_NAME}.txt
-        echo $TEST_INPUT
-        cat $TEST_INPUT
-
+        TEST_FILE=${TEST_VECTOR_LOCATION}${ACCOUNT_NAME}.txt
+        echo $TEST_FILE
+        FILE_CONTENTS=$(cat $TEST_FILE)
         # import filename interactively
         # ironfish wallet:import
         # expect "Paste the output of wallet:export, or your spending key:\r"
-        # send "$TEST_INPUT\r"
+        # send "$TEST_FILE\r"
         # echo "imported $ACCOUNT_NAME"
+        expect -c "
+            ironfish wallet:import
+            expect \"Paste the output of wallet:export, or your spending key:\r\"
+            send \"$FILE_CONTENTS\r\"
+            "
         # TODO verify return code of import
         ironfish wallet:delete $ACCOUNT_NAME --wait
         # TODO verify return code of delete
 
-        # test import by pipe
-        ironfish wallet:import < $TEST_INPUT
-        # TODO verify return code of import
-        ironfish wallet:delete $ACCOUNT_NAME --wait
-        # TODO verify return code of delete
+        # # test import by pipe
+        # ironfish wallet:import < $TEST_FILE
+        # # TODO verify return code of import
+        # ironfish wallet:delete $ACCOUNT_NAME --wait
+        # # TODO verify return code of delete
 
-        # test import by path
-        ironfish wallet:import --path $TEST_INPUT
-        # TODO verify return code of import
-        ironfish wallet:delete $ACCOUNT_NAME --wait
-        # TODO verify return code of delete
+        # # test import by path
+        # ironfish wallet:import --path $TEST_FILE
+        # # TODO verify return code of import
+        # ironfish wallet:delete $ACCOUNT_NAME --wait
+        # # TODO verify return code of delete
         done
     done
 
