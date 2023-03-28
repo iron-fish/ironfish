f3aa504a71028d69095c1a07e065e2e40650d71b
Evan Richard <evan@ironfish.network>
2023-03-28 16:04:52 -0400

    Update import-export-test.sh
    bash script can now open each test vector

--- ironfish-cli/scripts/import-export-test.sh
+++ ironfish-cli/scripts/import-export-test.sh
@@ -1,18 +1,27 @@
+#!/bin/bash
 #start a clean test node
 
+TEST_VECTOR_LOCATION='./import-export-test-vector/'
+FORMAT_ARRAY=( blob json mnemonic )
 #import each account
-for version in 0p1p[65...72]
-    for format in [blob, json, mnemonic]
-        filename = version + format
+for VERSION in {65..72}
+    do
+    for FORMAT in "${FORMAT_ARRAY[@]}"
+        do
+        TEST_INPUT=${TEST_VECTOR_LOCATION}0p1p${VERSION}_${FORMAT}.txt
+        echo $TEST_INPUT
+        cat $TEST_INPUT
 
-        import filename interactively
-        delete account --wait
+        # import filename interactively
+        # delete account --wait
 
-        cat filename > import
-        delete account --wait
+        # cat filename > import
+        # delete account --wait
 
-        import --path filename
-        delete account --wait
+        # import --path filename
+        # delete account --wait
+        done
+    done
 
-        
+
 
