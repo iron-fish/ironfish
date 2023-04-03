7da93d47d98d9ed41cc45d564268c2488cbfad43
Hugh Cunningham <hugh@ironfish.network>
2023-04-03 16:03:51 -0700

    fixes lint

--- ironfish/src/rpc/routes/wallet/types.ts
+++ ironfish/src/rpc/routes/wallet/types.ts
@@ -2,9 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { IronfishNode } from '../../../node'
-import { Note } from '../../../primitives'
-import { CurrencyUtils } from '../../../utils'
-import { Account } from '../../../wallet'
 import { TransactionValue } from '../../../wallet/walletdb/transactionValue'
 
 export type RpcAccountTransaction = {
--- ironfish/src/rpc/routes/wallet/utils.ts
+++ ironfish/src/rpc/routes/wallet/utils.ts
@@ -2,8 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { IronfishNode } from '../../../node'
+import { Note } from '../../../primitives'
+import { CurrencyUtils } from '../../../utils'
 import { Account } from '../../../wallet'
+import { TransactionValue } from '../../../wallet/walletdb/transactionValue'
 import { ValidationError } from '../../adapters'
+import { RpcAccountDecryptedNote } from './types'
 
 export function getAccount(node: IronfishNode, name?: string): Account {
   if (name) {
