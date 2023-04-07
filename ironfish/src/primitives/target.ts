82f41a7fbbf8041494d8128a98454d2c4caae0f2
Derek Guenther <derek@ironfish.network>
2023-04-07 15:48:32 -0400

    Revert changes

--- ironfish/src/primitives/target.ts
+++ ironfish/src/primitives/target.ts
@@ -136,8 +136,8 @@ export class Target {
    * Converts difficulty to Target
    */
   static fromDifficulty(difficulty: bigint): Target {
-    if (difficulty === 1n) {
-      return new Target(MAX_256_BIT_NUM)
+    if (difficulty <= Target.minDifficulty()) {
+      return Target.maxTarget()
     }
     return new Target((2n ** 256n / difficulty).valueOf())
   }
@@ -146,6 +146,9 @@ export class Target {
    * Return the difficulty representation as a big integer
    */
   toDifficulty(): bigint {
+    if (this.targetValue <= 1n) {
+      return MAX_256_BIT_NUM
+    }
     return 2n ** 256n / this.targetValue
   }
 
