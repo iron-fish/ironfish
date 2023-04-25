39b79cb84e581835c2dccb43f58474b58983e289
danield9tqh <danield9tqh@gmail.com>
2023-04-24 20:59:52 -0700

    Add RPC for validating public address

---
+++ ironfish/src/rpc/routes/chain/isValidPublicAddress.ts
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { isValidPublicAddress } from '@ironfish/rust-nodejs'
+import * as yup from 'yup'
+import { ApiNamespace, router } from '../router'
+
+export type IsValidPublicAddressRequest = {
+  address: string
+}
+
+export type IsValidPublicAddressResponse = {
+  valid: boolean
+}
+
+export const IsValidPublicAddressRequestSchema: yup.ObjectSchema<IsValidPublicAddressRequest> =
+  yup
+    .object({
+      address: yup.string().defined(),
+    })
+    .defined()
+
+export const IsValidPublicAddressResponseSchema: yup.ObjectSchema<IsValidPublicAddressResponse> =
+  yup
+    .object({
+      valid: yup.boolean().defined(),
+    })
+    .defined()
+
+router.register<typeof IsValidPublicAddressRequestSchema, IsValidPublicAddressResponse>(
+  `${ApiNamespace.chain}/isValidPublicAddress`,
+  IsValidPublicAddressRequestSchema,
+  (request): void => {
+    request.end({
+      valid: isValidPublicAddress(request.data.address),
+    })
+  },
+)
