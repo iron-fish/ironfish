41315b1c4ee63b047ffc5d07ecbb7834c776cc24
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2023-05-25 06:26:39 -0700

    adds rpc to follow transaction gossip stream (#3925)
    * adds rpc to follow transaction gossip stream
    
    creates an event rpc endpoint to stream serialized transactions as the peer
    network receives them in gossip messages.
    
    adds an event to the peer network that emits each transaction that the node
    receives.
    
    emits all transaction gossip without verifying  or waiting for the mempool to
    accept the transaction.
    
    supports syncing pending transactions to the api.
    
    * emits transaction gossip after filtering for duplicates
    
    removes need for LRU in RPC endpoint if we leverage the filtering in the peer
    network logic
    
    matches the logic for onBlockGossipReceived event, but emits event before
    validating transaction

---
+++ ironfish/src/rpc/routes/event/onTransactionGossip.test.ts
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Peer } from '../../../network/peers/peer'
+import { useAccountFixture, useMinerBlockFixture, useTxFixture } from '../../../testUtilities'
+import { createRouteTest } from '../../../testUtilities/routeTest'
+
+describe('Route event/onTransactionGossip', () => {
+  const routeTest = createRouteTest()
+
+  it('should stream transactions as they are gossiped to the node', async () => {
+    const { node } = routeTest
+
+    const account = await useAccountFixture(node.wallet, 'a')
+    const block2 = await useMinerBlockFixture(node.chain, 2, account)
+
+    await expect(node.chain).toAddBlock(block2)
+    await node.wallet.updateHead()
+
+    const transaction = await useTxFixture(node.wallet, account, account)
+
+    const response = await routeTest.client.request('event/onTransactionGossip').waitForRoute()
+
+    const peer = new Peer(null)
+
+    await node.peerNetwork['onNewTransaction'](peer, transaction)
+
+    const { value } = await response.contentStream().next()
+
+    expect(value).toMatchObject({
+      serializedTransaction: transaction.serialize().toString('hex'),
+    })
+
+    response.end()
+    expect(response.status).toEqual(200)
+  })
+})
