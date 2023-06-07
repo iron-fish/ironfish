ab5e3ec9a0ae84b29ff4a45017f66e470776e58a
Andrea Corbellini <andrea@iflabs.network>
2023-06-07 14:31:07 -0700

    Add functionality to rename accounts
    - added the `wallet/rename` RPC endpoint
    - added the `wallet:rename` CLI command

---
+++ ironfish-cli/src/commands/wallet/rename.ts
@@ -0,0 +1,38 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { IronfishCommand } from '../../command'
+import { RemoteFlags } from '../../flags'
+
+export class RenameCommand extends IronfishCommand {
+  static description = 'Change the name of an account'
+
+  static args = [
+    {
+      name: 'account',
+      parse: (input: string): Promise<string> => Promise.resolve(input.trim()),
+      required: true,
+      description: 'Name of the account to rename',
+    },
+    {
+      name: 'new-name',
+      parse: (input: string): Promise<string> => Promise.resolve(input.trim()),
+      required: true,
+      description: 'New name to assign to the account',
+    },
+  ]
+
+  static flags = {
+    ...RemoteFlags,
+  }
+
+  async start(): Promise<void> {
+    const { args } = await this.parse(RenameCommand)
+    const account = args.account as string
+    const newName = args['new-name'] as string
+
+    const client = await this.sdk.connectRpc()
+    await client.wallet.renameAccount({ account, newName })
+    this.log(`Account ${account} renamed to ${newName}`)
+  }
+}
