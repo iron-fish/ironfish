7bd2141264eccfef4187a4a7bb2e9593d1f25eea
Joe <joe@iflabs.network>
2023-06-30 10:31:59 -0700

    feat(ifl-1233): move language utils to SDK

--- ironfish/src/utils/language.ts
+++ ironfish/src/utils/language.ts
@@ -3,9 +3,8 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 import { LanguageCode } from '@ironfish/rust-nodejs'
-import inquirer from 'inquirer'
 
-export const LANGUAGES = {
+const LANGUAGES = {
   English: LanguageCode.English,
   ChineseSimplified: LanguageCode.ChineseSimplified,
   ChineseTraditional: LanguageCode.ChineseTraditional,
@@ -16,11 +15,11 @@ export const LANGUAGES = {
   Spanish: LanguageCode.Spanish,
 } as const
 
-type LanguageCodeKey = keyof typeof LANGUAGE_CODES
-type LanguageKey = keyof typeof LANGUAGES
+export type LanguageCodeKey = keyof typeof LANGUAGE_CODES
+export type LanguageKey = keyof typeof LANGUAGES
 
-export const LANGUAGE_KEYS = Object.keys(LANGUAGES) as Array<LanguageKey>
-export const LANGUAGE_VALUES = Object.values(LANGUAGES) as Array<LanguageCode>
+const LANGUAGE_KEYS = Object.keys(LANGUAGES) as Array<LanguageKey>
+const LANGUAGE_VALUES = Object.values(LANGUAGES) as Array<LanguageCode>
 
 const LANGUAGE_CODES = {
   en: LanguageCode.English,
@@ -33,21 +32,7 @@ const LANGUAGE_CODES = {
 const CHINESE_TRADITIONAL_CODES = ['zh-cht', 'zh-hant', 'zh-hk', 'zh-mo', 'zh-tw']
 const CHINESE_SIMPLIFIED_CODES = ['zh', 'zh-chs', 'zh-hans', 'zh-cn', 'zh-sg']
 
-export async function selectLanguage(): Promise<LanguageCode> {
-  const response = await inquirer.prompt<{
-    language: LanguageKey
-  }>([
-    {
-      name: 'language',
-      message: `Select your language`,
-      type: 'list',
-      choices: LANGUAGE_KEYS,
-    },
-  ])
-  return LANGUAGES[response.language]
-}
-
-export function inferLanguageCode(): LanguageCode | null {
+function inferLanguageCode(): LanguageCode | null {
   const languageCode = Intl.DateTimeFormat().resolvedOptions().locale
   if (languageCode.toLowerCase() in CHINESE_SIMPLIFIED_CODES) {
     return LanguageCode.ChineseSimplified
@@ -62,10 +47,21 @@ export function inferLanguageCode(): LanguageCode | null {
   return null
 }
 
-export function languageCodeToKey(code: LanguageCode): LanguageKey {
+function languageCodeToKey(code: LanguageCode): LanguageKey {
   const key = Object.entries(LANGUAGES).find(([_, value]) => value === code)?.[0]
   if (key) {
     return key as LanguageKey
   }
   throw new Error(`No language key found for language code: ${code}`)
+}
+
+export const LanguageUtils = {
+  LANGUAGES,
+  LANGUAGE_CODES,
+  LANGUAGE_KEYS,
+  LANGUAGE_VALUES,
+  CHINESE_TRADITIONAL_CODES,
+  CHINESE_SIMPLIFIED_CODES,
+  inferLanguageCode,
+  languageCodeToKey,
 }
