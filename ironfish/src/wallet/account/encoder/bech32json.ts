1875e945a004ae8fc0983016701b74848ed2c2aa
jowparks <jowparks@gmail.com>
2023-07-06 13:13:42 -0700

    feat(ifl-1234): account import rpc updates (#4030)
    * feat(ifl-1237): generic account encoder
    
    * feat(ifl-1234): account import updates to RPC
    
    * feat(ifl-1234): add tests for importAccount RPC

--- ironfish/src/wallet/account/encoder/bech32json.ts
+++ ironfish/src/wallet/account/encoder/bech32json.ts
@@ -1,23 +1,23 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { EncodingError } from 'bufio'
 import { Bech32m } from '../../../utils'
 import { AccountImport } from '../../walletdb/accountValue'
 import { AccountEncoder } from './encoder'
+import { JsonEncoder } from './json'
 export class Bech32JsonEncoder implements AccountEncoder {
   /**
    * @deprecated Bech32 JSON encoding is deprecated. Use the newest version of the Bech32JSONEncoder.
    */
   encode(value: AccountImport): string {
-    return Bech32m.encode(JSON.stringify(value), 'ironfishaccount00000')
+    return Bech32m.encode(new JsonEncoder().encode(value), 'ironfishaccount00000')
   }
 
   decode(value: string): AccountImport {
     const [decoded, _] = Bech32m.decode(value)
     if (!decoded) {
-      throw new EncodingError('Invalid bech32 JSON encoding')
+      throw new Error('Invalid bech32 JSON encoding')
     }
-    return JSON.parse(decoded) as AccountImport
+    return new JsonEncoder().decode(decoded)
   }
 }
