2973d0179693fdc5da8e3f733d15cb7efa306cdb
Hugh Cunningham <hugh@iflabs.network>
2023-07-06 14:12:53 -0700

    adds wallet/account/index.ts for account module exports

--- ironfish/src/wallet/account/index.ts
+++ ironfish/src/wallet/account/index.ts
@@ -2,7 +2,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 export * from './account'
-export * from './wallet'
-export { AccountValue } from './walletdb/accountValue'
-export * from './validator'
-export * from './walletdb/walletdb'
+export { Format } from './encoder/encoder'
