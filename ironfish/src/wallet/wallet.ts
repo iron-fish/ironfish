5d63f5b4566a02e37bab8da89f9c7e53f8ec1e90
Hugh Cunningham <hugh@iflabs.network>
2023-08-08 15:40:20 -0700

    uses do...while

--- ironfish/src/wallet/wallet.ts
+++ ironfish/src/wallet/wallet.ts
@@ -687,12 +687,11 @@ export class Wallet {
       await this.disconnectBlock(header, transactions)
     })
 
-    let { hashChanged } = await scanProcessor.update({ signal: scan.abortController.signal })
-
-    while (hashChanged) {
-      const result = await scanProcessor.update({ signal: scan.abortController.signal })
-      hashChanged = result.hashChanged
-    }
+    let hashChanged = false
+    do {
+      hashChanged = (await scanProcessor.update({ signal: scan.abortController.signal }))
+        .hashChanged
+    } while (hashChanged)
 
     // Update chainProcessor following scan
     this.chainProcessor.hash = scanProcessor.hash
