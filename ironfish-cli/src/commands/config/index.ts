b86eff5de1ff72b162ad92baa418ef21314af7ef
Rohan Jadvani <5459049+rohanjadvani@users.noreply.github.com>
2023-08-09 13:22:52 -0400

    feat(cli,ironfish): Create a CLI helper to create a config client (#4168)
    * feat(cli,ironfish): Create an SDK method to create a config client
    
    * feat(cli,ironfish): Move utility from SDK to CLI

--- ironfish-cli/src/commands/config/index.ts
+++ ironfish-cli/src/commands/config/index.ts
@@ -6,6 +6,7 @@ import jsonColorizer from 'json-colorizer'
 import { IronfishCommand } from '../../command'
 import { ColorFlag, ColorFlagKey } from '../../flags'
 import { RemoteFlags } from '../../flags'
+import { connectRpcConfig } from '../../utils/clients'
 
 export class ShowCommand extends IronfishCommand {
   static description = `Print out the entire config`
@@ -25,7 +26,7 @@ export class ShowCommand extends IronfishCommand {
   async start(): Promise<void> {
     const { flags } = await this.parse(ShowCommand)
 
-    const client = await this.sdk.connectRpc(flags.local)
+    const client = await connectRpcConfig(this.sdk, flags.local)
     const response = await client.config.getConfig({ user: flags.user })
 
     let output = JSON.stringify(response.content, undefined, '   ')
