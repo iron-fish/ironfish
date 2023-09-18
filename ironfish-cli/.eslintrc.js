10cbd21ee9f843c208a243e2cff5586dd3a21ee0
Rahul Patni <rahul@iflabs.network>
2023-09-18 15:54:18 -0700

    adding ability to show usage of deprecated fields

--- ironfish-cli/.eslintrc.js
+++ ironfish-cli/.eslintrc.js
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 module.exports = {
   extends: ['ironfish'],
   parserOptions: {
@@ -5,5 +8,7 @@ module.exports = {
   },
   rules: {
     'jest/no-standalone-expect': 'off',
+    'deprecation/deprecation': 'warn',
   },
+  plugins: ['deprecation'],
 }
