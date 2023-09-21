bf86e112bcb15917eb770f04de6d81d33adc7b2d
Rahul Patni <rahul@iflabs.network>
2023-09-20 21:29:41 -0700

    Asset verification field in get balance

--- ironfish/src/rpc/routes/wallet/getBalance.ts
+++ ironfish/src/rpc/routes/wallet/getBalance.ts
@@ -18,10 +18,6 @@ export type GetBalanceRequest =
 export type GetBalanceResponse = {
   account: string
   assetId: string
-  /**
-   * @deprecated Please use getAsset endpoint to get this information
-   * */
-  assetVerification: AssetVerification
   confirmed: string
   unconfirmed: string
   unconfirmedCount: number
@@ -31,6 +27,10 @@ export type GetBalanceResponse = {
   confirmations: number
   blockHash: string | null
   sequence: number | null
+  /**
+   * @deprecated Please use getAsset endpoint to get this information
+   * */
+  assetVerification: AssetVerification
 }
 
 export const GetBalanceRequestSchema: yup.ObjectSchema<GetBalanceRequest> = yup
