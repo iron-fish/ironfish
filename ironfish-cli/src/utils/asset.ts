ec08de77b529e0de0f7543479d61e546bc7b1afd
Rahul Patni <rahul@iflabs.network>
2023-09-25 08:25:56 -0700

    choice asset name

--- ironfish-cli/src/utils/asset.ts
+++ ironfish-cli/src/utils/asset.ts
@@ -130,7 +130,8 @@ export async function selectAsset(
   }
 
   const choices = balances.map((balance) => {
-    const assetName = BufferUtils.toHuman(Buffer.from(balance.assetName, 'hex'))
+    const assetName = BufferUtils.toHuman(Buffer.from(assetLookup[balance.assetId].name, 'hex'))
+
     const name = `${balance.assetId} (${assetName}) (${CurrencyUtils.renderIron(
       balance.available,
     )})`
