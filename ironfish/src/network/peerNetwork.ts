2d21cc0e9e39a3f54279f95cbfa792413679a15a
Rahul Patni <rahul@iflabs.network>
2023-10-23 11:24:19 -0700

    adding comment

--- ironfish/src/network/peerNetwork.ts
+++ ironfish/src/network/peerNetwork.ts
@@ -359,6 +359,7 @@ export class PeerNetwork {
       })
     }
 
+    // Connect to prior websocket outbound connections that were saved to disk
     for (const peerAddress of this.peerManager.addressManager.priorConnectedPeerAddresses) {
       this.peerManager.connectToWebSocketAddress({
         host: peerAddress.address,
