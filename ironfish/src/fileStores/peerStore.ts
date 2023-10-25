d86e8b16e4e6bd343bcd639c8ddcdb4ffc080f7e
Rahul Patni <rahul@iflabs.network>
2023-10-25 09:13:24 -0700

    update comment

--- ironfish/src/fileStores/peerStore.ts
+++ ironfish/src/fileStores/peerStore.ts
@@ -17,7 +17,7 @@ export const PeerStoreOptionsDefaults: PeerStoreOptions = {
   // banned peers can go here
 }
 
-// This name is left over from when this file used to be called HostsStore
+// This filename ("hosts.json") is left over from when this file used to be called HostsStore
 // We will likely change the name when we adding more functionality like storing
 // whitelisted peers, banned peers, etc.
 export const PEER_STORE_FILE_NAME = 'hosts.json'
