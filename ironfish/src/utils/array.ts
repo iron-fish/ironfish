b6bf36b702970b7949b985a3aeae41acd5152075
danield9tqh <danield9tqh@gmail.com>
2023-10-26 11:02:21 -0700

    Fix imports

--- ironfish/src/utils/array.ts
+++ ironfish/src/utils/array.ts
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { Assert } from '../assert'
-
 /**
  * Randomizes the order of elements in a given array and returns a new array.
  */
