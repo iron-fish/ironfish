c41468dde05672b3f4ce762269101a97b929dfd0
Andrea <andrea@iflabs.network>
2023-11-06 09:48:16 -0800

    Remove fishtank and simulator
    The new and improved fishtank (framework for simulating Iron Fish
    networks) now lives at https://github.com/iron-fish/ironfish-tank

--- tools/version-bump.js
+++ tools/version-bump.js
@@ -1,10 +1,10 @@
 /**
  * Bumps the patch version of npm packages, and updates the dependencies of other npm
  * packages to use the new version.
- * 
+ *
  * Does not bump the Rust version by default -- call `node version-bump.js rust` to bump
  * the Rust package version.
- * 
+ *
  * Flags:
  *   rust - Bumps the @ironfish/rust-nodejs package version.
  */
@@ -15,7 +15,6 @@ const path = require('path')
 const CLI_PACKAGE = path.join(__dirname, '../ironfish-cli/package.json')
 const NODE_PACKAGE = path.join(__dirname, '../ironfish/package.json')
 const RUST_PACKAGE = path.join(__dirname, '../ironfish-rust-nodejs/package.json')
-const SIMULATOR_PACKAGE = path.join(__dirname, '../simulator/package.json')
 
 const shouldBumpIronfishRust = process.argv.find((a) => a.includes('rust'))
 const isPatchRelease = process.argv.find((a) => a.includes('patch'))
@@ -63,13 +62,13 @@ const readPackage = async (path) => {
     console.log(`Error reading ${path}: ${err}`);
     throw err
   }
-  
+
   return JSON.parse(data)
 }
 
 const writePackage = async (path, package) => {
   const toWrite = JSON.stringify(package, null, 2) + '\n'
-  
+
   try {
     await fs.writeFile(path, toWrite, 'utf8')
   } catch (err) {
@@ -86,42 +85,39 @@ const getDirectories = async source =>
 const bumpNodeAndCliPackage = async (shouldBumpRust) => {
   const nodePackage = await readPackage(NODE_PACKAGE)
   const cliPackage = await readPackage(CLI_PACKAGE)
-  const simulatorPackage = await readPackage(SIMULATOR_PACKAGE)
-  
+
   // Bump node package and packages that depend on it
   const newNodeVersion = bumpVersion(nodePackage.version)
   nodePackage.version = newNodeVersion
   cliPackage.dependencies[nodePackage.name] = newNodeVersion
-  simulatorPackage.dependencies[nodePackage.name] = newNodeVersion
 
   // Bump the CLI
   cliPackage.version = bumpVersion(cliPackage.version)
-  
+
   writePackage(NODE_PACKAGE, nodePackage)
   writePackage(CLI_PACKAGE, cliPackage)
-  writePackage(SIMULATOR_PACKAGE, simulatorPackage)
 }
 
 const bumpRustPackage = async () => {
   const deps = await getDirectories(path.join(__dirname, '../ironfish-rust-nodejs/npm/'))
-  
+
   for (const dep of deps) {
     const package = path.join(__dirname, '../ironfish-rust-nodejs/npm/', dep, 'package.json')
     const depPackage = await readPackage(package)
     depPackage.version = bumpVersion(depPackage.version)
     await writePackage(package, depPackage)
   }
-  
+
   const nodePackage = await readPackage(NODE_PACKAGE)
   const cliPackage = await readPackage(CLI_PACKAGE)
   const rustPackage = await readPackage(RUST_PACKAGE)
-  
+
   const newRustVersion = bumpVersion(rustPackage.version)
-  
+
   rustPackage.version = newRustVersion
   nodePackage.dependencies[rustPackage.name] = newRustVersion
   cliPackage.dependencies[rustPackage.name] = newRustVersion
-  
+
   await writePackage(NODE_PACKAGE, nodePackage)
   await writePackage(CLI_PACKAGE, cliPackage)
   await writePackage(RUST_PACKAGE, rustPackage)
@@ -132,7 +128,7 @@ const main = async () => {
   if (shouldBumpIronfishRust) {
     await bumpRustPackage()
   }
-  
+
   await bumpNodeAndCliPackage()
 }
 
