287eeb2ec0c08405bdcde4422bc50dfa2fda14dd
Rahul Patni <rahul@iflabs.network>
2023-11-07 12:21:30 -0800

    add peer test now asserts on candidates, not yet a peer

--- ironfish/src/rpc/routes/peer/addPeer.test.ts
+++ ironfish/src/rpc/routes/peer/addPeer.test.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { formatWebSocketAddress } from '../../../network'
+import { Assert } from '../../../assert'
 import { createRouteTest } from '../../../testUtilities/routeTest'
 
 describe('Route peer/addPeer', () => {
@@ -16,11 +16,16 @@ describe('Route peer/addPeer', () => {
       routeTest.node.peerNetwork.peerManager.peerCandidates.has('ws://testhost:9037'),
     ).toBe(true)
 
-    const matchingPeers = [...routeTest.node.peerNetwork.peerManager.peers()].filter(
-      (p) => formatWebSocketAddress(p.wsAddress) === 'ws://testhost:9037',
-    )
+    const matchingPeer =
+      routeTest.node.peerNetwork.peerManager.peerCandidates.get('ws://testhost:9037')
 
-    expect(matchingPeers.length).toBe(1)
+    expect(matchingPeer).toBeDefined()
+    Assert.isNotUndefined(matchingPeer)
+
+    expect(matchingPeer.wsAddress).toMatchObject({
+      host: 'testhost',
+      port: 9037,
+    })
 
     expect(response.content).toMatchObject({
       added: true,
