0663596839cf2795480c65d09258cb761b437533
Derek Guenther <derek@iflabs.network>
2023-11-20 13:53:33 -0500

    Revert to jest-jasmine2 and fix CI

--- config/jest.config.base.js
+++ config/jest.config.base.js
@@ -5,5 +5,8 @@ module.exports = {
   coverageProvider: "v8",
   coverageReporters: ["text-summary", "json", "clover", "text"],
   testPathIgnorePatterns: [".*\\.test\\.slow\\.ts$", ".*\\.test\\.perf\\.ts$"],
+  // TODO: Reconfiguring legacy defaults, we want to upgrade these eventually
+  // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
+  testRunner: "jest-jasmine2", // TODO: Remove `jest-jasmine2` dependency if we remove this
   fakeTimers: { legacyFakeTimers: true },
 };
