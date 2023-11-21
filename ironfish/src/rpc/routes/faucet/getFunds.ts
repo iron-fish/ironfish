c7201d131872efe20a5b824852a5a7f9c6217ed5
Hugh Cunningham <hugh@iflabs.network>
2023-11-21 10:38:13 -0800

    removes WebApi from sdk
    removing the service commands from ironfish-cli removes all but two of the
    WebApi methods in the sdk: a method for submitting telemetry and a method to
    support the 'faucet/getFunds' RPC
    
    moving the logic for these methods out to their clients allows us to remove the
    WebApi module altogether
    
    removing the WebApi module decouples the faucet api from the telemetry api

--- ironfish/src/rpc/routes/faucet/getFunds.ts
+++ ironfish/src/rpc/routes/faucet/getFunds.ts
@@ -1,10 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { AxiosError } from 'axios'
+import axios, { AxiosError } from 'axios'
 import * as yup from 'yup'
 import { Assert } from '../../../assert'
-import { WebApi } from '../../../webApi'
 import { ERROR_CODES, ResponseError } from '../../adapters'
 import { ApiNamespace } from '../namespaces'
 import { routes } from '../router'
@@ -40,40 +39,58 @@ routes.register<typeof GetFundsRequestSchema, GetFundsResponse>(
 
     const account = getAccount(node.wallet, request.data.account)
 
-    const api = new WebApi({
-      getFundsEndpoint: node.config.get('getFundsApi'),
-    })
+    const getFundsEndpoint = node.config.get('getFundsApi')
 
-    const response = await api
-      .getFunds({
-        email: request.data.email,
-        public_key: account.publicAddress,
-      })
-      .catch((error: AxiosError<{ code: string; message?: string }>) => {
-        if (error.response) {
-          const { data, status } = error.response
+    const response = await getFunds(getFundsEndpoint, {
+      email: request.data.email,
+      public_key: account.publicAddress,
+    }).catch((error: AxiosError<{ code: string; message?: string }>) => {
+      if (error.response) {
+        const { data, status } = error.response
 
-          if (status === 422) {
-            if (data.code === 'faucet_max_requests_reached') {
-              Assert.isNotUndefined(data.message)
-              throw new ResponseError(data.message, ERROR_CODES.VALIDATION, status)
-            }
-
-            throw new ResponseError(
-              'You entered an invalid email.',
-              ERROR_CODES.VALIDATION,
-              status,
-            )
-          } else if (data.message) {
-            throw new ResponseError(data.message, ERROR_CODES.ERROR, status)
+        if (status === 422) {
+          if (data.code === 'faucet_max_requests_reached') {
+            Assert.isNotUndefined(data.message)
+            throw new ResponseError(data.message, ERROR_CODES.VALIDATION, status)
           }
-        }
 
-        throw new ResponseError(error.message, ERROR_CODES.ERROR, Number(error.code))
-      })
+          throw new ResponseError(
+            'You entered an invalid email.',
+            ERROR_CODES.VALIDATION,
+            status,
+          )
+        } else if (data.message) {
+          throw new ResponseError(data.message, ERROR_CODES.ERROR, status)
+        }
+      }
+
+      throw new ResponseError(error.message, ERROR_CODES.ERROR, Number(error.code))
+    })
 
     request.end({
       id: response.id.toString(),
     })
   },
 )
+
+type GetFundsApiRequest = {
+  email: string | undefined
+  public_key: string
+}
+
+type GetFundsApiResponse = {
+  id: number
+  object: 'faucet_transaction'
+  public_key: string
+  completed_at: number | null
+  started_at: number | null
+}
+
+async function getFunds(
+  endpoint: string,
+  request: GetFundsApiRequest,
+): Promise<GetFundsApiResponse> {
+  const response = await axios.post<GetFundsApiResponse>(endpoint, request)
+
+  return response.data
+}
