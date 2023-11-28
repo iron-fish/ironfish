d8e67917d973c06ba7ec5560419981ae61ade8ad
Jason Spafford <nullprogrammer@gmail.com>
2023-11-27 16:40:40 -0800

    Remove Wallet Node from SDK (#4426)
    * Remove WalletNode from SDK
    
    This is now moved entirely into the standalone CLI codebase.
    
    * Fixed broken util

--- ironfish/src/utils/node.ts
+++ ironfish/src/utils/node.ts
@@ -4,13 +4,12 @@
 
 import { FullNode } from '../node'
 import { DatabaseIsLockedError } from '../storage/database/errors'
-import { WalletNode } from '../walletNode'
 import { PromiseUtils } from './promise'
 
 /**
  * Try to open the node DB's and wait until they can be opened
  */
-async function waitForOpen(node: IronfishNode, abort?: null | (() => boolean)): Promise<void> {
+async function waitForOpen(node: FullNode, abort?: null | (() => boolean)): Promise<void> {
   let logged = false
 
   while (!abort || !abort()) {
@@ -36,5 +35,3 @@ async function waitForOpen(node: IronfishNode, abort?: null | (() => boolean)): 
 }
 
 export const NodeUtils = { waitForOpen }
-
-export type IronfishNode = FullNode | WalletNode
