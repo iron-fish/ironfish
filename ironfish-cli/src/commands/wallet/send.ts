4fd517b7a204802d21bb59e987a2b557cd5463d9
Rahul Patni <patnir@users.noreply.github.com>
2023-11-29 17:35:28 -0500

    adding ability to select notes in the transaction send command (#4465)
    * adding ability to select notes in the transaction send command
    
    * change notes to note

--- ironfish-cli/src/commands/wallet/send.ts
+++ ironfish-cli/src/commands/wallet/send.ts
@@ -90,6 +90,11 @@ export class Send extends IronfishCommand {
       default: false,
       description: 'Allow offline transaction creation',
     }),
+    note: Flags.string({
+      char: 'n',
+      description: 'The note hashes to include in the transaction',
+      multiple: true,
+    }),
   }
 
   async start(): Promise<void> {
@@ -189,6 +194,7 @@ export class Send extends IronfishCommand {
       feeRate: flags.feeRate ? CurrencyUtils.encode(flags.feeRate) : null,
       expiration: flags.expiration,
       confirmations: flags.confirmations,
+      notes: flags.note,
     }
 
     let raw: RawTransaction
