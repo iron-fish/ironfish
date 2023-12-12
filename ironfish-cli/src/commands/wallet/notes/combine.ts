15100f3ef12a0f4cc6d8231316eea83445742a6c
Rahul Patni <rahul@iflabs.network>
2023-12-12 12:39:59 -0500

    slightly reducing expiration time

--- ironfish-cli/src/commands/wallet/notes/combine.ts
+++ ironfish-cli/src/commands/wallet/notes/combine.ts
@@ -433,7 +433,7 @@ export class CombineNotesCommand extends IronfishCommand {
     const averageBlockTimeInMs = timeLastFiveBlocksInMs / 5
 
     const expiration = Math.ceil(
-      currentBlockSequence + (spendPostTimeInMs * numberOfNotes * 2) / averageBlockTimeInMs,
+      currentBlockSequence + (spendPostTimeInMs * numberOfNotes * 1.5) / averageBlockTimeInMs, // * 1.5 added to account for the time it takes to calculate fees
     )
 
     return expiration
