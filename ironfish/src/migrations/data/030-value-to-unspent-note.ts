d94d64fa8ce210d0ddb597139f5e22291f4f0295
Rahul Patni <rahul@iflabs.network>
2024-01-10 10:23:59 -0800

    adding migration

---
+++ ironfish/src/migrations/data/030-value-to-unspent-note.ts
@@ -0,0 +1,72 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { Logger } from '../../logger'
+import { IDatabase, IDatabaseTransaction } from '../../storage'
+import { createDB } from '../../storage/utils'
+import { Account } from '../../wallet'
+import { Database, Migration, MigrationContext } from '../migration'
+import { GetStores } from './030-value-to-unspent-note/stores'
+
+export class Migration030 extends Migration {
+  path = __filename
+  database = Database.WALLET
+
+  prepare(context: MigrationContext): IDatabase {
+    return createDB({ location: context.config.walletDatabasePath })
+  }
+
+  async forward(
+    context: MigrationContext,
+    db: IDatabase,
+    tx: IDatabaseTransaction | undefined,
+    logger: Logger,
+  ): Promise<void> {
+    const stores = GetStores(db)
+
+    const accounts = []
+
+    for await (const accountValue of stores.old.accounts.getAllValuesIter()) {
+      accounts.push(
+        new Account({
+          ...accountValue,
+          createdAt: null,
+          walletDb: context.wallet.walletDb,
+        }),
+      )
+    }
+
+    logger.info(`Indexing notes and sorting them by value for ${accounts.length} accounts`)
+
+    for (const account of accounts) {
+      let decryptedNotes = 0
+
+      logger.info(` Indexing notes and sorting them by value for account ${account.name}`)
+      for await (const [[, noteHash], note] of stores.old.decryptedNotes.getAllIter(
+        undefined,
+        account.prefixRange,
+      )) {
+        if (note.sequence === null || note.spent) {
+          continue
+        }
+
+        await stores.new.valueToUnspentNoteHash.put(
+          [account.prefix, [note.note.assetId(), [note.note.value(), noteHash]]],
+          null,
+        )
+        decryptedNotes++
+      }
+
+      logger.info(
+        ` Indexed ${decryptedNotes} notes and sorted by value for account ${account.name}`,
+      )
+    }
+  }
+
+  async backward(context: MigrationContext, db: IDatabase): Promise<void> {
+    const stores = GetStores(db)
+
+    await stores.new.valueToUnspentNoteHash.clear()
+  }
+}
---
+++ ironfish/src/migrations/data/030-value-to-unspent-note/old/AccountValue.ts
@@ -0,0 +1,84 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { PUBLIC_ADDRESS_LENGTH } from '@ironfish/rust-nodejs'
+import bufio from 'bufio'
+import { IDatabaseEncoding } from '../../../../storage'
+
+const KEY_LENGTH = 32
+const VIEW_KEY_LENGTH = 64
+const VERSION_LENGTH = 2
+
+export interface AccountValue {
+  version: number
+  id: string
+  name: string
+  spendingKey: string | null
+  viewKey: string
+  incomingViewKey: string
+  outgoingViewKey: string
+  publicAddress: string
+}
+
+export class AccountValueEncoding implements IDatabaseEncoding<AccountValue> {
+  serialize(value: AccountValue): Buffer {
+    const bw = bufio.write(this.getSize(value))
+    let flags = 0
+    flags |= Number(!!value.spendingKey) << 0
+    bw.writeU8(flags)
+    bw.writeU16(value.version)
+    bw.writeVarString(value.id, 'utf8')
+    bw.writeVarString(value.name, 'utf8')
+    if (value.spendingKey) {
+      bw.writeBytes(Buffer.from(value.spendingKey, 'hex'))
+    }
+    bw.writeBytes(Buffer.from(value.viewKey, 'hex'))
+    bw.writeBytes(Buffer.from(value.incomingViewKey, 'hex'))
+    bw.writeBytes(Buffer.from(value.outgoingViewKey, 'hex'))
+    bw.writeBytes(Buffer.from(value.publicAddress, 'hex'))
+
+    return bw.render()
+  }
+
+  deserialize(buffer: Buffer): AccountValue {
+    const reader = bufio.read(buffer, true)
+    const flags = reader.readU8()
+    const version = reader.readU16()
+    const hasSpendingKey = flags & (1 << 0)
+    const id = reader.readVarString('utf8')
+    const name = reader.readVarString('utf8')
+    const spendingKey = hasSpendingKey ? reader.readBytes(KEY_LENGTH).toString('hex') : null
+    const viewKey = reader.readBytes(VIEW_KEY_LENGTH).toString('hex')
+    const incomingViewKey = reader.readBytes(KEY_LENGTH).toString('hex')
+    const outgoingViewKey = reader.readBytes(KEY_LENGTH).toString('hex')
+    const publicAddress = reader.readBytes(PUBLIC_ADDRESS_LENGTH).toString('hex')
+
+    return {
+      version,
+      id,
+      name,
+      viewKey,
+      incomingViewKey,
+      outgoingViewKey,
+      spendingKey,
+      publicAddress,
+    }
+  }
+
+  getSize(value: AccountValue): number {
+    let size = 0
+    size += 1
+    size += VERSION_LENGTH
+    size += bufio.sizeVarString(value.id, 'utf8')
+    size += bufio.sizeVarString(value.name, 'utf8')
+    if (value.spendingKey) {
+      size += KEY_LENGTH
+    }
+    size += VIEW_KEY_LENGTH
+    size += KEY_LENGTH
+    size += KEY_LENGTH
+    size += PUBLIC_ADDRESS_LENGTH
+
+    return size
+  }
+}
