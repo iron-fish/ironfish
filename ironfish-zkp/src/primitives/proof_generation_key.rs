76a0a3eec2615a29f2e7510de62b57bc65e76d5d
Rahul Patni <rahul@iflabs.network>
2024-01-12 10:56:25 -0800

    initial serialize tests

--- ironfish-zkp/src/primitives/proof_generation_key.rs
+++ ironfish-zkp/src/primitives/proof_generation_key.rs
@@ -3,12 +3,12 @@ use jubjub::{Fr, SubgroupPoint};
 use zcash_primitives::sapling::ProofGenerationKey;
 
 pub trait ProofGenerationKeySerializable {
-    fn serialize(self) -> [u8; 64];
+    fn serialize(&self) -> [u8; 64];
     fn deserialize(bytes: [u8; 64]) -> Self;
 }
 
 impl ProofGenerationKeySerializable for ProofGenerationKey {
-    fn serialize(self) -> [u8; 64] {
+    fn serialize(&self) -> [u8; 64] {
         let mut proof_generation_key_bytes: [u8; 64] = [0; 64];
         proof_generation_key_bytes[0..32].copy_from_slice(&self.ak.to_bytes());
         proof_generation_key_bytes[32..].copy_from_slice(&self.nsk.to_bytes());
@@ -26,5 +26,51 @@ impl ProofGenerationKeySerializable for ProofGenerationKey {
         let nsk = Fr::from_bytes(&nsk_bytes).unwrap();
 
         ProofGenerationKey { ak, nsk }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::primitives::proof_generation_key::ProofGenerationKeySerializable;
+    use ff::Field;
+    use group::Group;
+    use jubjub;
+    use rand::{rngs::StdRng, SeedableRng};
+    use zcash_primitives::sapling::ProofGenerationKey; // Import the rand crate for generating random bytes
+
+    #[test]
+    fn test_serialize() {
+        let mut rng = StdRng::seed_from_u64(0);
+
+        let proof_generation_key = ProofGenerationKey {
+            ak: jubjub::SubgroupPoint::random(&mut rng),
+            nsk: jubjub::Fr::random(&mut rng),
+        };
+
+        let serialized_bytes = proof_generation_key.serialize();
+
+        assert_eq!(serialized_bytes.len(), 64);
+    }
+
+    #[test]
+    fn test_proof_generation_key_serializable_deserializable() {
+        let mut rng = StdRng::seed_from_u64(0);
+        let proof_generation_key = ProofGenerationKey {
+            ak: jubjub::SubgroupPoint::random(&mut rng),
+            nsk: jubjub::Fr::random(&mut rng),
+        };
+
+        let proof_generation_key_bytes = proof_generation_key.serialize();
+        let proof_generation_key_deserialized =
+            ProofGenerationKey::deserialize(proof_generation_key_bytes);
+
+        assert_eq!(
+            proof_generation_key.ak,
+            proof_generation_key_deserialized.ak
+        );
+        assert_eq!(
+            proof_generation_key.nsk,
+            proof_generation_key_deserialized.nsk
+        );
     }
 }
