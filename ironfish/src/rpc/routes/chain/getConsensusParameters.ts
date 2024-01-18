1497f22a840f3b7aec0a74d5b9a4274301bad8f7
Daniel Cogan <danield9tqh@gmail.com>
2024-01-18 16:30:51 -0700

    Add missing consensus params to chain/getConsensusParameters (#4566)

--- ironfish/src/rpc/routes/chain/getConsensusParameters.ts
+++ ironfish/src/rpc/routes/chain/getConsensusParameters.ts
@@ -3,21 +3,17 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as yup from 'yup'
 import { Assert } from '../../../assert'
+import { ActivationSequence, ConsensusParameters } from '../../../consensus/consensus'
 import { FullNode } from '../../../node'
 import { ApiNamespace } from '../namespaces'
 import { routes } from '../router'
 
-interface ConsensusParameters {
-  allowedBlockFuturesSeconds: number
-  genesisSupplyInIron: number
-  targetBlockTimeInSeconds: number
-  targetBucketTimeInSeconds: number
-  maxBlockSizeBytes: number
-  minFee: number
-}
-
 export type GetConsensusParametersRequest = Record<string, never> | undefined
-export type GetConsensusParametersResponse = ConsensusParameters
+export type GetConsensusParametersResponse = {
+  [K in keyof ConsensusParameters]: ConsensusParameters[K] extends number
+    ? ConsensusParameters[K]
+    : number | null
+}
 
 export const GetConsensusParametersRequestSchema: yup.MixedSchema<GetConsensusParametersRequest> =
   yup.mixed().oneOf([undefined] as const)
@@ -25,12 +21,15 @@ export const GetConsensusParametersRequestSchema: yup.MixedSchema<GetConsensusPa
 export const GetConsensusParametersResponseSchema: yup.ObjectSchema<GetConsensusParametersResponse> =
   yup
     .object({
-      allowedBlockFuturesSeconds: yup.number().defined(),
+      allowedBlockFutureSeconds: yup.number().defined(),
       genesisSupplyInIron: yup.number().defined(),
       targetBlockTimeInSeconds: yup.number().defined(),
       targetBucketTimeInSeconds: yup.number().defined(),
       maxBlockSizeBytes: yup.number().defined(),
       minFee: yup.number().defined(),
+      enableAssetOwnership: yup.number().nullable().defined(),
+      enforceSequentialBlockTime: yup.number().nullable().defined(),
+      enableFishHash: yup.number().nullable().defined(),
     })
     .defined()
 
@@ -39,17 +38,18 @@ routes.register<typeof GetConsensusParametersRequestSchema, GetConsensusParamete
   GetConsensusParametersRequestSchema,
   (request, node): void => {
     Assert.isInstanceOf(node, FullNode)
-    Assert.isNotNull(node.chain.consensus, 'no consensus parameters')
 
     const consensusParameters = node.chain.consensus.parameters
 
+    const neverToNull = (value: ActivationSequence): number | null => {
+      return value === 'never' ? null : value
+    }
+
     request.end({
-      allowedBlockFuturesSeconds: consensusParameters.allowedBlockFutureSeconds,
-      genesisSupplyInIron: consensusParameters.genesisSupplyInIron,
-      targetBlockTimeInSeconds: consensusParameters.targetBlockTimeInSeconds,
-      targetBucketTimeInSeconds: consensusParameters.targetBucketTimeInSeconds,
-      maxBlockSizeBytes: consensusParameters.maxBlockSizeBytes,
-      minFee: consensusParameters.minFee,
+      ...consensusParameters,
+      enableAssetOwnership: neverToNull(consensusParameters.enableAssetOwnership),
+      enforceSequentialBlockTime: neverToNull(consensusParameters.enforceSequentialBlockTime),
+      enableFishHash: neverToNull(consensusParameters.enableFishHash),
     })
   },
 )
