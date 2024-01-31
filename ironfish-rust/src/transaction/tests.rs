feb3f0ea0b7d0c0ed54d90297a13f5b9418f0fb0
Rahul Patni <rahul@iflabs.network>
2024-01-31 09:54:46 -0800

    replacing pgk with proof authorizing key

--- ironfish-rust/src/transaction/tests.rs
+++ ironfish-rust/src/transaction/tests.rs
@@ -242,7 +242,7 @@ fn test_proposed_transaction_build() {
 
     let unsigned_transaction = transaction
         .build(
-            spender_key.sapling_proof_generation_key(),
+            spender_key.proof_authorizing_key,
             spender_key.view_key().clone(),
             spender_key.outgoing_view_key().clone(),
             intended_fee,
@@ -685,7 +685,7 @@ fn test_sign_simple() {
     // build transaction, generate proofs
     let unsigned_transaction = transaction
         .build(
-            spender_key.sapling_proof_generation_key(),
+            spender_key.proof_authorizing_key,
             spender_key.view_key().clone(),
             spender_key.outgoing_view_key().clone(),
             1,
@@ -779,7 +779,7 @@ fn test_sign_frost() {
     // build UnsignedTransaction without signing
     let mut unsigned_transaction = transaction
         .build(
-            key_packages.proof_generation_key,
+            key_packages.proof_generation_key.nsk,
             key_packages.view_key,
             key_packages.outgoing_view_key,
             intended_fee,
