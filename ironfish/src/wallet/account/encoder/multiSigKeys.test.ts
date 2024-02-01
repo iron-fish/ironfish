0a40d284097bf40fc3de9f29bd59c9855bb6de02
Rohan Jadvani <rohan@iflabs.network>
2024-02-01 14:24:36 -0500

    test(ironfish): Add test for multisig encoder

---
+++ ironfish/src/wallet/account/encoder/multiSigKeys.test.ts
@@ -0,0 +1,46 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { MultiSigCoordinator, MultiSigSigner } from '../../interfaces/multiSigKeys'
+import { NullableMultiSigKeysEncoding } from './multiSigKeys'
+
+describe('multiSigKeys encoder', () => {
+  describe('with a defined signer value', () => {
+    it('serializes the value into a buffer and deserializes to the original value', () => {
+      const encoder = new NullableMultiSigKeysEncoding()
+
+      const value: MultiSigSigner = {
+        identifier: 'aaaaaa',
+        keyPackage: 'bbbb',
+        proofGenerationKey: 'cccc',
+      }
+      const buffer = encoder.serialize(value)
+      const deserializedValue = encoder.deserialize(buffer)
+      expect(deserializedValue).toEqual(value)
+    })
+  })
+
+  describe('with a defined coordinator value', () => {
+    it('serializes the value into a buffer and deserializes to the original value', () => {
+      const encoder = new NullableMultiSigKeysEncoding()
+
+      const value: MultiSigCoordinator = {
+        publicKeyPackage: 'aaaa',
+      }
+      const buffer = encoder.serialize(value)
+      const deserializedValue = encoder.deserialize(buffer)
+      expect(deserializedValue).toEqual(value)
+    })
+  })
+
+  describe('with an undefined value', () => {
+    it('serializes the value into a buffer and deserializes to the original value', () => {
+      const encoder = new NullableMultiSigKeysEncoding()
+
+      const value = undefined
+      const buffer = encoder.serialize(value)
+      const deserializedValue = encoder.deserialize(buffer)
+      expect(deserializedValue).toEqual(value)
+    })
+  })
+})
