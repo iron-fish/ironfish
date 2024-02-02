d3400424e49bfbee5341afc1b71b52db8ce9f1e9
Hugh Cunningham <hugh@iflabs.network>
2024-02-02 09:57:47 -0800

    adds test that buildTransaction RPC output can be signed

--- ironfish/src/rpc/routes/wallet/buildTransaction.test.ts
+++ ironfish/src/rpc/routes/wallet/buildTransaction.test.ts
@@ -2,8 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { UnsignedTransaction } from '@ironfish/rust-nodejs'
+import { Transaction } from '../../../primitives'
 import { RawTransactionSerde } from '../../../primitives/rawTransaction'
-import { useAccountFixture } from '../../../testUtilities'
+import { useAccountFixture, useMinerBlockFixture } from '../../../testUtilities'
 import { createRawTransaction } from '../../../testUtilities/helpers/transaction'
 import { createRouteTest } from '../../../testUtilities/routeTest'
 
@@ -25,6 +27,36 @@ describe('Route wallet/buildTransaction', () => {
 
     expect(response.status).toBe(200)
     expect(response.content.transaction).toBeDefined()
+  })
+
+  it('should produce output that can be signed', async () => {
+    const account = await useAccountFixture(routeTest.node.wallet, 'signingAccount')
+
+    const block = await useMinerBlockFixture(routeTest.node.chain, undefined, account)
+    await routeTest.node.chain.addBlock(block)
+    await routeTest.node.wallet.updateHead()
+
+    const rawTransaction = await createRawTransaction({
+      wallet: routeTest.node.wallet,
+      from: account,
+      expiration: 12345,
+    })
+
+    const response = await routeTest.client.wallet.buildTransaction({
+      transaction: RawTransactionSerde.serialize(rawTransaction).toString('hex'),
+      account: account.name,
+    })
+
+    expect(response.status).toBe(200)
+    expect(response.content.transaction).toBeDefined()
+
+    const unsignedTransaction = new UnsignedTransaction(
+      Buffer.from(response.content.transaction, 'hex'),
+    )
+    const signedTransaction = unsignedTransaction.sign(account.spendingKey)
+
+    const transaction = new Transaction(signedTransaction)
+    expect(transaction.expiration()).toEqual(rawTransaction.expiration)
   })
 
   it("should return an error if the transaction won't deserialize", async () => {
--- ironfish/src/wallet/wallet.ts
+++ ironfish/src/wallet/wallet.ts
@@ -4,7 +4,6 @@
 import {
   Asset,
   generateKey,
-  generateKeyFromPrivateKey,
   Note as NativeNote,
   UnsignedTransaction,
 } from '@ironfish/rust-nodejs'
