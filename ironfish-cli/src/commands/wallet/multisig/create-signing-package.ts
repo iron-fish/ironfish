3ad5e92a27738e79504cc548080ecc09bd8d8d96
Joe <joe@iflabs.network>
2024-02-05 09:35:01 -0800

    feat: create signing package cli

---
+++ ironfish-cli/src/commands/wallet/multisig/create-signing-package.ts
@@ -0,0 +1,49 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { Flags } from '@oclif/core'
+import { IronfishCommand } from '../../../command'
+import { RemoteFlags } from '../../../flags'
+
+export class CreateSigningPackage extends IronfishCommand {
+  static description = `Creates a signing package for a given signing session for a multisig account`
+  static hidden = true
+
+  static flags = {
+    ...RemoteFlags,
+    unsignedTransaction: Flags.string({
+      char: 'u',
+      description: 'The unsigned transaction for which the signing share will be created',
+      required: true,
+    }),
+    commitment: Flags.string({
+      char: 'c',
+      description:
+        'The signing commitments from participants to be used for creating the signing package',
+      required: true,
+      multiple: true,
+    }),
+  }
+
+  async start(): Promise<void> {
+    const { flags } = await this.parse(CreateSigningPackage)
+    const commitments = flags.commitment.map(
+      (c: string) =>
+        JSON.parse(c) as {
+          identifier: string
+          commitment: { hiding: string; binding: string }
+        },
+    )
+
+    const client = await this.sdk.connectRpc()
+
+    const signingShareResponse = await client.wallet.multisig.createSigningPackage({
+      unsignedTransaction: flags.unsignedTransaction,
+      commitments,
+    })
+
+    this.log(`Signing Package for commitments from ${commitments.length} participants:\n`)
+    this.log(signingShareResponse.content.signingPackage)
+  }
+}
