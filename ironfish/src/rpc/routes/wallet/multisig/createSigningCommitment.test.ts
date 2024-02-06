85fb937cad2892a368933bb13fb828a54537111d
Rahul Patni <patnir@users.noreply.github.com>
2024-02-06 11:51:44 -0800

    Rahul/ifl 2187 replace keypackage with account name input for (#4689)
    * accepts account name as input for createSigningCommitment
    
    * using AssertMultiSig
    
    * asserts that a trusted dealer cannot create a signing commitment
    
    * removing node client depedency

--- ironfish/src/rpc/routes/wallet/multisig/createSigningCommitment.test.ts
+++ ironfish/src/rpc/routes/wallet/multisig/createSigningCommitment.test.ts
@@ -2,17 +2,66 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { ParticipantSecret } from '@ironfish/rust-nodejs'
+import { createNodeTest } from '../../../../testUtilities'
 import { createRouteTest } from '../../../../testUtilities/routeTest'
+import { ACCOUNT_SCHEMA_VERSION } from '../../../../wallet'
 
 describe('Route wallet/multisig/createSigningCommitment', () => {
   const routeTest = createRouteTest()
+  createNodeTest()
 
-  it('should error on invalid keypackage', async () => {
-    const keyPackage = 'invalid key package'
-    const request = { keyPackage, seed: 0 }
+  it('should error on account that does not exist', async () => {
+    const account = 'invalid account'
+    const request = { account, seed: 0 }
     await expect(
-      routeTest.client.request('wallet/multisig/createSigningCommitment', request).waitForEnd(),
-    ).rejects.toThrow('InvalidData')
+      routeTest.client.wallet.multisig.createSigningCommitment(request),
+    ).rejects.toThrow(
+      expect.objectContaining({
+        message: expect.stringContaining(` No account with name invalid account`),
+        status: 400,
+      }),
+    )
+  })
+
+  it('cannot perform signing commitment if the account is a trusted dealer', async () => {
+    const participants = Array.from({ length: 3 }, () => ({
+      identifier: ParticipantSecret.random().toIdentity().toFrostIdentifier(),
+    }))
+
+    const request = { minSigners: 2, participants }
+
+    const trustedDealerPackage = (
+      await routeTest.client.wallet.multisig.createTrustedDealerKeyPackage(request)
+    ).content
+
+    const importAccountRequest = {
+      name: 'td',
+      account: {
+        name: 'td',
+        version: ACCOUNT_SCHEMA_VERSION,
+        viewKey: trustedDealerPackage.viewKey,
+        incomingViewKey: trustedDealerPackage.incomingViewKey,
+        outgoingViewKey: trustedDealerPackage.outgoingViewKey,
+        publicAddress: trustedDealerPackage.publicAddress,
+        spendingKey: null,
+        createdAt: null,
+        multiSigKeys: {
+          publicKeyPackage: trustedDealerPackage.publicKeyPackage,
+        },
+        proofAuthorizingKey: null,
+      },
+    }
+
+    const importAccountResponse = await routeTest.client.wallet.importAccount(
+      importAccountRequest,
+    )
+
+    await expect(
+      routeTest.client.wallet.multisig.createSigningCommitment({
+        account: importAccountResponse.content.name,
+        seed: 420,
+      }),
+    ).rejects.toThrow()
   })
 
   it('should create signing commitment', async () => {
@@ -21,20 +70,41 @@ describe('Route wallet/multisig/createSigningCommitment', () => {
     }))
 
     const request = { minSigners: 2, participants }
-    const response = await routeTest.client.wallet.multisig.createTrustedDealerKeyPackage(
-      request,
+
+    const trustedDealerPackage = (
+      await routeTest.client.wallet.multisig.createTrustedDealerKeyPackage(request)
+    ).content
+
+    const importAccountRequest = {
+      name: 'participant1',
+      account: {
+        name: 'participant1',
+        version: ACCOUNT_SCHEMA_VERSION,
+        viewKey: trustedDealerPackage.viewKey,
+        incomingViewKey: trustedDealerPackage.incomingViewKey,
+        outgoingViewKey: trustedDealerPackage.outgoingViewKey,
+        publicAddress: trustedDealerPackage.publicAddress,
+        spendingKey: null,
+        createdAt: null,
+        multiSigKeys: {
+          keyPackage: trustedDealerPackage.keyPackages[0].keyPackage,
+          identifier: trustedDealerPackage.keyPackages[0].identifier,
+          publicKeyPackage: trustedDealerPackage.publicKeyPackage,
+        },
+        proofAuthorizingKey: null,
+      },
+    }
+
+    const importAccountResponse = await routeTest.client.wallet.importAccount(
+      importAccountRequest,
     )
 
-    const trustedDealerPackage = response.content
+    const response = await routeTest.client.wallet.multisig.createSigningCommitment({
+      account: importAccountResponse.content.name,
+      seed: 420,
+    })
 
-    const signingCommitment = await routeTest.client
-      .request('wallet/multisig/createSigningCommitment', {
-        keyPackage: trustedDealerPackage.keyPackages[0].keyPackage,
-        seed: 420,
-      })
-      .waitForEnd()
-
-    expect(signingCommitment.content).toMatchObject({
+    expect(response.content).toMatchObject({
       hiding: expect.any(String),
       binding: expect.any(String),
     })
