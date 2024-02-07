40074d4001022062a32df1844a859ea8f0fa455a
Rahul Patni <patnir@users.noreply.github.com>
2024-02-07 08:25:45 -0800

    feat(cli,ironfish): Add `wallet:multisig:sign` (#4685)
    * starting sign cli command
    
    * basic signing share signing functionality
    
    * leaving todos with open ended questions
    
    * WIP
    
    * Implement broadcast / accepted
    
    * Fix flag description
    
    * Update description
    
    * Fix account character
    
    * Update error
    
    * PR feedback
    
    * Make account optional
    
    * Make account optional
    
    * Update description
    
    * Fix ux action
    
    ---------
    
    Co-authored-by: Rohan Jadvani <rohan@hey.com>

--- ironfish/src/rpc/routes/wallet/multisig/aggregateSignatureShares.ts
+++ ironfish/src/rpc/routes/wallet/multisig/aggregateSignatureShares.ts
@@ -3,6 +3,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { UnsignedTransaction } from '@ironfish/rust-nodejs'
 import * as yup from 'yup'
+import { Transaction } from '../../../../primitives/transaction'
 import { AssertMultisig } from '../../../../wallet'
 import { ApiNamespace } from '../../namespaces'
 import { routes } from '../../router'
@@ -10,20 +11,23 @@ import { AssertHasRpcContext } from '../../rpcContext'
 import { getAccount } from '../utils'
 
 export type AggregateSignatureSharesRequest = {
-  account: string
+  account?: string
   unsignedTransaction: string
   signingPackage: string
   signatureShares: Array<string>
+  broadcast?: boolean
 }
 
 export type AggregateSignatureSharesResponse = {
+  accepted: boolean
+  broadcasted: boolean
   transaction: string
 }
 
 export const AggregateSignatureSharesRequestSchema: yup.ObjectSchema<AggregateSignatureSharesRequest> =
   yup
     .object({
-      account: yup.string().defined(),
+      account: yup.string().optional(),
       unsignedTransaction: yup.string().defined(),
       signingPackage: yup.string().defined(),
       signatureShares: yup.array(yup.string().defined()).defined(),
@@ -33,6 +37,8 @@ export const AggregateSignatureSharesRequestSchema: yup.ObjectSchema<AggregateSi
 export const AggregateSignatureSharesResponseSchema: yup.ObjectSchema<AggregateSignatureSharesResponse> =
   yup
     .object({
+      accepted: yup.boolean().defined(),
+      broadcasted: yup.boolean().defined(),
       transaction: yup.string().defined(),
     })
     .defined()
@@ -40,7 +46,7 @@ export const AggregateSignatureSharesResponseSchema: yup.ObjectSchema<AggregateS
 routes.register<typeof AggregateSignatureSharesRequestSchema, AggregateSignatureSharesResponse>(
   `${ApiNamespace.wallet}/multisig/aggregateSignatureShares`,
   AggregateSignatureSharesRequestSchema,
-  (request, node): void => {
+  async (request, node): Promise<void> => {
     AssertHasRpcContext(request, node, 'wallet')
     const account = getAccount(node.wallet, request.data.account)
     AssertMultisig(account)
@@ -48,14 +54,26 @@ routes.register<typeof AggregateSignatureSharesRequestSchema, AggregateSignature
     const unsigned = new UnsignedTransaction(
       Buffer.from(request.data.unsignedTransaction, 'hex'),
     )
-    const transaction = unsigned.aggregateSignatureShares(
+    const serialized = unsigned.aggregateSignatureShares(
       account.multisigKeys.publicKeyPackage,
       request.data.signingPackage,
       request.data.signatureShares,
     )
+    const transaction = new Transaction(serialized)
+
+    let accepted = false
+    let broadcasted = false
+
+    if (request.data.broadcast) {
+      const result = await node.wallet.broadcastTransaction(transaction)
+      accepted = result.accepted
+      broadcasted = result.broadcasted
+    }
 
     request.end({
-      transaction: transaction.toString('hex'),
+      accepted,
+      broadcasted,
+      transaction: serialized.toString('hex'),
     })
   },
 )
