1a3d4674d861362b921d619bfc57010b24ed1529
Rahul Patni <rahul@iflabs.network>
2024-02-13 12:38:49 -0800

    fixing import

--- ironfish-cli/src/commands/wallet/multisig/create-signature-share.ts
+++ ironfish-cli/src/commands/wallet/multisig/create-signature-share.ts
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import { SigningPackageEncoder } from '@ironfish/sdk/src/wallet/account/encoder/signingPackage'
+import { SigningPackageEncoder } from '@ironfish/sdk'
 import { CliUx, Flags } from '@oclif/core'
 import { IronfishCommand } from '../../../command'
 import { RemoteFlags } from '../../../flags'
--- ironfish-cli/src/commands/wallet/multisig/create-signing-package.ts
+++ ironfish-cli/src/commands/wallet/multisig/create-signing-package.ts
@@ -2,10 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-import {
-  SigningPackage,
-  SigningPackageEncoder,
-} from '@ironfish/sdk/src/wallet/account/encoder/signingPackage'
+import { SigningPackage, SigningPackageEncoder } from '@ironfish/sdk'
 import { Flags } from '@oclif/core'
 import { IronfishCommand } from '../../../command'
 import { RemoteFlags } from '../../../flags'
