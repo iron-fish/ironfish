7338d7bd8f8249497409d63dd73ab8b564cdede0
Hugh Cunningham <hugh@iflabs.network>
2024-02-15 14:36:05 -0800

    verify that commitments all come from known identities
    adds account parameter to wallet/multisig/createSigningPackage
    
    updates cli command with account flag
    
    before creating signing package, verifies that the identity on each commitment
    passed to createSigningPackage matches an identity in the set of all
    identities for the group
    
    uses getParticipantIdentities to build the set of identities
    
    adds unit test

--- ironfish/src/rpc/routes/wallet/multisig/createSigningPackage.test.ts
+++ ironfish/src/rpc/routes/wallet/multisig/createSigningPackage.test.ts
@@ -1,10 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { ParticipantSecret } from '@ironfish/rust-nodejs'
+import { createSigningCommitment, ParticipantSecret } from '@ironfish/rust-nodejs'
+import { Assert } from '../../../../assert'
 import { useAccountAndAddFundsFixture, useUnsignedTxFixture } from '../../../../testUtilities'
 import { createRouteTest } from '../../../../testUtilities/routeTest'
 import { ACCOUNT_SCHEMA_VERSION } from '../../../../wallet'
+import { RpcRequestError } from '../../../clients'
 
 describe('Route multisig/createSigningPackage', () => {
   const routeTest = createRouteTest()
@@ -68,6 +70,7 @@ describe('Route multisig/createSigningPackage', () => {
     )
 
     const responseSigningPackage = await routeTest.client.wallet.multisig.createSigningPackage({
+      account: 'participant1',
       commitments,
       unsignedTransaction,
     })
@@ -75,5 +78,97 @@ describe('Route multisig/createSigningPackage', () => {
     expect(responseSigningPackage.content).toMatchObject({
       signingPackage: expect.any(String),
     })
+  })
+
+  it('should verify commitment identities', async () => {
+    // create a multisig group and import an account
+    const participant1 = ParticipantSecret.random().toIdentity()
+    const participant2 = ParticipantSecret.random().toIdentity()
+    const keyRequest1 = {
+      minSigners: 2,
+      participants: [
+        { identity: participant1.serialize().toString('hex') },
+        { identity: participant2.serialize().toString('hex') },
+      ],
+    }
+
+    const package1 = (
+      await routeTest.client.wallet.multisig.createTrustedDealerKeyPackage(keyRequest1)
+    ).content
+
+    const importAccountRequest = {
+      name: 'participant1',
+      account: {
+        name: 'participant1',
+        version: ACCOUNT_SCHEMA_VERSION,
+        viewKey: package1.viewKey,
+        incomingViewKey: package1.incomingViewKey,
+        outgoingViewKey: package1.outgoingViewKey,
+        publicAddress: package1.publicAddress,
+        spendingKey: null,
+        createdAt: null,
+        multisigKeys: {
+          keyPackage: package1.keyPackages[0].keyPackage,
+          identity: package1.keyPackages[0].identity,
+          publicKeyPackage: package1.publicKeyPackage,
+        },
+        proofAuthorizingKey: null,
+      },
+    }
+
+    await routeTest.client.wallet.importAccount(importAccountRequest)
+
+    // add participants 1 and 2 to address book for participant 1
+    const account1 = routeTest.wallet.getAccountByName('participant1')
+    Assert.isNotNull(account1)
+    await routeTest.wallet.walletDb.addParticipantIdentity(account1, participant1.serialize())
+    await routeTest.wallet.walletDb.addParticipantIdentity(account1, participant2.serialize())
+
+    // create a transaction for the signing package
+    const txAccount = await useAccountAndAddFundsFixture(routeTest.wallet, routeTest.chain)
+    const unsignedTransaction = await useUnsignedTxFixture(
+      routeTest.wallet,
+      txAccount,
+      txAccount,
+    )
+
+    // create a second multisig group
+    const participant3 = ParticipantSecret.random().toIdentity()
+    const participant4 = ParticipantSecret.random().toIdentity()
+    const keyRequest2 = {
+      minSigners: 2,
+      participants: [
+        { identity: participant3.serialize().toString('hex') },
+        { identity: participant4.serialize().toString('hex') },
+      ],
+    }
+
+    const package2 = (
+      await routeTest.client.wallet.multisig.createTrustedDealerKeyPackage(keyRequest2)
+    ).content
+
+    // include a commitment from participant 3, who is not in the first group
+    const commitments = [
+      createSigningCommitment(
+        participant1.serialize().toString('hex'),
+        package1.keyPackages[0].keyPackage,
+        unsignedTransaction.withReference((t) => t.hash()),
+        [participant1.serialize().toString('hex'), participant2.serialize().toString('hex')],
+      ),
+      createSigningCommitment(
+        participant3.serialize().toString('hex'),
+        package2.keyPackages[0].keyPackage,
+        unsignedTransaction.withReference((t) => t.hash()),
+        [participant1.serialize().toString('hex'), participant2.serialize().toString('hex')],
+      ),
+    ]
+
+    await expect(async () =>
+      routeTest.client.wallet.multisig.createSigningPackage({
+        account: 'participant1',
+        commitments,
+        unsignedTransaction: unsignedTransaction.serialize().toString('hex'),
+      }),
+    ).rejects.toThrow(RpcRequestError)
   })
 })
--- ironfish/src/rpc/routes/wallet/multisig/createSigningPackage.ts
+++ ironfish/src/rpc/routes/wallet/multisig/createSigningPackage.ts
@@ -1,14 +1,18 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { UnsignedTransaction } from '@ironfish/rust-nodejs'
+import { IDENTITY_LEN, UnsignedTransaction } from '@ironfish/rust-nodejs'
 import * as yup from 'yup'
+import { RpcValidationError } from '../../../adapters'
 import { ApiNamespace } from '../../namespaces'
 import { routes } from '../../router'
+import { AssertHasRpcContext } from '../../rpcContext'
+import { getAccount } from '../utils'
 
 export type CreateSigningPackageRequest = {
   unsignedTransaction: string
   commitments: Array<string>
+  account?: string
 }
 
 export type CreateSigningPackageResponse = {
@@ -20,6 +24,7 @@ export const CreateSigningPackageRequestSchema: yup.ObjectSchema<CreateSigningPa
     .object({
       unsignedTransaction: yup.string().defined(),
       commitments: yup.array(yup.string().defined()).defined(),
+      account: yup.string().optional(),
     })
     .defined()
 
@@ -33,10 +38,32 @@ export const CreateSigningPackageResponseSchema: yup.ObjectSchema<CreateSigningP
 routes.register<typeof CreateSigningPackageRequestSchema, CreateSigningPackageResponse>(
   `${ApiNamespace.wallet}/multisig/createSigningPackage`,
   CreateSigningPackageRequestSchema,
-  (request, _context): void => {
+  async (request, context): Promise<void> => {
+    AssertHasRpcContext(request, context, 'wallet')
+
     const unsignedTransaction = new UnsignedTransaction(
       Buffer.from(request.data.unsignedTransaction, 'hex'),
     )
+
+    const account = getAccount(context.wallet, request.data.account)
+
+    const identitySet = new Set()
+    for await (const identityBuffer of context.wallet.walletDb.getParticipantIdentities(
+      account,
+    )) {
+      const identity = identityBuffer.toString('hex')
+      identitySet.add(identity)
+    }
+
+    for (const commitment of request.data.commitments) {
+      const identity = commitment.slice(0, IDENTITY_LEN * 2)
+      if (!identitySet.has(identity)) {
+        throw new RpcValidationError(
+          `Received commitment from identity (${identity}) that is not part of the multsig group for account ${account.name}`,
+          400,
+        )
+      }
+    }
     const signingPackage = unsignedTransaction.signingPackage(request.data.commitments)
 
     request.end({
