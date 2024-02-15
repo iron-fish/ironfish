6eff3db924a323b0f1e3336a7af6dc5f68cea622
Rahul Patni <rahul@iflabs.network>
2024-02-15 14:07:11 -0800

    Creates a new signing package struct.
    - Adds unsignedTransaction to the signing package.
    - This new struct is passed between the rust/ typescript layer instead of the frost SigningPackage.
    - This change has also led to simplification on the RPC and CLI side for creating signature shares.
    - Removes the need to pass the transaction hash of the unsigned transaction

--- ironfish/src/rpc/routes/wallet/multisig/createSignatureShare.ts
+++ ironfish/src/rpc/routes/wallet/multisig/createSignatureShare.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { createSignatureShare, UnsignedTransaction } from '@ironfish/rust-nodejs'
+import { createSignatureShare } from '@ironfish/rust-nodejs'
 import * as yup from 'yup'
 import { AssertMultisigSigner } from '../../../../wallet'
 import { ApiNamespace } from '../../namespaces'
@@ -12,7 +12,6 @@ import { getAccount } from '../utils'
 export type CreateSignatureShareRequest = {
   account?: string
   signingPackage: string
-  unsignedTransaction: string // TODO make `unsignedTransaction` part of `signingPackage`
   signers: Array<{ identity: string }> // TODO make `signers` part of `signingPackage`
 }
 
@@ -25,7 +24,6 @@ export const CreateSignatureShareRequestSchema: yup.ObjectSchema<CreateSignature
     .object({
       account: yup.string().optional(),
       signingPackage: yup.string().defined(),
-      unsignedTransaction: yup.string().defined(),
       signers: yup
         .array(
           yup
@@ -54,15 +52,10 @@ routes.register<typeof CreateSignatureShareRequestSchema, CreateSignatureShareRe
     const account = getAccount(node.wallet, request.data.account)
     AssertMultisigSigner(account)
 
-    const unsigned = new UnsignedTransaction(
-      Buffer.from(request.data.unsignedTransaction, 'hex'),
-    )
     const signatureShare = createSignatureShare(
       account.multisigKeys.identity,
       account.multisigKeys.keyPackage,
       request.data.signingPackage,
-      unsigned.hash(),
-      unsigned.publicKeyRandomness(),
       request.data.signers.map((signer) => signer.identity),
     )
 
