0d84d097502d253b8d9aabf6b923d0946e1b26da
Rahul Patni <patnir@users.noreply.github.com>
2024-02-23 14:59:36 -0800

    Rahul/add unsigned transaction summary to create signing commitment (#4769)
    * moving renderUnsignedTransactionDetails
    
    * confirmation for createing signing commitment
    
    * sending logger to renderUnsignedTransactionDetails

--- ironfish-cli/src/commands/wallet/multisig/create-signing-commitment.ts
+++ ironfish-cli/src/commands/wallet/multisig/create-signing-commitment.ts
@@ -1,9 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Flags } from '@oclif/core'
+import { UnsignedTransaction } from '@ironfish/sdk'
+import { CliUx, Flags } from '@oclif/core'
 import { IronfishCommand } from '../../../command'
 import { RemoteFlags } from '../../../flags'
+import { longPrompt } from '../../../utils/longPrompt'
+import { renderUnsignedTransactionDetails } from '../../../utils/transaction'
 
 export class CreateSigningCommitmentCommand extends IronfishCommand {
   static description = 'Create a signing commitment from a participant for a given transaction'
@@ -20,28 +23,68 @@ export class CreateSigningCommitmentCommand extends IronfishCommand {
     unsignedTransaction: Flags.string({
       char: 'u',
       description: 'The unsigned transaction that needs to be signed',
-      required: true,
     }),
-    signerIdentity: Flags.string({
+    identity: Flags.string({
       char: 'i',
       description:
         'The identity of the participants that will sign the transaction (may be specified multiple times to add multiple signers)',
-      required: true,
       multiple: true,
+    }),
+    confirm: Flags.boolean({
+      default: false,
+      description: 'Confirm creating signature share without confirming',
     }),
   }
 
   async start(): Promise<void> {
     const { flags } = await this.parse(CreateSigningCommitmentCommand)
 
+    let identities = flags.identity
+    if (!identities || identities.length < 2) {
+      const input = await CliUx.ux.prompt('Enter the identities separated by commas', {
+        required: true,
+      })
+      identities = input.split(',')
+
+      if (identities.length < 2) {
+        this.error('Minimum number of identities must be at least 2')
+      }
+    }
+    identities = identities.map((i) => i.trim())
+
+    let unsignedTransactionInput = flags.unsignedTransaction?.trim()
+    if (!unsignedTransactionInput) {
+      unsignedTransactionInput = await longPrompt('Enter the unsigned transaction: ', {
+        required: true,
+      })
+    }
+
     const client = await this.sdk.connectRpc()
+    const unsignedTransaction = new UnsignedTransaction(
+      Buffer.from(unsignedTransactionInput, 'hex'),
+    )
+
+    await renderUnsignedTransactionDetails(
+      client,
+      unsignedTransaction,
+      flags.account,
+      this.logger,
+    )
+
+    if (!flags.confirm) {
+      const confirmed = await CliUx.ux.confirm('Confirm signing commitment creation (Y/N)')
+      if (!confirmed) {
+        this.error('Creating signing commitment aborted')
+      }
+    }
+
     const response = await client.wallet.multisig.createSigningCommitment({
       account: flags.account,
-      unsignedTransaction: flags.unsignedTransaction,
-      signers: flags.signerIdentity.map((identity) => ({ identity })),
+      unsignedTransaction: unsignedTransactionInput,
+      signers: identities.map((identity) => ({ identity })),
     })
 
-    this.log('Commitment:\n')
+    this.log('\nCommitment:\n')
     this.log(response.content.commitment)
   }
 }
