c49dce37ef68ddb2c0c10a81e2c8add025d4bfa2
Andrea <andrea@iflabs.network>
2024-02-27 16:36:57 -0800

    Add functionality to Base64JsonEncoder to encrypt multisig accounts
    Multisig accounts can be encrypted as follows:
    
    ```ts
    const encoder = new Base64JsonEncoder()
    encoder.encode(account, {
      encryptWith: { kind: 'MultisigIdentity', identity: ... })
    ```
    
    and decrypted as follows:
    
    ```ts
    decodeAccount(encodedAccount, { multisigSecret: ... })
    ```
    
    Base64 multisig encrypted accounts differ from regular base64 encoded
    accounts from the prefix: `ifaccount` vs `ifmsaccount`.

--- ironfish/src/wallet/account/encoder/encoder.ts
+++ ironfish/src/wallet/account/encoder/encoder.ts
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { ParticipantIdentity, ParticipantSecret } from '@ironfish/rust-nodejs'
 import { LanguageKey } from '../../../utils'
 import { AccountImport } from '../../walletdb/accountValue'
 
@@ -26,12 +27,32 @@ export enum AccountFormat {
   SpendingKey = 'SpendingKey',
 }
 
+export interface MultisigIdentityEncryption {
+  kind: 'MultisigIdentity'
+  identity: ParticipantIdentity | Buffer
+}
+
+// This is meant to be a tagged union type: `AccountEncryptionMethod = Method1 | Method2 | Method3 | ...`
+export type AccountEncryptionMethod = MultisigIdentityEncryption
+
+export function isMultisigIdentityEncryption(
+  method: AccountEncryptionMethod,
+): method is MultisigIdentityEncryption {
+  return 'kind' in method && method.kind === 'MultisigIdentity'
+}
+
 export type AccountEncodingOptions = {
   language?: LanguageKey
+  encryptWith?: AccountEncryptionMethod
 }
 
 export type AccountDecodingOptions = {
   name?: string
+  // It would have been nice to have a `wallet?: Wallet` field and let the
+  // encoders extract all the decryption information they needed from it, but
+  // sadly interacting with the wallet DB is an asynchronous operation, and
+  // decoders are all synchronous
+  multisigSecret?: ParticipantSecret | Buffer
 }
 
 export type AccountEncoder = {
