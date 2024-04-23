iron-fish/ironfish

commit 4ab45df032efa43456438db24d3f1dbe87eb31d5

Multisig: fetch participant identities from the public key package
When importing a multisig account, participant identities are stored in
two places:
1. the public key package (part of the account record in the walletdb)
2. on dedicated records in the walletdb

The #2 records can sometimes get lost. This is the case currently of
account rescans.

Because there is no future development scheduled that will benefit
from #2, it's easier to just ignore those records and instead rely only
on the public key package.

ironfish/src/wallet/wallet.test.slow.ts
@@ -1362,11 +1362,7 @@ describe('Wallet', () => {
       ...trustedDealerPackage,
     })
 
-    const storedIdentities: string[] = []
-    for await (const identity of node.wallet.walletDb.getParticipantIdentities(account)) {
-      storedIdentities.push(identity.toString('hex'))
-    }
-
+    const storedIdentities = account.getMultisigParticipantIdentities()
     expect(identities.sort()).toEqual(storedIdentities.sort())
   })
 })

ironfish/src/rpc/routes/wallet/multisig/createSignatureShare.test.ts
@@ -5,7 +5,7 @@ import { generateKey } from '@ironfish/rust-nodejs'
 import { Assert } from '../../../../assert'
 import { useAccountAndAddFundsFixture, useUnsignedTxFixture } from '../../../../testUtilities'
 import { createRouteTest } from '../../../../testUtilities/routeTest'
-import { ACCOUNT_SCHEMA_VERSION } from '../../../../wallet'
+import { ACCOUNT_SCHEMA_VERSION, AssertMultisig } from '../../../../wallet'
 
 describe('Route wallt/multisig/createSignatureShare', () => {
   const routeTest = createRouteTest()
@@ -108,14 +108,21 @@ describe('Route wallt/multisig/createSignatureShare', () => {
       })
     ).content.signingPackage
 
-    // Remove one participant from the participants store to simulate unknown signer
+    // Alter the public key package to replace one identity with another, so
+    // that we can later pretend that we created a signature share from an
+    // unknown identity
     const account = routeTest.wallet.getAccountByName(accountNames[0])
     Assert.isNotNull(account)
+    AssertMultisig(account)
 
-    await routeTest.wallet.walletDb.deleteParticipantIdentity(
-      account,
-      Buffer.from(participants[1].identity, 'hex'),
+    const fromIdentity = participants[1].identity
+    const toIdentity = participants[2].identity
+    account.multisigKeys.publicKeyPackage = account.multisigKeys.publicKeyPackage.replace(
+      fromIdentity,
+      toIdentity,
     )
+
+    await routeTest.wallet.walletDb.setAccount(account)
 
     // Attempt to create signature share
     await expect(

ironfish/src/rpc/routes/wallet/multisig/createSignatureShare.ts
@@ -4,7 +4,6 @@
 import { multisig } from '@ironfish/rust-nodejs'
 import { BufferSet } from 'buffer-map'
 import * as yup from 'yup'
-import { AsyncUtils } from '../../../../utils'
 import { AssertMultisigSigner } from '../../../../wallet'
 import { RpcValidationError } from '../../../adapters'
 import { ApiNamespace } from '../../namespaces'
@@ -39,7 +38,7 @@ export const CreateSignatureShareResponseSchema: yup.ObjectSchema<CreateSignatur
 routes.register<typeof CreateSignatureShareRequestSchema, CreateSignatureShareResponse>(
   `${ApiNamespace.wallet}/multisig/createSignatureShare`,
   CreateSignatureShareRequestSchema,
-  async (request, node): Promise<void> => {
+  (request, node) => {
     AssertHasRpcContext(request, node, 'wallet')
 
     const account = getAccount(node.wallet, request.data.account)
@@ -49,10 +48,7 @@ routes.register<typeof CreateSignatureShareRequestSchema, CreateSignatureShareRe
       Buffer.from(request.data.signingPackage, 'hex'),
     )
 
-    const participantIdentities = new BufferSet(
-      await AsyncUtils.materialize(node.wallet.walletDb.getParticipantIdentities(account)),
-    )
-
+    const participantIdentities = new BufferSet(account.getMultisigParticipantIdentities())
     for (const signer of signingPackage.signers()) {
       if (!participantIdentities.has(signer)) {
         throw new RpcValidationError(

ironfish/src/rpc/routes/wallet/multisig/getAccountIdentities.ts
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { multisig } from '@ironfish/rust-nodejs'
 import * as yup from 'yup'
 import { AssertMultisig } from '../../../../wallet'
 import { ApiNamespace } from '../../namespaces'
@@ -39,10 +38,9 @@ routes.register<typeof GetAccountIdentitiesRequestSchema, GetAccountIdentitiesRe
     const account = getAccount(context.wallet, request.data.account)
     AssertMultisig(account)
 
-    const publicKeyPackage = new multisig.PublicKeyPackage(
-      account.multisigKeys.publicKeyPackage,
-    )
-    const identities = publicKeyPackage.identities().map((identity) => identity.toString('hex'))
+    const identities = account
+      .getMultisigParticipantIdentities()
+      .map((identity) => identity.toString('hex'))
 
     request.end({ identities })
   },

ironfish/src/wallet/account/account.ts
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { multisig } from '@ironfish/rust-nodejs'
 import { Asset } from '@ironfish/rust-nodejs'
 import { BufferMap, BufferSet } from 'buffer-map'
 import MurmurHash3 from 'imurmurhash'
@@ -1281,6 +1282,12 @@ export class Account {
     }
 
     return notes
+  }
+
+  getMultisigParticipantIdentities(): Array<Buffer> {
+    AssertMultisig(this)
+    const publicKeyPackage = new multisig.PublicKeyPackage(this.multisigKeys.publicKeyPackage)
+    return publicKeyPackage.identities()
   }
 }
 

ironfish/src/wallet/wallet.ts
@@ -1640,16 +1640,6 @@ export class Wallet {
       } else {
         await account.updateHead(null, tx)
       }
-
-      if (account.multisigKeys) {
-        const publicKeyPackage = new multisig.PublicKeyPackage(
-          account.multisigKeys.publicKeyPackage,
-        )
-
-        for (const identity of publicKeyPackage.identities()) {
-          await this.walletDb.addParticipantIdentity(account, identity, tx)
-        }
-      }
     })
 
     this.accounts.set(account.id, account)

ironfish/src/wallet/walletdb/walletdb.test.ts
@@ -410,24 +410,4 @@ describe('WalletDB', () => {
       expect(storedSecret.secret).toEqualBuffer(serializedSecret)
     })
   })
-
-  describe('participantIdentities', () => {
-    it('should store participant identities for a multisig account', async () => {
-      const node = (await nodeTest.createSetup()).node
-      const walletDb = node.wallet.walletDb
-
-      const account = await useAccountFixture(node.wallet, 'multisig')
-
-      const identity = multisig.ParticipantSecret.random().toIdentity()
-
-      await walletDb.addParticipantIdentity(account, identity.serialize())
-
-      const storedIdentities = await AsyncUtils.materialize(
-        walletDb.getParticipantIdentities(account),
-      )
-
-      expect(storedIdentities.length).toEqual(1)
-      expect(storedIdentities[0]).toEqualBuffer(identity.serialize())
-    })
-  })
 })

ironfish/src/wallet/walletdb/walletdb.ts
@@ -1297,32 +1297,4 @@ export class WalletDB {
       yield value
     }
   }
-
-  async addParticipantIdentity(
-    account: Account,
-    identity: Buffer,
-    tx?: IDatabaseTransaction,
-  ): Promise<void> {
-    await this.participantIdentities.put([account.prefix, identity], { identity }, tx)
-  }
-
-  async deleteParticipantIdentity(
-    account: Account,
-    identity: Buffer,
-    tx?: IDatabaseTransaction,
-  ): Promise<void> {
-    await this.participantIdentities.del([account.prefix, identity], tx)
-  }
-
-  async *getParticipantIdentities(
-    account: Account,
-    tx?: IDatabaseTransaction,
-  ): AsyncGenerator<Buffer> {
-    for await (const [_, identity] of this.participantIdentities.getAllKeysIter(
-      tx,
-      account.prefixRange,
-    )) {
-      yield identity
-    }
-  }
 }

commit 2cf9068970d7ef90b8257d0bb0f982ff4c99fa3c

Fix `wallet.test.slow.ts` and `round3.test.ts`
`wallet.test.slow.ts` was broken by 4ab45df0, which inadvertently
removed a type conversion step in the test.

`round3.test.ts` was broken by merging 17b9d680 on top of 4ab45df0
without proper rebasing.

ironfish/src/wallet/wallet.test.slow.ts
@@ -1362,7 +1362,9 @@ describe('Wallet', () => {
       ...trustedDealerPackage,
     })
 
-    const storedIdentities = account.getMultisigParticipantIdentities()
+    const storedIdentities = account
+      .getMultisigParticipantIdentities()
+      .map((identity) => identity.toString('hex'))
     expect(identities.sort()).toEqual(storedIdentities.sort())
   })
 })

ironfish/src/rpc/routes/wallet/multisig/dkg/round3.test.ts
@@ -3,7 +3,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { Assert } from '../../../../../assert'
 import { createRouteTest } from '../../../../../testUtilities/routeTest'
-import { AsyncUtils } from '../../../../../utils'
 
 function removeOneElement<T>(array: Array<T>): Array<T> {
   const newArray = [...array]
@@ -93,17 +92,14 @@ describe('Route multisig/dkg/round3', () => {
 
     // Check that the imported accounts all know about other participants'
     // identities
-    const expectedIdentities = participants.map(({ identity }) => identity)
-    expectedIdentities.sort()
+    const expectedIdentities = participants.map(({ identity }) => identity).sort()
     for (const accountName of accountNames) {
       const account = routeTest.wallet.getAccountByName(accountName)
       Assert.isNotNull(account)
-      const knownIdentities = (
-        await AsyncUtils.materialize(
-          routeTest.wallet.walletDb.getParticipantIdentities(account),
-        )
-      ).map((identity) => identity.toString('hex'))
-      knownIdentities.sort()
+      const knownIdentities = account
+        .getMultisigParticipantIdentities()
+        .map((identity) => identity.toString('hex'))
+        .sort()
       expect(knownIdentities).toStrictEqual(expectedIdentities)
     }
   })