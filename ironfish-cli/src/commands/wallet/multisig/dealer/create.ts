938fdaed5c2ca25f7d9cf1b18c8aeb506cf59946
Hugh Cunningham <hugh@iflabs.network>
2024-04-24 10:50:02 -0700

    updates text of identities prompts
    clarifies which participant identities are expected when prompting for a list of
    identities on the command line in 'commitment:create', 'dealer:create', and
    'dkg:round1'

--- ironfish-cli/src/commands/wallet/multisig/dealer/create.ts
+++ ironfish-cli/src/commands/wallet/multisig/dealer/create.ts
@@ -38,9 +38,12 @@ export class MultisigCreateDealer extends IronfishCommand {
 
     let identities = flags.identity
     if (!identities || identities.length < 2) {
-      const input = await longPrompt('Enter the identities separated by commas', {
-        required: true,
-      })
+      const input = await longPrompt(
+        'Enter the identities of all participants separated by commas',
+        {
+          required: true,
+        },
+      )
       identities = input.split(',')
 
       if (identities.length < 2) {
