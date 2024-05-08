b6e793bccdc5d731648272b80c0ac6499f3aa17d
lovedret <lovedret@6block.com>
2024-05-08 08:39:41 +0800

    Fix: getBlocks test

--- ironfish/src/rpc/routes/chain/getBlocks.test.ts
+++ ironfish/src/rpc/routes/chain/getBlocks.test.ts
@@ -1,7 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Assert } from '../../../assert'
 import { useBlockWithTx, useMinerBlockFixture } from '../../../testUtilities'
 import { createRouteTest } from '../../../testUtilities/routeTest'
 import { RPC_ERROR_CODES } from '../../adapters'
@@ -34,13 +33,13 @@ describe('Route chain/getBlocks', () => {
         sequence: 1,
       },
     })
-    expect(blocks[0]).toMatchObject({
+    expect(blocks[1]).toMatchObject({
       block: {
         hash: hash1,
         sequence: 2,
       },
     })
-    expect(blocks[0]).toMatchObject({
+    expect(blocks[2]).toMatchObject({
       block: {
         hash: hash2,
         sequence: 3,
