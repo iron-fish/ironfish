5cdbb98735f41acbea8748796f3e3ea9ed53dcf2
Derek Guenther <derek@iflabs.network>
2024-05-08 16:30:18 -0400

    WIP Add Known Transactions

--- ironfish/src/rpc/clients/client.ts
+++ ironfish/src/rpc/clients/client.ts
@@ -5,6 +5,8 @@ import type { RpcResponse, RpcResponseEnded } from '../response'
 import type {
   AcceptTransactionRequest,
   AcceptTransactionResponse,
+  AddKnownTransactionsRequest,
+  AddKnownTransactionsResponse,
   AddPeerRequest,
   AddPeerResponse,
   AddTransactionRequest,
@@ -308,6 +310,15 @@ export abstract class RpcClient {
           ).waitForEnd()
         },
       },
+    },
+
+    addKnownTransactions: (
+      params: AddKnownTransactionsRequest,
+    ): Promise<RpcResponseEnded<AddKnownTransactionsResponse>> => {
+      return this.request<AddKnownTransactionsResponse>(
+        `${ApiNamespace.wallet}/addKnownTransactions`,
+        params,
+      ).waitForEnd()
     },
 
     getAccounts: (
