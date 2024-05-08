8fe0e9a40d5693be916c3e01b6c5bde8a90ce1fd
Derek Guenther <derek@iflabs.network>
2024-05-08 16:57:08 -0400

    Fix lints

--- ironfish-cli/src/commands/wallet/syncing.ts
+++ ironfish-cli/src/commands/wallet/syncing.ts
@@ -1,8 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { GENESIS_BLOCK_SEQUENCE } from '@ironfish/sdk'
-import { parseBoolean, parseNumber } from '../../args'
+import { parseBoolean } from '../../args'
 import { IronfishCommand } from '../../command'
 import { RemoteFlags } from '../../flags'
 
