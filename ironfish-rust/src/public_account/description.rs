8f83e9a38ac5b860c813b1ffc79cc24f08beb0fc
Joe <joe@iflabs.network>
2024-05-22 13:55:32 -0700

    review comments; require valid to be called during instantiation.

--- ironfish-rust/src/public_account/description.rs
+++ ironfish-rust/src/public_account/description.rs
@@ -5,7 +5,10 @@
 use ed25519_dalek::{Signature, Verifier, VerifyingKey};
 use std::io;
 
-use crate::{errors::{IronfishError, IronfishErrorKind}, PublicAddress};
+use crate::{
+    errors::{IronfishError, IronfishErrorKind},
+    PublicAddress,
+};
 
 use super::transfer::Transfer;
 
@@ -13,32 +16,53 @@ use super::transfer::Transfer;
 pub struct PublicAccountDescription {
     /// TODO(jwp): do we need a separate account address for later migration
     /// pub(crate) account_address: EthereumAddress
-    pub(crate) version: u8,
-    // Minimum number of required signers
-    pub(crate) threshold: i16,
+    version: u8,
+    // Minimum number of signers required to sign a message and be valid
+    min_signers: u16,
     // Signers of the public account
-    pub(crate) signers: Vec<VerifyingKey>,
+    signers: Vec<VerifyingKey>,
     // Signatures of the signers for a given message
-    pub(crate) signatures: Vec<Signature>,
+    signatures: Vec<Signature>,
     // asset transfers
-    pub(crate) transfers: Vec<Transfer>,
+    transfers: Vec<Transfer>,
     // address of the account
-    pub(crate) address: PublicAddress,
+    address: PublicAddress,
 }
 
 impl PublicAccountDescription {
+    pub fn new(
+        version: u8,
+        min_signers: u16,
+        signers: Vec<VerifyingKey>,
+        signatures: Vec<Signature>,
+        transfers: Vec<Transfer>,
+        address: PublicAddress,
+    ) -> Result<PublicAccountDescription, IronfishError> {
+        let description = Self {
+            version,
+            min_signers,
+            signers,
+            signatures,
+            address,
+            transfers,
+        };
+
+        description.valid()?;
+
+        Ok(description)
+    }
     pub fn read<R: io::Read>(mut reader: R) -> Result<Self, IronfishError> {
         let mut version_buf = [0; 1];
         reader.read_exact(&mut version_buf)?;
         let version = version_buf[0];
-        
-        let mut threshold_buf = [0; 2];
-        reader.read_exact(&mut threshold_buf)?;
-        let threshold = i16::from_le_bytes(threshold_buf);
+
+        let mut min_signers_buf = [0; 2];
+        reader.read_exact(&mut min_signers_buf)?;
+        let min_signers = u16::from_le_bytes(min_signers_buf);
 
         let mut signers_len_buf = [0; 2];
         reader.read_exact(&mut signers_len_buf)?;
-        let signers_len = i16::from_le_bytes(signers_len_buf) as usize;
+        let signers_len = u16::from_le_bytes(signers_len_buf) as usize;
 
         let mut signers = Vec::with_capacity(signers_len);
         for _ in 0..signers_len {
@@ -51,7 +75,7 @@ impl PublicAccountDescription {
 
         let mut signatures_len_buf = [0; 2];
         reader.read_exact(&mut signatures_len_buf)?;
-        let signatures_len = i16::from_le_bytes(signatures_len_buf) as usize;
+        let signatures_len = u16::from_le_bytes(signatures_len_buf) as usize;
 
         let mut signatures = Vec::with_capacity(signatures_len);
         for _ in 0..signatures_len {
@@ -63,7 +87,7 @@ impl PublicAccountDescription {
 
         let mut transfers_len_buf = [0; 2];
         reader.read_exact(&mut transfers_len_buf)?;
-        let transfers_len = i16::from_le_bytes(transfers_len_buf) as usize;
+        let transfers_len = u16::from_le_bytes(transfers_len_buf) as usize;
 
         let mut transfers = Vec::with_capacity(transfers_len);
         for _ in 0..transfers_len {
@@ -72,38 +96,39 @@ impl PublicAccountDescription {
         }
         let address = PublicAddress::read(&mut reader)?;
 
-
-        Ok(Self {
+        let description = Self {
             version,
-            threshold,
+            min_signers,
             signers,
             signatures,
             address,
             transfers,
-        })
+        };
+        description.valid()?;
+        Ok(description)
     }
 
     pub fn write<W: io::Write>(&self, mut writer: W) -> Result<(), IronfishError> {
         // TODO: think about ideal ordering here
         writer.write_all(&self.version.to_le_bytes())?;
-        
-        writer.write_all(&self.threshold.to_le_bytes())?;
 
-        let signers_len = self.signers.len() as i16;
+        writer.write_all(&self.min_signers.to_le_bytes())?;
+
+        let signers_len: u16 = self.signers.len().try_into()?;
         writer.write_all(&signers_len.to_le_bytes())?;
 
         for signer in &self.signers {
             writer.write_all(signer.as_bytes())?;
         }
 
-        let signatures_len = self.signatures.len() as i16;
+        let signatures_len: u16 = self.signatures.len().try_into()?;
         writer.write_all(&signatures_len.to_le_bytes())?;
 
         for signature in &self.signatures {
             writer.write_all(&signature.to_bytes())?;
         }
 
-        let transfers_len = self.transfers.len() as i16;
+        let transfers_len: u16 = self.transfers.len().try_into()?;
         writer.write_all(&transfers_len.to_le_bytes())?;
 
         for transfer in &self.transfers {
@@ -115,16 +140,16 @@ impl PublicAccountDescription {
         Ok(())
     }
 
-    pub fn valid(&self) -> Result<(), IronfishError> {
-        if self.threshold < 1 {
+    fn valid(&self) -> Result<(), IronfishError> {
+        if self.min_signers < 1 {
             return Err(IronfishError::new(IronfishErrorKind::InvalidThreshold));
         }
 
-        if self.signers.len() < self.threshold as usize {
+        if self.signers.len() < self.min_signers as usize {
             return Err(IronfishError::new(IronfishErrorKind::InvalidThreshold));
         }
 
-        if self.signatures.len() < self.threshold as usize {
+        if self.signatures.len() < self.min_signers as usize {
             return Err(IronfishError::new(IronfishErrorKind::InvalidData));
         }
 
@@ -134,8 +159,13 @@ impl PublicAccountDescription {
     pub fn verify(&self) -> Result<(), IronfishError> {
         self.valid()?;
 
-        let hash =
-            &PublicAccountDescription::hash(&self.version, &self.threshold, &self.signers, &self.address, &self.transfers)?;
+        let hash = &PublicAccountDescription::hash(
+            &self.version,
+            &self.min_signers,
+            &self.signers,
+            &self.address,
+            &self.transfers,
+        )?;
         let is_valid = self
             .signers
             .iter()
@@ -149,7 +179,7 @@ impl PublicAccountDescription {
 
     pub fn hash(
         version: &u8,
-        threshold: &i16,
+        threshold: &u16,
         signers: &Vec<VerifyingKey>,
         address: &PublicAddress,
         transfers: &Vec<Transfer>,
@@ -166,6 +196,30 @@ impl PublicAccountDescription {
         }
         hasher.update(&address.public_address());
         Ok(hasher.finalize().into())
+    }
+
+    pub fn version(&self) -> u8 {
+        self.version
+    }
+
+    pub fn min_signers(&self) -> u16 {
+        self.min_signers
+    }
+
+    pub fn signers(&self) -> &Vec<VerifyingKey> {
+        &self.signers
+    }
+
+    pub fn signatures(&self) -> &Vec<Signature> {
+        &self.signatures
+    }
+
+    pub fn transfers(&self) -> &Vec<Transfer> {
+        &self.transfers
+    }
+
+    pub fn address(&self) -> &PublicAddress {
+        &self.address
     }
 }
 
@@ -190,18 +244,26 @@ mod tests {
             to: public_address,
             memo: PublicMemo([0; 256]),
         };
-        let hash = PublicAccountDescription::hash(&1, &1, &vec![verifying_key], &public_address, &vec![transfer])
-            .expect("Should successfully hash");
+        let hash = PublicAccountDescription::hash(
+            &1,
+            &1,
+            &vec![verifying_key],
+            &public_address,
+            &vec![transfer],
+        )
+        .expect("Should successfully hash");
         let signature = signing_key.sign(&hash);
 
-        let original = PublicAccountDescription {
-            version: 1,
-            threshold: 1,
-            signers: vec![verifying_key],
-            signatures: vec![signature],
-            address: public_address,
-            transfers: vec![transfer],
-        };
+        let original = PublicAccountDescription::new(
+            1,
+            1,
+            vec![verifying_key],
+            vec![signature],
+            vec![transfer],
+            public_address,
+        )
+        .expect("Should successfully create description");
+
         original
             .verify()
             .expect("Should be valid/verified creation");
@@ -211,7 +273,7 @@ mod tests {
 
         let read = PublicAccountDescription::read(&buffer[..]).unwrap();
 
-        assert_eq!(original.threshold, read.threshold);
+        assert_eq!(original.min_signers, read.min_signers);
         assert_eq!(original.signers, read.signers);
         assert_eq!(original.signatures, read.signatures);
     }
