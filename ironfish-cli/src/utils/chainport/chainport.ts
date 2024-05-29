c5ee358bb1e585ccf1cbc4c730059158ebbf1296
Rahul Patni <patnir@users.noreply.github.com>
2024-05-29 12:47:26 -0700

    combining chainport transaction logic (#4992)
    * combining chainport transaction logic
    
    * Separating out config
    
    * linkt
    
    * simplifying incoming chainport transaction logic
    
    * simplifying transaction logic

--- ironfish-cli/src/utils/chainport/chainport.ts
+++ ironfish-cli/src/utils/chainport/chainport.ts
@@ -1,52 +1,16 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import {
-  Logger,
-  MAINNET,
-  RpcWalletNote,
-  RpcWalletTransaction,
-  TESTNET,
-  TransactionType,
-} from '@ironfish/sdk'
+import { Logger } from '@ironfish/sdk'
 import { CliUx } from '@oclif/core'
 import axios from 'axios'
-import { ChainportMemoMetadata } from './metadata'
+import { getNetworkConfig } from './config'
 import {
   ChainportBridgeTransaction,
   ChainportNetwork,
   ChainportTransactionStatus,
   ChainportVerifiedToken,
 } from './types'
-
-const config = {
-  [TESTNET.id]: {
-    endpoint: 'https://preprod-api.chainport.io',
-    outgoingAddresses: [
-      '06102d319ab7e77b914a1bd135577f3e266fd82a3e537a02db281421ed8b3d13',
-      'db2cf6ec67addde84cc1092378ea22e7bb2eecdeecac5e43febc1cb8fb64b5e5',
-      '3bE494deb669ff8d943463bb6042eabcf0c5346cf444d569e07204487716cb85',
-    ],
-    incomingAddresses: ['06102d319ab7e77b914a1bd135577f3e266fd82a3e537a02db281421ed8b3d13'],
-  },
-  [MAINNET.id]: {
-    endpoint: 'https://api.chainport.io',
-    outgoingAddresses: [],
-    incomingAddresses: [],
-  },
-}
-
-const getNetworkConfig = (networkId: number) => {
-  if (networkId !== TESTNET.id && networkId !== MAINNET.id) {
-    throw new Error(`Unsupported network ${networkId} for chainport`)
-  }
-
-  if (networkId === MAINNET.id) {
-    throw new Error(`Mainnet is not yet supported.`)
-  }
-
-  return config[networkId]
-}
 
 export const getChainportTransactionStatus = async (networkId: number, hash: string) => {
   const config = getNetworkConfig(networkId)
@@ -97,144 +61,6 @@ export const fetchBridgeTransactionDetails = async (
   } = await axios.get(url)
 
   return response.data
-}
-
-export const getIncomingBridgeTransactionDetails = (
-  networkId: number,
-  transaction: RpcWalletTransaction,
-  networks: { [key: string]: ChainportNetwork } | undefined = undefined,
-): {
-  isIncomingTransaction: boolean
-  details?: {
-    network: string
-    address: string
-  }
-} => {
-  if (
-    networkId !== TESTNET.id ||
-    transaction.type !== TransactionType.RECEIVE ||
-    !transaction.notes
-  ) {
-    return {
-      isIncomingTransaction: false,
-    }
-  }
-
-  const config = getNetworkConfig(networkId)
-
-  let bridgeNote: RpcWalletNote | undefined = undefined
-
-  for (const note of transaction.notes) {
-    const incomingAddresses = config.incomingAddresses
-    if (
-      note.sender.toLowerCase() !==
-      incomingAddresses.find((address) => address.toLowerCase() === note.sender.toLowerCase())
-    ) {
-      return {
-        isIncomingTransaction: false,
-      }
-    }
-    bridgeNote = note
-  }
-
-  if (!bridgeNote) {
-    return {
-      isIncomingTransaction: false,
-    }
-  }
-
-  if (!networks) {
-    return {
-      isIncomingTransaction: true,
-    }
-  }
-
-  const [sourceNetwork, address, _] = ChainportMemoMetadata.decode(
-    Buffer.from(bridgeNote?.memoHex).toString(),
-  )
-
-  if (!networks[sourceNetwork]) {
-    return {
-      isIncomingTransaction: true,
-    }
-  }
-
-  return {
-    isIncomingTransaction: true,
-    details: {
-      network: networks[sourceNetwork].name || 'Unknown',
-      address: address,
-    },
-  }
-}
-
-export const getOutgoingBridgeTransactionDetails = (
-  networkId: number,
-  transaction: RpcWalletTransaction,
-  networks: { [key: string]: ChainportNetwork } | undefined = undefined,
-): {
-  isOutgoingTransaction: boolean
-  details?: {
-    network: string
-    address: string
-  }
-} => {
-  if (
-    networkId !== TESTNET.id ||
-    transaction.type !== TransactionType.SEND ||
-    !transaction.notes ||
-    transaction.notes.length < 2
-  ) {
-    return {
-      isOutgoingTransaction: false,
-    }
-  }
-
-  const config = getNetworkConfig(networkId)
-
-  const bridgeAddresses = config.outgoingAddresses.map((address) => address.toLowerCase())
-
-  const bridgeNote = transaction.notes.find((note) =>
-    bridgeAddresses.includes(note.owner.toLowerCase()),
-  )
-
-  if (!bridgeNote) {
-    return {
-      isOutgoingTransaction: false,
-    }
-  }
-
-  const feeNote = transaction.notes.find((note) => note.memo === '{"type": "fee_payment"}')
-
-  if (!feeNote) {
-    return {
-      isOutgoingTransaction: false,
-    }
-  }
-
-  if (!networks) {
-    return {
-      isOutgoingTransaction: true,
-    }
-  }
-
-  const [sourceNetwork, address, _] = ChainportMemoMetadata.decode(
-    Buffer.from(bridgeNote.memoHex).toString(),
-  )
-
-  if (!networks[sourceNetwork]) {
-    return {
-      isOutgoingTransaction: true,
-    }
-  }
-
-  return {
-    isOutgoingTransaction: true,
-    details: {
-      network: networks[sourceNetwork].name || 'Unknown',
-      address: address,
-    },
-  }
 }
 
 export const showChainportTransactionSummary = async (
