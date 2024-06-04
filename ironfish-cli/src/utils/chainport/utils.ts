abee6bbb8cfa8435e5f74c5c1b04234325d5b877
Rahul Patni <rahul@iflabs.network>
2024-06-04 09:52:49 -0700

    Adds utlitity functions to extract chainport data from a transaction

---
+++ ironfish-cli/src/utils/chainport/utils.ts
@@ -0,0 +1,86 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+import { RpcWalletTransaction, TransactionType } from '@ironfish/sdk'
+import { getConfig } from './config'
+import { ChainportMemoMetadata } from './metadata'
+
+type ChainportTransactionData =
+  | {
+      type: TransactionType.SEND | TransactionType.RECEIVE
+      chainportNetworkId: number
+      address: string
+    }
+  | undefined
+
+export const extractChainportDataFromTransaction = (
+  networkId: number,
+  transaction: RpcWalletTransaction,
+): ChainportTransactionData => {
+  const config = getConfig(networkId)
+
+  if (transaction.type === TransactionType.SEND) {
+    return getOutgoingChainportTransactionData(transaction, config)
+  }
+  if (transaction.type === TransactionType.RECEIVE) {
+    return getIncomingChainportTransactionData(transaction, config)
+  }
+  return undefined
+}
+
+const getIncomingChainportTransactionData = (
+  transaction: RpcWalletTransaction,
+  config: { incomingAddresses: string[] },
+): ChainportTransactionData => {
+  const bridgeNote = transaction.notes?.[0]
+
+  if (!bridgeNote || !isAddressInList(bridgeNote.sender, config.incomingAddresses)) {
+    return undefined
+  }
+
+  const [sourceNetwork, address, _] = ChainportMemoMetadata.decode(
+    Buffer.from(bridgeNote.memoHex).toString(),
+  )
+
+  return {
+    type: TransactionType.RECEIVE,
+    chainportNetworkId: sourceNetwork,
+    address: address,
+  }
+}
+
+const getOutgoingChainportTransactionData = (
+  transaction: RpcWalletTransaction,
+  config: { outgoingAddresses: string[] },
+): ChainportTransactionData => {
+  if (!transaction.notes || transaction.notes.length < 2) {
+    return undefined
+  }
+
+  if (!transaction.notes.find((note) => note.memo === '{"type": "fee_payment"}')) {
+    return undefined
+  }
+
+  const bridgeNote = transaction.notes.find((note) =>
+    isAddressInList(note.owner, config.outgoingAddresses),
+  )
+
+  if (!bridgeNote) {
+    return undefined
+  }
+
+  const [sourceNetwork, address, _] = ChainportMemoMetadata.decode(
+    Buffer.from(bridgeNote.memoHex).toString(),
+  )
+
+  return {
+    type: TransactionType.SEND,
+    chainportNetworkId: sourceNetwork,
+    address: address,
+  }
+}
+
+const isAddressInList = (address: string, addressList: string[]): boolean => {
+  return addressList.some((addr) => addr.toLowerCase() === address.toLowerCase())
+}
