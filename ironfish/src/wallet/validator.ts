d412ed1dc2a7812e8aa29167e8e4db67879b468c
Jason Spafford <nullprogrammer@gmail.com>
2024-06-11 12:32:13 -0700

    Rename function to be more clear (#5035)
    This function isn't checking if you have allowed characters, its
    specifically checking if a string is hex encoded. This makes it more
    clear.

--- ironfish/src/wallet/validator.ts
+++ ironfish/src/wallet/validator.ts
@@ -18,21 +18,15 @@ export function isValidPublicAddress(publicAddress: string): boolean {
 }
 
 export function isValidSpendingKey(spendingKey: string): boolean {
-  return spendingKey.length === SPENDING_KEY_LENGTH && haveAllowedCharacters(spendingKey)
+  return spendingKey.length === SPENDING_KEY_LENGTH && isHexEncoding(spendingKey)
 }
 
 export function isValidIncomingViewKey(incomingViewKey: string): boolean {
-  return (
-    incomingViewKey.length === INCOMING_VIEW_KEY_LENGTH &&
-    haveAllowedCharacters(incomingViewKey)
-  )
+  return incomingViewKey.length === INCOMING_VIEW_KEY_LENGTH && isHexEncoding(incomingViewKey)
 }
 
 export function isValidOutgoingViewKey(outgoingViewKey: string): boolean {
-  return (
-    outgoingViewKey.length === OUTGOING_VIEW_KEY_LENGTH &&
-    haveAllowedCharacters(outgoingViewKey)
-  )
+  return outgoingViewKey.length === OUTGOING_VIEW_KEY_LENGTH && isHexEncoding(outgoingViewKey)
 }
 
 export function isValidIVKAndPublicAddressPair(ivk: string, publicAddress: string): boolean {
@@ -40,7 +34,7 @@ export function isValidIVKAndPublicAddressPair(ivk: string, publicAddress: strin
 }
 
 export function isValidViewKey(viewKey: string): boolean {
-  return viewKey.length === VIEW_KEY_LENGTH && haveAllowedCharacters(viewKey)
+  return viewKey.length === VIEW_KEY_LENGTH && isHexEncoding(viewKey)
 }
 
 export function validateAccount(toImport: Partial<AccountValue>): void {
@@ -94,7 +88,7 @@ export function validateAccount(toImport: Partial<AccountValue>): void {
   }
 }
 
-function haveAllowedCharacters(text: string): boolean {
+function isHexEncoding(text: string): boolean {
   const validInputRegex = /^[0-9a-f]+$/
   return validInputRegex.exec(text.toLowerCase()) != null
 }
