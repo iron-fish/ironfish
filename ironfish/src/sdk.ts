396b057d23e964e207b6d1d9462d65d9d12d786e
Andrea <andrea@iflabs.network>
2024-06-12 14:49:11 -0700

    Remove the `fishHashContext` argument from `sdk.node()`
    This argument was unused.

--- ironfish/src/sdk.ts
+++ ironfish/src/sdk.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { BoxKeyPair, FishHashContext } from '@ironfish/rust-nodejs'
+import { BoxKeyPair } from '@ironfish/rust-nodejs'
 import {
   Config,
   ConfigOptions,
@@ -179,7 +179,6 @@ export class IronfishSdk {
   }: {
     autoSeed?: boolean
     privateIdentity?: PrivateIdentity
-    fishHashContext?: FishHashContext
     customNetworkPath?: string
     networkId?: number
   } = {}): Promise<FullNode> {
