96f40e2664d670cd45e4199d057cae270584e1d8
Hugh Cunningham <hugh@iflabs.network>
2024-06-18 16:11:22 -0700

    wip: adds prototype evm execution
    todo: generate private keys, generate addresses, sign transaction

--- ironfish-cli/src/commands/test-evm.ts
+++ ironfish-cli/src/commands/test-evm.ts
@@ -5,6 +5,7 @@ import { Account, Address, hexToBytes } from '@ethereumjs/util'
 import { IronfishEvm } from '@ironfish/sdk'
 import { IronfishCommand } from '../command'
 import { LocalFlags } from '../flags'
+import { LegacyTransaction } from '@ethereumjs/tx'
 
 export class TestEvmCommand extends IronfishCommand {
   static description = `Test adding EVM support to the Iron Fish network`
@@ -19,16 +20,24 @@ export class TestEvmCommand extends IronfishCommand {
 
     const address = new Address(hexToBytes('0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'))
 
-    let balance = (await evm.evm.stateManager.getAccount(address))?.balance ?? 0n
+    let balance = (await evm.stateManager.getAccount(address))?.balance ?? 0n
     this.log(`Account at address ${address.toString()} has balance ${balance}`)
 
     const account = new Account(BigInt(0), balance + 1000n)
 
-    await evm.evm.stateManager.checkpoint()
-    await evm.evm.stateManager.putAccount(address, account)
-    await evm.evm.stateManager.commit()
+    await evm.stateManager.checkpoint()
+    await evm.stateManager.putAccount(address, account)
+    await evm.stateManager.commit()
 
-    balance = (await evm.evm.stateManager.getAccount(address))?.balance ?? 0n
+    const tx = new LegacyTransaction({
+      to: '0x533C6179249a8A6306d22eF18512508810E42690',
+      value: 10n,
+    })
+
+    const result = await evm.runTx({ tx })
+    console.log(result)
+
+    balance = (await evm.stateManager.getAccount(address))?.balance ?? 0n
     this.log(`Account at address ${address.toString()} has balance ${balance}`)
 
     await node.closeDB()
--- ironfish/src/evm/evm.ts
+++ ironfish/src/evm/evm.ts
@@ -5,15 +5,18 @@ import { EVM } from '@ethereumjs/evm'
 import { DefaultStateManager } from '@ethereumjs/statemanager'
 import { Trie } from '@ethereumjs/trie'
 import { ValueEncoding } from '@ethereumjs/util'
+import { RunTxOpts, RunTxResult, VM } from '@ethereumjs/vm'
 import { BlockchainDB } from '../blockchain/database/blockchaindb'
 import { EvmBlockchain } from './blockchain'
 import { EvmStateDB } from './database'
 
 export class IronfishEvm {
-  evm: EVM
+  stateManager: DefaultStateManager
+  private vm: VM
 
-  constructor(evm: EVM) {
-    this.evm = evm
+  constructor(stateManager: DefaultStateManager, vm: VM) {
+    this.stateManager = stateManager
+    this.vm = vm
   }
 
   static async create(blockchainDb: BlockchainDB): Promise<IronfishEvm> {
@@ -28,6 +31,12 @@ export class IronfishEvm {
 
     const evm = await EVM.create({ blockchain, stateManager })
 
-    return new IronfishEvm(evm)
+    const vm = await VM.create({ evm, stateManager })
+
+    return new IronfishEvm(stateManager, vm)
+  }
+
+  async runTx(opts: RunTxOpts): Promise<RunTxResult> {
+    return this.vm.runTx(opts)
   }
 }
