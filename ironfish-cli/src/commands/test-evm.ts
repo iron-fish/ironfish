61c0a82a9da3847b7586bbb827d8be5b19d998d5
Hugh Cunningham <hugh@iflabs.network>
2024-06-25 13:11:27 -0700

    wip: adds example of resetting state root
    resetting the state root to a previous value resets account balances to their
    values at that state root

--- ironfish-cli/src/commands/test-evm.ts
+++ ironfish-cli/src/commands/test-evm.ts
@@ -1,8 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+// import { LegacyTransaction } from '@ethereumjs/tx'
 import { LegacyTransaction } from '@ethereumjs/tx'
-import { Account, Address } from '@ethereumjs/util'
+import { Account, Address, hexToBytes } from '@ethereumjs/util'
 import { generateKey } from '@ironfish/rust-nodejs'
 import { IronfishEvm } from '@ironfish/sdk'
 import { IronfishCommand } from '../command'
@@ -19,6 +20,8 @@ export class TestEvmCommand extends IronfishCommand {
 
     const evm = await IronfishEvm.create(node.chain.blockchainDb)
 
+    // const senderAddress = new Address(hexToBytes('0x0e5069fd80d59b92e359dade34f6a66f0bf8dcc5'))
+
     const senderKey = generateKey()
     const receipientKey = generateKey()
 
@@ -28,13 +31,24 @@ export class TestEvmCommand extends IronfishCommand {
     const senderAddress = Address.fromPrivateKey(senderPrivateKey)
     const recipientAddress = Address.fromPrivateKey(recipientPrivateKey)
 
-    const senderAccount = new Account(BigInt(0), 500000n)
+    let senderBalance = (await evm.stateManager.getAccount(senderAddress))?.balance ?? 0n
+    this.log(
+      `Sender account at address ${senderAddress.toString()} has balance ${senderBalance}`,
+    )
+
+    const senderAccount = new Account(BigInt(0), senderBalance + 500000n)
+
+    const oldStateRoot = await evm.stateManager.getStateRoot()
+    this.log(`Old state root: ${Buffer.from(oldStateRoot).toString('hex')}`)
 
     await evm.stateManager.checkpoint()
     await evm.stateManager.putAccount(senderAddress, senderAccount)
     await evm.stateManager.commit()
 
-    let senderBalance = (await evm.stateManager.getAccount(senderAddress))?.balance ?? 0n
+    let newStateRoot = await evm.stateManager.getStateRoot()
+    this.log(`New state root: ${Buffer.from(newStateRoot).toString('hex')}`)
+
+    senderBalance = (await evm.stateManager.getAccount(senderAddress))?.balance ?? 0n
     this.log(
       `Sender account at address ${senderAddress.toString()} has balance ${senderBalance}`,
     )
@@ -55,8 +69,20 @@ export class TestEvmCommand extends IronfishCommand {
     senderBalance = (await evm.stateManager.getAccount(senderAddress))?.balance ?? 0n
     this.log(`Sender at address ${recipientAddress.toString()} has balance ${senderBalance}`)
 
-    const recipientBalance =
-      (await evm.stateManager.getAccount(recipientAddress))?.balance ?? 0n
+    let recipientBalance = (await evm.stateManager.getAccount(recipientAddress))?.balance ?? 0n
+    this.log(
+      `Recipient at address ${recipientAddress.toString()} has balance ${recipientBalance}`,
+    )
+
+    newStateRoot = await evm.stateManager.getStateRoot()
+    this.log(`New state root: ${Buffer.from(newStateRoot).toString('hex')}`)
+
+    await evm.stateManager.setStateRoot(oldStateRoot)
+
+    senderBalance = (await evm.stateManager.getAccount(senderAddress))?.balance ?? 0n
+    this.log(`Sender at address ${recipientAddress.toString()} has balance ${senderBalance}`)
+
+    recipientBalance = (await evm.stateManager.getAccount(recipientAddress))?.balance ?? 0n
     this.log(
       `Recipient at address ${recipientAddress.toString()} has balance ${recipientBalance}`,
     )
