113e93528143d0eabe112a65e1053062fe5d468f
Rohan Jadvani <rohan@hey.com>
2024-07-09 00:45:31 -0400

    feat(cli): Add passphrase to wallet:accounts

--- ironfish-cli/src/commands/wallet/accounts.ts
+++ ironfish-cli/src/commands/wallet/accounts.ts
@@ -1,9 +1,10 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Flags } from '@oclif/core'
+import { Flags, ux } from '@oclif/core'
 import { IronfishCommand } from '../../command'
 import { RemoteFlags } from '../../flags'
+import { Assert } from '@ironfish/sdk'
 
 export class AccountsCommand extends IronfishCommand {
   static description = `List all the accounts on the node`
@@ -14,12 +15,36 @@ export class AccountsCommand extends IronfishCommand {
       default: false,
       description: `Display a hash of the account's read-only keys along with the account name`,
     }),
+    passphrase: Flags.string({
+      required: false,
+      description: 'Passphrase for wallet',
+    }),
+    timeout: Flags.integer({
+      required: false,
+      description: 'Timeout to unlock for wallet',
+    }),
   }
 
   async start(): Promise<void> {
     const { flags } = await this.parse(AccountsCommand)
 
     const client = await this.sdk.connectRpc()
+
+    let passphrase = flags.passphrase
+    const status = await client.wallet.getNodeStatus()
+    if (status.content.accounts.locked && !passphrase) {
+      passphrase = await ux.prompt('Enter your passphrase to unlock the wallet', {
+        required: true,
+      })
+    }
+
+    if (status.content.accounts.locked) {
+      Assert.isNotUndefined(passphrase)
+      await client.wallet.unlock({
+        passphrase,
+        timeout: flags.timeout,
+      })
+    }
 
     const response = await client.wallet.getAccounts({ displayName: flags.displayName })
 
