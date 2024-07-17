10afe84e1c0684ad5a58e1b50649b8c4a20b1d21
Hugh Cunningham <hugh@iflabs.network>
2024-07-17 14:11:42 -0700

    fixes lint

--- ironfish/src/evm/evm.ts
+++ ironfish/src/evm/evm.ts
@@ -1,12 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Block } from '@ethereumjs/block'
 import { EVM } from '@ethereumjs/evm'
 import { Address } from '@ethereumjs/util'
 import { RunTxOpts, RunTxResult, VM } from '@ethereumjs/vm'
 import { BlockchainDB } from '../blockchain/database/blockchaindb'
 import { EvmBlockchain } from './blockchain'
-import { Block } from '@ethereumjs/block'
 
 export class IronfishEvm {
   private vm: VM
