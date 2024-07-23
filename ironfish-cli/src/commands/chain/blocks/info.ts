7958acea123f5a7f426b56589cd685c5d8432054
Mat <mat@iflabs.network>
2024-07-22 17:48:37 -0700

    Add ability to use JSON flag with CLI commands
    Also implements it for the `chain:blocks:info` command

--- ironfish-cli/src/commands/chain/blocks/info.ts
+++ ironfish-cli/src/commands/chain/blocks/info.ts
@@ -1,9 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { BufferUtils, TimeUtils } from '@ironfish/sdk'
 import { Args } from '@oclif/core'
 import { IronfishCommand } from '../../../command'
 import { LocalFlags } from '../../../flags'
+import * as ui from '../../../ui'
 
 export default class BlockInfo extends IronfishCommand {
   static description = 'Show the block header of a requested hash or sequence'
@@ -19,13 +21,30 @@ export default class BlockInfo extends IronfishCommand {
     ...LocalFlags,
   }
 
-  async start(): Promise<void> {
+  static enableJsonFlag: boolean = true
+
+  async start(): Promise<unknown> {
     const { args } = await this.parse(BlockInfo)
     const { search } = args
 
     const client = await this.sdk.connectRpc()
     const data = await client.chain.getBlock({ search })
+    const blockData = data.content
 
-    this.log(JSON.stringify(data.content, undefined, '  '))
+    this.log(
+      ui.card({
+        Hash: blockData.block.hash,
+        Confirmed: blockData.metadata.confirmed,
+        Fork: !blockData.metadata.main,
+        Sequence: blockData.block.sequence,
+        'Previous Block Hash': blockData.block.previousBlockHash,
+        Difficulty: blockData.block.difficulty,
+        Timestamp: TimeUtils.renderString(blockData.block.timestamp),
+        Graffiti: BufferUtils.toHuman(Buffer.from(blockData.block.graffiti, 'hex')),
+        'Transaction Count': blockData.block.transactions.length,
+      }),
+    )
+
+    return blockData
   }
 }
