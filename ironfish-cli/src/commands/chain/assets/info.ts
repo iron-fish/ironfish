65127c63a459ca67f15135eaa665796ab07a1ad6
Mat <mat@iflabs.network>
2024-08-07 14:21:28 -0700

    Use placeholders to auto-populate examples

--- ironfish-cli/src/commands/chain/assets/info.ts
+++ ironfish-cli/src/commands/chain/assets/info.ts
@@ -15,7 +15,7 @@ export default class AssetInfo extends IronfishCommand {
     {
       description: 'show the native $IRON asset info',
       command:
-        'ironfish chain:assets:info 51f33a2f14f92735e562dc658a5639279ddca3d5079a6d1242b2a588a9cbf44c',
+        '<%= config.bin %> <%= command.id %> 51f33a2f14f92735e562dc658a5639279ddca3d5079a6d1242b2a588a9cbf44c',
     },
   ]
 
