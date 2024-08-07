773b880735e94c6a3744922fe4d60c5735ba1c0d
Jason Spafford <nullprogrammer@gmail.com>
2024-08-07 15:54:31 -0700

    WIP: Add accounting command

---
+++ ironfish-cli/src/commands/wallet/accounting.ts
@@ -0,0 +1,510 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Asset } from '@ironfish/rust-nodejs'
+import {
+  Assert,
+  CurrencyUtils,
+  GetAccountTransactionsResponse,
+  PartialRecursive,
+  RpcAsset,
+  RpcClient,
+  TransactionType,
+} from '@ironfish/sdk'
+import { Flags } from '@oclif/core'
+import { IronfishCommand } from '../../command'
+import { RemoteFlags } from '../../flags'
+import { table, TableColumns, TableFlags } from '../../ui'
+import { getAssetsByIDs } from '../../utils'
+import { extractChainportDataFromTransaction } from '../../utils/chainport'
+import { Format, TableCols } from '../../utils/table'
+
+const { sort: _, ...tableFlags } = TableFlags
+
+export class AccountingCommand extends IronfishCommand {
+  static description = `Display the account transactions`
+
+  static flags = {
+    ...RemoteFlags,
+    ...tableFlags,
+    account: Flags.string({
+      char: 'a',
+      description: 'Name of the account to get transactions for',
+    }),
+    hash: Flags.string({
+      char: 't',
+      description: 'Transaction hash to get details for',
+    }),
+    sequence: Flags.integer({
+      char: 's',
+      description: 'Block sequence to get transactions for',
+    }),
+    limit: Flags.integer({
+      description: 'Number of latest transactions to get details for',
+    }),
+    offset: Flags.integer({
+      description: 'Number of latest transactions to skip',
+    }),
+    confirmations: Flags.integer({
+      description: 'Number of block confirmations needed to confirm a transaction',
+    }),
+    notes: Flags.boolean({
+      default: false,
+      description: 'Include data from transaction output notes',
+    }),
+  }
+
+  async start(): Promise<void> {
+    const { flags } = await this.parse(AccountingCommand)
+
+    const format: Format =
+      flags.csv || flags.output === 'csv'
+        ? Format.csv
+        : flags.output === 'json'
+        ? Format.json
+        : Format.cli
+
+    const getAccountTransactions = async (
+      client: RpcClient,
+      account: string,
+      knownWallets: Map<string, string>,
+    ): Promise<PartialRecursive<TransactionRow>[]> => {
+      if (account === 'airdrop') {
+        return []
+      }
+
+      const results: PartialRecursive<TransactionRow>[] = []
+
+      const response = client.wallet.getAccountTransactionsStream({
+        account,
+        hash: flags.hash,
+        sequence: flags.sequence,
+        limit: flags.limit,
+        offset: flags.offset,
+        confirmations: flags.confirmations,
+        notes: flags.notes,
+      })
+
+      const networkId = (await client.chain.getNetworkInfo()).content.networkId
+
+      for await (const transaction of response.contentStream()) {
+        let transactionRows: PartialRecursive<TransactionRow>[]
+
+        if (flags.notes) {
+          Assert.isNotUndefined(transaction.notes)
+          const assetLookup = await getAssetsByIDs(
+            client,
+            transaction.notes.map((n) => n.assetId) || [],
+            account,
+            flags.confirmations,
+          )
+          if (extractChainportDataFromTransaction(networkId, transaction)) {
+            transaction.type =
+              transaction.type === TransactionType.SEND
+                ? ('Bridge (outgoing)' as TransactionType)
+                : ('Bridge (incoming)' as TransactionType)
+          }
+          transactionRows = this.getTransactionRowsByNote(
+            assetLookup,
+            transaction,
+            format,
+            knownWallets,
+          )
+        } else {
+          const assetLookup = await getAssetsByIDs(
+            client,
+            transaction.assetBalanceDeltas.map((d) => d.assetId),
+            account,
+            flags.confirmations,
+          )
+          transactionRows = this.getTransactionRows(
+            assetLookup,
+            transaction,
+            format,
+            knownWallets,
+          )
+        }
+
+        for (const row of transactionRows) {
+          results.push(row)
+        }
+      }
+
+      return results
+    }
+
+    const getAccountsTransactions = async (
+      client: RpcClient,
+    ): Promise<PartialRecursive<TransactionRow>[]> => {
+      const response = await client.wallet.getAccounts()
+
+      const knownWallets = new Map<string, string>()
+
+      for (const account of response.content.accounts) {
+        const publicKey = (await client.wallet.getAccountPublicKey({ account })).content
+          .publicKey
+        knownWallets.set(publicKey, account)
+        // console.log(`${account}=${publicKey}`)
+      }
+
+      const results: PartialRecursive<TransactionRow>[] = []
+
+      for (const account of response.content.accounts) {
+        for (const transaction of await getAccountTransactions(client, account, knownWallets)) {
+          results.push(transaction)
+        }
+      }
+
+      return results
+    }
+
+    const client = await this.connectRpc()
+    const columns = this.getColumns(flags.extended, flags.notes, true, format)
+
+    let transactions = await getAccountsTransactions(client)
+
+    transactions = transactions.filter(
+      (t) =>
+        !!t.timestamp &&
+        t.timestamp >= new Date('07/01/2024').valueOf() &&
+        t.timestamp <= new Date('08/01/2024').valueOf() &&
+        !!t.type &&
+        t.type === 'send' &&
+        !!t.to &&
+        !!t.from &&
+        t.to !== t.from,
+    )
+
+    if (!transactions.length) {
+      this.log('No transactions found')
+      this.exit(0)
+    }
+
+    table(transactions, columns, {
+      printLine: this.log.bind(this),
+      ...flags,
+    })
+  }
+
+  getTransactionRows(
+    assetLookup: { [key: string]: RpcAsset },
+    transaction: GetAccountTransactionsResponse,
+    format: Format,
+    knownWallets: Map<string, string>,
+  ): PartialRecursive<TransactionRow>[] {
+    const nativeAssetId = Asset.nativeId().toString('hex')
+
+    const assetBalanceDeltas = transaction.assetBalanceDeltas.sort((d) =>
+      d.assetId === nativeAssetId ? -1 : 1,
+    )
+
+    const feePaid = transaction.type === TransactionType.SEND ? BigInt(transaction.fee) : 0n
+
+    const transactionRows = []
+
+    let assetCount = assetBalanceDeltas.length
+
+    for (const [index, { assetId, delta }] of assetBalanceDeltas.entries()) {
+      const asset = assetLookup[assetId]
+      let amount = BigInt(delta)
+
+      if (assetId === Asset.nativeId().toString('hex')) {
+        if (transaction.type === TransactionType.SEND) {
+          amount += feePaid
+        }
+
+        // exclude the native asset in cli output if no amount was sent/received
+        // and it was not the only asset exchanged
+        if (format === Format.cli && amount === 0n && assetCount > 1) {
+          assetCount -= 1
+          continue
+        }
+      }
+
+      const group = this.getRowGroup(index, assetCount, transactionRows.length)
+
+      const transactionRow = {
+        group,
+        assetId,
+        assetName: asset.name,
+        amount,
+        assetDecimals: asset.verification.decimals,
+        assetSymbol: asset.verification.symbol,
+        from: undefined,
+        to: undefined,
+      }
+
+      // include full transaction details in first row or non-cli-formatted output
+      if (transactionRows.length === 0 || format !== Format.cli) {
+        transactionRows.push({
+          ...transaction,
+          ...transactionRow,
+          feePaid,
+        })
+      } else {
+        transactionRows.push(transactionRow)
+      }
+    }
+
+    return transactionRows
+  }
+
+  getTransactionRowsByNote(
+    assetLookup: { [key: string]: RpcAsset },
+    transaction: GetAccountTransactionsResponse,
+    format: Format,
+    knownWallets: Map<string, string>,
+  ): PartialRecursive<TransactionRow>[] {
+    Assert.isNotUndefined(transaction.notes)
+    const transactionRows = []
+
+    const nativeAssetId = Asset.nativeId().toString('hex')
+
+    const notes = transaction.notes.sort((n) => (n.assetId === nativeAssetId ? -1 : 1))
+
+    const noteCount = transaction.notes.length
+
+    const feePaid = transaction.type === TransactionType.SEND ? BigInt(transaction.fee) : 0n
+
+    for (const [index, note] of notes.entries()) {
+      const amount = BigInt(note.value)
+      const assetId = note.assetId
+      const assetName = assetLookup[note.assetId].name
+      const assetDecimals = assetLookup[note.assetId].verification.decimals
+      const assetSymbol = assetLookup[note.assetId].verification.symbol
+      const sender = note.sender
+      const recipient = note.owner
+      const from = knownWallets.get(sender) ?? sender
+      const to = knownWallets.get(recipient) ?? recipient
+      const memo = note.memo
+
+      const group = this.getRowGroup(index, noteCount, transactionRows.length)
+
+      // include full transaction details in first row or non-cli-formatted output
+      if (transactionRows.length === 0 || format !== Format.cli) {
+        transactionRows.push({
+          ...transaction,
+          group,
+          assetId,
+          assetName,
+          assetDecimals,
+          assetSymbol,
+          amount,
+          feePaid,
+          sender,
+          recipient,
+          memo,
+          from,
+          to,
+        })
+      } else {
+        transactionRows.push({
+          group,
+          assetId,
+          assetName,
+          assetDecimals,
+          assetSymbol,
+          amount,
+          sender,
+          recipient,
+          memo,
+          from,
+          to,
+        })
+      }
+    }
+
+    return transactionRows
+  }
+
+  getColumns(
+    extended: boolean,
+    notes: boolean,
+    accounting: boolean,
+    format: Format,
+  ): TableColumns<PartialRecursive<TransactionRow>> {
+    if (accounting) {
+      return {
+        timestamp: TableCols.timestamp(),
+        status: {
+          header: 'Status',
+          minWidth: 12,
+          get: (row) => row.status ?? '',
+        },
+        from: {
+          header: 'From',
+          minWidth: 24,
+          get: (row) => row.from ?? '',
+        },
+        to: {
+          header: 'To',
+          minWidth: 24,
+          get: (row) => row.to ?? '',
+        },
+        feePaid: {
+          header: 'Fee',
+          get: (row) =>
+            row.feePaid && row.feePaid !== 0n
+              ? CurrencyUtils.render(row.feePaid, true, row.assetId)
+              : '',
+        },
+        amount: {
+          header: 'Amount',
+          get: (row) => {
+            Assert.isNotUndefined(row.amount)
+            return CurrencyUtils.render(row.amount, true, row.assetId, {
+              decimals: row.assetDecimals,
+              symbol: row.assetSymbol,
+            })
+          },
+          minWidth: 16,
+        },
+        hash: {
+          header: 'Hash',
+          minWidth: 32,
+          get: (row) => row.hash ?? '',
+        },
+      }
+    }
+
+    let columns: TableColumns<PartialRecursive<TransactionRow>> = {
+      timestamp: TableCols.timestamp({
+        streaming: true,
+      }),
+      status: {
+        header: 'Status',
+        minWidth: 12,
+        get: (row) => row.status ?? '',
+      },
+      type: {
+        header: 'Type',
+        minWidth: notes ? 18 : 8,
+        get: (row) => row.type ?? '',
+      },
+      hash: {
+        header: 'Hash',
+        minWidth: 32,
+        get: (row) => row.hash ?? '',
+      },
+      notesCount: {
+        header: 'Notes',
+        minWidth: 5,
+        extended: true,
+        get: (row) => row.notesCount ?? '',
+      },
+      spendsCount: {
+        header: 'Spends',
+        minWidth: 5,
+        extended: true,
+        get: (row) => row.spendsCount ?? '',
+      },
+      mintsCount: {
+        header: 'Mints',
+        minWidth: 5,
+        extended: true,
+        get: (row) => row.mintsCount ?? '',
+      },
+      burnsCount: {
+        header: 'Burns',
+        minWidth: 5,
+        extended: true,
+        get: (row) => row.burnsCount ?? '',
+      },
+      expiration: {
+        header: 'Expiration',
+        get: (row) => row.expiration ?? '',
+      },
+      submittedSequence: {
+        header: 'Submitted Sequence',
+        get: (row) => row.submittedSequence ?? '',
+      },
+      feePaid: {
+        header: 'Fee Paid ($IRON)',
+        get: (row) =>
+          row.feePaid && row.feePaid !== 0n ? CurrencyUtils.render(row.feePaid) : '',
+      },
+      ...TableCols.asset({ extended, format }),
+      amount: {
+        header: 'Amount',
+        get: (row) => {
+          Assert.isNotUndefined(row.amount)
+          return CurrencyUtils.render(row.amount, false, row.assetId, {
+            decimals: row.assetDecimals,
+            symbol: row.assetSymbol,
+          })
+        },
+        minWidth: 16,
+      },
+    }
+
+    if (notes) {
+      columns = {
+        ...columns,
+        sender: {
+          header: 'Sender Address',
+          get: (row) => row.sender ?? '',
+        },
+        recipient: {
+          header: 'Recipient Address',
+          get: (row) => row.recipient ?? '',
+        },
+        memo: {
+          header: 'Memo',
+          get: (row) => row.memo ?? '',
+        },
+      }
+    }
+
+    if (format === Format.cli) {
+      columns = {
+        group: {
+          header: '',
+          minWidth: 3,
+          get: (row) => row.group ?? '',
+        },
+        ...columns,
+      }
+    }
+
+    return columns
+  }
+
+  getRowGroup(index: number, assetCount: number, assetRowCount: number): string {
+    if (assetCount > 1) {
+      if (assetRowCount === 0) {
+        return '┏'
+      } else if (assetRowCount > 0 && index < assetCount - 1) {
+        return '┣'
+      } else if (assetRowCount > 0 && index === assetCount - 1) {
+        return '┗'
+      }
+    }
+
+    return ''
+  }
+}
+
+type TransactionRow = {
+  group?: string
+  timestamp: number
+  status: string
+  type: string
+  hash: string
+  assetId: string
+  assetName: string
+  assetDecimals?: number
+  assetSymbol?: string
+  amount: bigint
+  feePaid?: bigint
+  notesCount: number
+  spendsCount: number
+  mintsCount: number
+  burnsCount: number
+  expiration: number
+  submittedSequence: number
+  sender: string
+  recipient: string
+  memo?: string
+  from?: string
+  to?: string
+}
