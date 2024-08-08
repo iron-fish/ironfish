e747def605f5d918931136ba2a152510e02f6679
Rohan Jadvani <rohan@iflabs.network>
2024-08-08 13:12:09 -0400

    chore(rust-nodejs): Update Napi JS index file

--- ironfish-rust-nodejs/index.d.ts
+++ ironfish-rust-nodejs/index.d.ts
@@ -48,6 +48,7 @@ export const TRANSACTION_FEE_LENGTH: number
 export const LATEST_TRANSACTION_VERSION: number
 export function verifyTransactions(serializedTransactions: Array<Buffer>): boolean
 export function encrypt(plaintext: string, passphrase: string): string
+export function decrypt(encryptedBlob: string, passphrase: string): string
 export const enum LanguageCode {
   English = 0,
   ChineseSimplified = 1,
