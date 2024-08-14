ff9408152e43a1e6e3e43455826cd71a6e42696b
Jason Spafford <nullprogrammer@gmail.com>
2024-08-14 15:21:40 -0700

    Change wallet:status to output aggregation

--- ironfish-cli/src/commands/wallet/status.ts
+++ ironfish-cli/src/commands/wallet/status.ts
@@ -1,63 +1,66 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import chalk from 'chalk'
+import { MathUtils, TimeUtils } from '@ironfish/sdk'
 import { IronfishCommand } from '../../command'
-import { RemoteFlags } from '../../flags'
-import { table, TableFlags } from '../../ui'
+import { JsonFlags, RemoteFlags } from '../../flags'
+import * as ui from '../../ui'
 
 export class StatusCommand extends IronfishCommand {
   static description = `Get status of all accounts`
+  static enableJsonFlag = true
 
   static flags = {
     ...RemoteFlags,
-    ...TableFlags,
+    ...JsonFlags,
   }
 
-  async start(): Promise<void> {
-    const { flags } = await this.parse(StatusCommand)
+  async start(): Promise<unknown> {
+    await this.parse(StatusCommand)
 
     const client = await this.connectRpc()
 
-    const response = await client.wallet.getAccountsStatus()
+    const [nodeStatus, walletStatus] = await Promise.all([
+      client.node.getStatus(),
+      client.wallet.getAccounts(),
+    ])
 
-    table(
-      response.content.accounts,
-      {
-        name: {
-          get: (row) => row.name,
-          header: 'Account Name',
-          minWidth: 11,
-        },
-        id: {
-          get: (row) => row.id,
-          header: 'Account ID',
-        },
-        viewOnly: {
-          get: (row) => row.viewOnly,
-          header: 'View Only',
-        },
-        headHash: {
-          get: (row) => row.head?.hash ?? 'NULL',
-          header: 'Head Hash',
-        },
-        headInChain: {
-          get: (row) => row.head?.inChain ?? 'NULL',
-          header: 'Head In Chain',
-        },
-        sequence: {
-          get: (row) => row.head?.sequence ?? 'NULL',
-          header: 'Head Sequence',
-        },
-        scanningEnabled: {
-          get: (row) => (row.scanningEnabled ? chalk.green('✓') : ''),
-          header: 'Scanning Enabled',
-        },
-      },
-      {
-        printLine: this.log.bind(this),
-        ...flags,
-      },
-    )
+    const status: Record<string, unknown> = {
+      Wallet: nodeStatus.content.accounts.enabled ? 'ENABLED' : 'DISABLED',
+      Accounts: walletStatus.content.accounts.length,
+      Head: nodeStatus.content.accounts.head.hash,
+      Sequence: nodeStatus.content.accounts.head.sequence,
+      Scanner: 'IDLE',
+    }
+
+    if (nodeStatus.content.accounts.scanning) {
+      const progress = MathUtils.round(
+        (nodeStatus.content.accounts.scanning.sequence /
+          nodeStatus.content.accounts.scanning.endSequence) *
+          100,
+        2,
+      )
+
+      const duration = Date.now() - nodeStatus.content.accounts.scanning.startedAt
+      const speed = MathUtils.round(nodeStatus.content.accounts.scanning.speed, 2)
+
+      status['Scanner'] = 'SCANNING'
+      status['Scan Progress'] = progress + '%'
+      status['Scan Speed'] = `${speed} B/s`
+      status['Scan Duration'] = TimeUtils.renderSpan(duration, {
+        hideMilliseconds: true,
+        forceSecond: true,
+      })
+      status[
+        'Scan Block'
+      ] = `${nodeStatus.content.accounts.scanning.sequence} -> ${nodeStatus.content.accounts.scanning.endSequence}`
+    }
+
+    this.log(ui.card(status))
+
+    return {
+      ...nodeStatus.content.accounts,
+      accountsCount: walletStatus.content.accounts.length,
+    }
   }
 }
