d270671fc5592df35a95cb728efe661f2f4b6d52
Jason Spafford <nullprogrammer@gmail.com>
2024-08-13 17:37:18 -0700

    Add table output to wallet command
    This adds optional new table output to the wallet command

--- ironfish-cli/src/commands/wallet/index.ts
+++ ironfish-cli/src/commands/wallet/index.ts
@@ -1,36 +1,76 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Flags } from '@oclif/core'
+import chalk from 'chalk'
 import { IronfishCommand } from '../../command'
-import { RemoteFlags } from '../../flags'
+import { JsonFlags, RemoteFlags } from '../../flags'
+import * as ui from '../../ui'
 
 export class AccountsCommand extends IronfishCommand {
   static description = `list accounts in the wallet`
+  static enableJsonFlag = true
 
   static hiddenAliases = ['wallet:accounts']
 
   static flags = {
     ...RemoteFlags,
-    displayName: Flags.boolean({
-      default: false,
-      description: `Display a hash of the account's read-only keys along with the account name`,
-    }),
+    ...JsonFlags,
+    ...ui.TableFlags,
   }
 
-  async start(): Promise<void> {
+  async start(): Promise<unknown> {
     const { flags } = await this.parse(AccountsCommand)
 
     const client = await this.connectRpc()
 
-    const response = await client.wallet.getAccounts({ displayName: flags.displayName })
+    const response = await client.wallet.getAccountsStatus()
 
     if (response.content.accounts.length === 0) {
       this.log('you have no accounts')
+      return []
     }
 
-    for (const name of response.content.accounts) {
-      this.log(name)
-    }
+    ui.table(
+      response.content.accounts,
+      {
+        name: {
+          get: (row) => row.name,
+          header: 'Account',
+          minWidth: 11,
+        },
+        viewOnly: {
+          get: (row) => (row.viewOnly ? chalk.green('✓') : ''),
+          header: 'View Only',
+          extended: true,
+        },
+        headInChain: {
+          get: (row) => (row.head?.inChain ? chalk.green('✓') : ''),
+          header: 'In Chain',
+          extended: true,
+        },
+        scanningEnabled: {
+          get: (row) => (row.scanningEnabled ? chalk.green('✓') : ''),
+          header: 'Scanning',
+          extended: true,
+        },
+        sequence: {
+          get: (row) => row.head?.sequence ?? '',
+          header: 'Sequence',
+          extended: true,
+        },
+        headHash: {
+          get: (row) => row.head?.hash ?? '',
+          header: 'Head',
+          extended: true,
+        },
+      },
+      {
+        ...flags,
+        printLine: this.log.bind(this),
+        'no-header': flags['no-header'] ?? !flags.extended,
+      },
+    )
+
+    return response.content.accounts
   }
 }
