386659bf1eff4af91ba21fcbb6770cc7089f27bc
Joe <joe@iflabs.network>
2024-08-20 09:19:54 -0700

    evm rpc wrapper

--- ironfish/src/rpc/routes/eth/sendRawTransaction.ts
+++ ironfish/src/rpc/routes/eth/sendRawTransaction.ts
@@ -7,6 +7,7 @@ import { Assert } from '../../../assert'
 import { GLOBAL_IF_ACCOUNT } from '../../../evm'
 import { FullNode } from '../../../node'
 import { legacyTransactionToEvmDescription } from '../../../primitives'
+import { RpcRequest } from '../../request'
 import { ApiNamespace } from '../namespaces'
 import { registerEthRoute } from './ethRouter'
 
@@ -41,31 +42,36 @@ registerEthRoute<typeof SendRawTransactionRequestSchema, SendRawTransactionRespo
   `eth_sendRawTransaction`,
   `${ApiNamespace.eth}/sendRawTransaction`,
   SendRawTransactionRequestSchema,
-  async (request, node): Promise<void> => {
-    Assert.isInstanceOf(node, FullNode)
-
-    const bytes = Buffer.from(request.data.transaction, 'hex')
-    const ethTransaction = LegacyTransaction.fromSerializedTx(bytes)
-    const evmDescription = legacyTransactionToEvmDescription(ethTransaction)
-
-    const { events } = await node.chain.evm.simulateTx({ tx: ethTransaction })
-    Assert.isNotUndefined(events, "No events returned from 'simulateTx'")
-
-    const raw = await node.wallet.createEvmTransaction({
-      evm: evmDescription,
-      evmEvents: events,
-    })
-
-    const transaction = await node.wallet.post({
-      transaction: raw,
-      spendingKey: GLOBAL_IF_ACCOUNT.spendingKey,
-    })
-
-    request.end({
-      hash: Buffer.from(ethTransaction.hash()).toString('hex'),
-      ifHash: transaction.transaction.hash().toString('hex'),
-      accepted: transaction.accepted,
-      broadcasted: transaction.broadcasted,
-    })
-  },
+  sendRawTransaction,
 )
+
+export async function sendRawTransaction(
+  request: RpcRequest<SendRawTransactionRequest, SendRawTransactionResponse>,
+  node: RpcContext,
+): Promise<void> {
+  Assert.isInstanceOf(node, FullNode)
+
+  const bytes = Buffer.from(request.data.transaction, 'hex')
+  const ethTransaction = LegacyTransaction.fromSerializedTx(bytes)
+  const evmDescription = legacyTransactionToEvmDescription(ethTransaction)
+
+  const { events } = await node.chain.evm.simulateTx({ tx: ethTransaction })
+  Assert.isNotUndefined(events, "No events returned from 'simulateTx'")
+
+  const raw = await node.wallet.createEvmTransaction({
+    evm: evmDescription,
+    evmEvents: events,
+  })
+
+  const transaction = await node.wallet.post({
+    transaction: raw,
+    spendingKey: GLOBAL_IF_ACCOUNT.spendingKey,
+  })
+
+  request.end({
+    hash: Buffer.from(ethTransaction.hash()).toString('hex'),
+    ifHash: transaction.transaction.hash().toString('hex'),
+    accepted: transaction.accepted,
+    broadcasted: transaction.broadcasted,
+  })
+}
--- ironfish/src/rpc/routes/eth/sendTransaction.ts
+++ ironfish/src/rpc/routes/eth/sendTransaction.ts
@@ -10,6 +10,7 @@ import { FullNode } from '../../../node'
 import { legacyTransactionToEvmDescription } from '../../../primitives'
 import { EthUtils } from '../../../utils/eth'
 import { AssertSpending } from '../../../wallet/account/account'
+import { RpcRequest } from '../../request'
 import { ApiNamespace } from '../namespaces'
 import { registerEthRoute } from './ethRouter'
 
@@ -50,62 +51,62 @@ registerEthRoute<typeof EthSendTransactionRequestSchema, EthSendTransactionRespo
   'eth_sendTransaction',
   `${ApiNamespace.eth}/sendTransaction`,
   EthSendTransactionRequestSchema,
-  async (request, node): Promise<void> => {
-    Assert.isInstanceOf(node, FullNode)
-
-    const account = node.wallet.listAccounts().find((a) => a.ethAddress === request.data.from)
-    Assert.isNotUndefined(account, 'Account not found')
-    AssertSpending(account)
-
-    const ethAccount = account.ethAddress
-      ? await node.chain.evm.getAccount(
-          Address.fromString(EthUtils.prefix0x(account.ethAddress)),
-        )
-      : undefined
-
-    const nonce = request.data.nonce
-      ? BigInt(request.data.nonce)
-      : ethAccount?.nonce ?? BigInt(0)
-
-    const gas = request.data.gas ? BigInt(request.data.gas) : 1000000n
-    const gasPrice = request.data.gasPrice ? BigInt(request.data.gasPrice) : 0n
-    const value = request.data.value ? BigInt(request.data.value) : undefined
-
-    const ethTransaction = new LegacyTransaction({
-      nonce: nonce,
-      to: request.data.to,
-      gasLimit: gas,
-      gasPrice: gasPrice,
-      value: value,
-      data: request.data.data,
-    })
-
-    const signed = ethTransaction.sign(Buffer.from(account.spendingKey, 'hex'))
-
-    const evmDescription = legacyTransactionToEvmDescription(signed)
-    const result = await node.chain.evm.simulateTx({ tx: signed })
-
-    const events = result.events
-    Assert.isUndefined(result.error, `Error simulating transaction ${result.error?.message}`)
-
-    const raw = await node.wallet.createEvmTransaction({
-      evm: evmDescription,
-      evmEvents: events,
-      account: account,
-    })
-
-    // TODO: This is pretty hacky to figure out which key to post with
-    const unshields = events ? events.filter((e) => e.name === 'unshield') : []
-    const spendingKey =
-      unshields.length > 0 ? account.spendingKey : GLOBAL_IF_ACCOUNT.spendingKey
-
-    const { transaction } = await node.wallet.post({
-      transaction: raw,
-      spendingKey: spendingKey,
-    })
-
-    request.end({
-      result: Buffer.from(transaction.hash()).toString('hex'),
-    })
-  },
+  sendTransaction,
 )
+
+export async function sendTransaction(
+  request: RpcRequest<EthSendTransactionRequest, EthSendTransactionResponse>,
+  node: RpcContext,
+): Promise<void> {
+  Assert.isInstanceOf(node, FullNode)
+
+  const account = node.wallet.listAccounts().find((a) => a.ethAddress === request.data.from)
+  Assert.isNotUndefined(account, 'Account not found')
+  AssertSpending(account)
+
+  const ethAccount = account.ethAddress
+    ? await node.chain.evm.getAccount(Address.fromString(EthUtils.prefix0x(account.ethAddress)))
+    : undefined
+
+  const nonce = request.data.nonce ? BigInt(request.data.nonce) : ethAccount?.nonce ?? BigInt(0)
+
+  const gas = request.data.gas ? BigInt(request.data.gas) : 1000000n
+  const gasPrice = request.data.gasPrice ? BigInt(request.data.gasPrice) : 0n
+  const value = request.data.value ? BigInt(request.data.value) : undefined
+
+  const ethTransaction = new LegacyTransaction({
+    nonce: nonce,
+    to: request.data.to,
+    gasLimit: gas,
+    gasPrice: gasPrice,
+    value: value,
+    data: request.data.data,
+  })
+
+  const signed = ethTransaction.sign(Buffer.from(account.spendingKey, 'hex'))
+
+  const evmDescription = legacyTransactionToEvmDescription(signed)
+  const result = await node.chain.evm.simulateTx({ tx: signed })
+
+  const events = result.events
+  Assert.isUndefined(result.error, `Error simulating transaction ${result.error?.message}`)
+
+  const raw = await node.wallet.createEvmTransaction({
+    evm: evmDescription,
+    evmEvents: events,
+    account: account,
+  })
+
+  // TODO: This is pretty hacky to figure out which key to post with
+  const unshields = events ? events.filter((e) => e.name === 'unshield') : []
+  const spendingKey = unshields.length > 0 ? account.spendingKey : GLOBAL_IF_ACCOUNT.spendingKey
+
+  const { transaction } = await node.wallet.post({
+    transaction: raw,
+    spendingKey: spendingKey,
+  })
+
+  request.end({
+    result: Buffer.from(transaction.hash()).toString('hex'),
+  })
+}
