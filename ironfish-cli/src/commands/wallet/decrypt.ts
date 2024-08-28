24e41c8819459ed6b474c3ac6646e29a67ac25f1
Rohan Jadvani <rohan@iflabs.network>
2024-08-28 15:56:12 -0400

    feat(cli): Hide passphrases in encryption commands

--- ironfish-cli/src/commands/wallet/decrypt.ts
+++ ironfish-cli/src/commands/wallet/decrypt.ts
@@ -32,7 +32,9 @@ export class DecryptCommand extends IronfishCommand {
 
     let passphrase = flags.passphrase
     if (!passphrase) {
-      passphrase = await inputPrompt('Enter a passphrase to decrypt the wallet', true)
+      passphrase = await inputPrompt('Enter a passphrase to decrypt the wallet', true, {
+        password: true,
+      })
     }
 
     try {
