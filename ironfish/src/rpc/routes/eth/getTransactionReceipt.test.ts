a71bfb5f9d9a3c89f1a3e31f86c7b6fa164ada16
Hugh Cunningham <hugh@iflabs.network>
2024-08-28 17:10:28 -0700

    updates getTransactionReceipt test to use a transaction with logs

--- ironfish/src/rpc/routes/eth/getTransactionReceipt.test.ts
+++ ironfish/src/rpc/routes/eth/getTransactionReceipt.test.ts
@@ -1,10 +1,19 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { Address } from '@ethereumjs/util'
+import { Log } from '@ethereumjs/evm'
+import { LegacyTransaction } from '@ethereumjs/tx'
+import { bytesToHex, hexToBytes } from '@ethereumjs/util'
+import { Asset } from '@ironfish/rust-nodejs'
+import { ethers } from 'ethers'
+import { Assert } from '../../../assert'
 import { Consensus } from '../../../consensus'
+import { ContractArtifact, GLOBAL_CONTRACT_ADDRESS } from '../../../evm'
 import { TransactionVersion } from '../../../primitives'
-import { evmDescriptionToLegacyTransaction } from '../../../primitives/evmDescription'
+import {
+  evmDescriptionToLegacyTransaction,
+  legacyTransactionToEvmDescription,
+} from '../../../primitives/evmDescription'
 import { useAccountFixture, useMinerBlockFixture } from '../../../testUtilities'
 import { createRouteTest } from '../../../testUtilities/routeTest'
 import { EthUtils } from '../../../utils'
@@ -19,56 +28,101 @@ describe('Route eth/getTransactionReceipt', () => {
   })
 
   it('should call retrieve correct block/transaction info', async () => {
-    const senderIf = await useAccountFixture(routeTest.node.wallet, 'sender')
-    const evmPrivateKey = Uint8Array.from(Buffer.from(senderIf.spendingKey || '', 'hex'))
+    const { chain, wallet } = routeTest.node
 
-    const evmSenderAddress = Address.fromPrivateKey(evmPrivateKey)
-    const raw = await routeTest.wallet.createEvmTransaction({
-      evm: {
-        nonce: 0n,
-        value: 0n,
-        gasLimit: 1000000000n,
-        gasPrice: 0n,
-        privateIron: BigInt(0),
-        publicIron: BigInt(0),
-        to: Buffer.from('a'.repeat(40), 'hex'),
-        data: Buffer.alloc(0),
-      },
+    const account = await useAccountFixture(wallet)
+    Assert.isNotNull(account.ethAddress)
+
+    const globalContract = new ethers.Interface(ContractArtifact.abi)
+
+    // Give account some IRON
+    const block = await useMinerBlockFixture(chain, undefined, account)
+    await expect(chain).toAddBlock(block)
+    await wallet.scan()
+
+    const encodedFunctionData = globalContract.encodeFunctionData('unshield_iron', [
+      account.ethAddress,
+      20n * 10n ** 8n,
+    ])
+
+    const tx = new LegacyTransaction({
+      nonce: 0n,
+      to: GLOBAL_CONTRACT_ADDRESS,
+      gasLimit: 1000000n,
+      gasPrice: 0n,
+      data: encodedFunctionData,
     })
 
-    const transaction = raw.post(senderIf.spendingKey)
+    const signed = tx.sign(Buffer.from(account.spendingKey, 'hex'))
 
-    const block1 = await useMinerBlockFixture(
-      routeTest.node.chain,
-      undefined,
-      undefined,
-      undefined,
-      [transaction],
-    )
-    await expect(routeTest.node.chain).toAddBlock(block1)
-    await routeTest.node.wallet.scan()
+    const description = legacyTransactionToEvmDescription(signed)
+
+    const { result: runTxResult, events: evmEvents } = await chain.evm.simulateTx({
+      tx: signed,
+    })
+    Assert.isNotUndefined(evmEvents)
+    const raw = await wallet.createEvmTransaction({
+      expiration: 0,
+      expirationDelta: 0,
+      evm: description,
+      evmEvents,
+      account,
+    })
+
+    const transaction = raw.post(account.spendingKey)
+
+    const block1 = await useMinerBlockFixture(chain, undefined, undefined, undefined, [
+      transaction,
+    ])
+
+    await expect(chain).toAddBlock(block1)
 
     const ethHash = evmDescriptionToLegacyTransaction(transaction.evm!).hash()
-    const result = await routeTest.client.eth.getTransactionReceipt(
-      Buffer.from(ethHash).toString('hex'),
-    )
+    const result = await routeTest.client.eth.getTransactionReceipt(bytesToHex(ethHash))
 
     expect(result.status).toEqual(200)
     expect(result.content).toMatchObject({
-      transactionHash: EthUtils.prefix0x(Buffer.from(ethHash).toString('hex')),
+      transactionHash: bytesToHex(ethHash),
       transactionIndex: '0x1',
       blockHash: EthUtils.prefix0x(block1.header.hash.toString('hex')),
-      blockNumber: '0x2',
-      from: EthUtils.prefix0x(evmSenderAddress.toString()),
-      to: EthUtils.prefix0x('a'.repeat(40)),
-      cumulativeGasUsed: '0x5208',
+      blockNumber: '0x3',
+      from: EthUtils.prefix0x(account.ethAddress),
+      to: EthUtils.prefix0x('f'.repeat(40)),
+      cumulativeGasUsed: '0x7a1a',
       effectiveGasPrice: '0x0',
-      gasUsed: '0x0',
+      gasUsed: '0x25be',
       contractAddress: null,
-      logs: [],
-      logsBloom: EthUtils.prefix0x('0'.repeat(512)),
+      logs: [
+        expect.objectContaining({
+          transactionHash: bytesToHex(ethHash),
+          transactionIndex: '0x1',
+          blockHash: EthUtils.prefix0x(block1.header.hash.toString('hex')),
+          blockNumber: '0x3',
+          address: EthUtils.prefix0x('f'.repeat(40)),
+        }),
+      ],
+      logsBloom: bytesToHex(runTxResult.bloom.bitvector),
       type: '0x0',
       status: '0x1',
+    })
+
+    expect(result.content.logs).toHaveLength(1)
+
+    const resultLog = result.content.logs[0]
+
+    const log: Log = [
+      hexToBytes(resultLog.address),
+      resultLog.topics.map(hexToBytes),
+      hexToBytes(resultLog.data),
+    ]
+
+    const events = chain.evm.decodeLogs([log])
+
+    expect(events).toHaveLength(1)
+    expect(events[0]).toEqual({
+      name: 'unshield',
+      assetId: Asset.nativeId(),
+      amount: 20n * 10n ** 8n,
     })
   })
 })
