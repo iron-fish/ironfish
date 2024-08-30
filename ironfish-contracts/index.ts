4d6610ca51bf010f4358dfe0924e7c50fc8a6c08
jowparks <joe@iflabs.network>
2024-08-30 14:30:22 -0700

    eth_getStorageAt (#5342)
    * eth_getStorageAt
    
    * add counter token so I can use in getStorageAt rpc

--- ironfish-contracts/index.ts
+++ ironfish-contracts/index.ts
@@ -1,5 +1,7 @@
 import IronfishContractJson from "./artifacts/contracts/Ironfish.sol/Ironfish.json"
 import GoldTokenJsonImport from "./artifacts/contracts/GoldToken.sol/GoldToken.json"
+import CounterJsonImport from "./artifacts/contracts/Counter.sol/Counter.json"
 
+export const CounterJson = CounterJsonImport
 export const GoldTokenJson = GoldTokenJsonImport
 export default IronfishContractJson
---
+++ ironfish/src/rpc/routes/eth/getStorageAt.ts
@@ -0,0 +1,51 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import { Address, bytesToHex, hexToBytes } from '@ethereumjs/util'
+import * as yup from 'yup'
+import { Assert } from '../../../assert'
+import { FullNode } from '../../../node'
+import { EthUtils } from '../../../utils'
+import { ApiNamespace } from '../namespaces'
+import { registerEthRoute } from './ethRouter'
+
+export type GetStorageAtRequest = [string, string, string]
+
+export const GetStorageAtRequestSchema: yup.MixedSchema<GetStorageAtRequest> = yup
+  .mixed<[string, string, string]>()
+  .defined()
+
+export type GetStorageAtResponse = string
+
+export const GetStorageAtResponseSchema: yup.StringSchema<GetStorageAtResponse> = yup
+  .string()
+  .defined()
+
+registerEthRoute<typeof GetStorageAtRequestSchema, GetStorageAtResponse>(
+  `eth_getStorageAt`,
+  `${ApiNamespace.eth}/getStorageAt`,
+  GetStorageAtRequestSchema,
+  async (request, node): Promise<void> => {
+    Assert.isInstanceOf(node, FullNode)
+
+    const [addressHex, position, blockNumber] = request.data
+    let block
+    // TODO handle pending transactions
+    if (blockNumber !== 'latest') {
+      block = await node.chain.getBlockAtSequence(parseInt(blockNumber))
+    } else {
+      block = await node.chain.getBlock(node.chain.latest)
+    }
+
+    const stateManager = await node.chain.blockchainDb.stateManager.withStateRoot(
+      block?.header.stateCommitment,
+    )
+    const code = bytesToHex(
+      await stateManager.getContractStorage(
+        new Address(hexToBytes(addressHex)),
+        Buffer.from(EthUtils.remove0x(position), 'hex'),
+      ),
+    )
+    request.end(code)
+  },
+)
