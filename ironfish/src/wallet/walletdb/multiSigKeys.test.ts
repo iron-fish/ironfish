1b640a20ab0ee5cfbb06c205c46bb7296ad7fb52
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2024-09-12 15:36:50 -0700

    defines MultisigHardwareSigner for Ledger multisig keys (#5366)
    a multisig account generated using a Ledger device will have a access to the
    participant identity, but not the secret or keyPackage
    
    uses a separate interface for MultisigHardwareSigner to cover this case and
    expands the MultisigKeys type to cover this interface
    
    the distinct interface allows us to store multisig keys for the Ledger case
    without a database migration for existing multisig keys

--- ironfish/src/wallet/walletdb/multiSigKeys.test.ts
+++ ironfish/src/wallet/walletdb/multiSigKeys.test.ts
@@ -1,7 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { MultisigCoordinator, MultisigSigner } from '../interfaces/multisigKeys'
+import {
+  MultisigCoordinator,
+  MultisigHardwareSigner,
+  MultisigSigner,
+} from '../interfaces/multisigKeys'
 import { MultisigKeysEncoding } from './multisigKeys'
 
 describe('multisigKeys encoder', () => {
@@ -26,6 +30,20 @@ describe('multisigKeys encoder', () => {
 
       const value: MultisigCoordinator = {
         publicKeyPackage: 'aaaa',
+      }
+      const buffer = encoder.serialize(value)
+      const deserializedValue = encoder.deserialize(buffer)
+      expect(deserializedValue).toEqual(value)
+    })
+  })
+
+  describe('with a hardware multisig', () => {
+    it('serializes the value into a buffer and deserialized to the original value', () => {
+      const encoder = new MultisigKeysEncoding()
+
+      const value: MultisigHardwareSigner = {
+        publicKeyPackage: 'aaaa',
+        identity: 'c0ffee',
       }
       const buffer = encoder.serialize(value)
       const deserializedValue = encoder.deserialize(buffer)
