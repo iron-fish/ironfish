815636fd34f05b77f1f5b865cb8ac100e691681f
Rohan Jadvani <rohan@iflabs.network>
2024-09-16 12:14:20 -0400

    Fix import

--- ironfish/src/wallet/walletdb/accountValue.test.ts
+++ ironfish/src/wallet/walletdb/accountValue.test.ts
@@ -1,7 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { encrypt, generateKey, SALT_LENGTH, XNONCE_LENGTH } from '@ironfish/rust-nodejs'
+import { encrypt, generateKey, xchacha20poly1305 } from '@ironfish/rust-nodejs'
 import {
   AccountValueEncoding,
   DecryptedAccountValue,
@@ -95,8 +95,8 @@ describe('AccountValueEncoding', () => {
     const encryptedValue: EncryptedAccountValue = {
       encrypted: true,
       data: encryptedData,
-      salt: Buffer.alloc(SALT_LENGTH),
-      nonce: Buffer.alloc(XNONCE_LENGTH),
+      salt: Buffer.alloc(xchacha20poly1305.XSALT_LENGTH),
+      nonce: Buffer.alloc(xchacha20poly1305.XNONCE_LENGTH),
     }
 
     const buffer = encoder.serialize(encryptedValue)
