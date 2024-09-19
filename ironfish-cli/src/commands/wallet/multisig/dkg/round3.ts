ea60d7f97774496149abcb47db462a0c4a228ea6
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2024-09-19 14:42:44 -0700

    updates ledger.ts for upgrade to @zondax/ledger-js (#5386)
    * updates ledger.ts for upgrade to @zondax/ledger-js
    
    removes response handling for error codes, which are no longer included in
    responses
    
    implements tryInstruction to handle errors from any app instructions
    
    adds methods to help discriminate between KeyResponse types and checks type of
    KeyResponse for each type of key
    
    adds method to recognize Ledger response errors
    
    defines classes for common recoverable Ledger errors: locked device and app not
    open. the error code for the app not being open is identified in the docs as a
    'technical error', but this is the error code we get when the app isn't open
    
    * adds ledger flag to participant creation (#5387)
    
    * adds ledger flag to participant creation
    
    adds dkgGetIDentity method to ledger util module
    
    adds '--ledger' flag to 'wallet:multisig:participant:create' command
    
    uses 'wallet/multisig/importParticipant' RPC to add the participant identity to
    the walletDb
    
    * implements ledger dkg round1 (#5389)
    
    * implements ledger dkg round1
    
    adds dkgRound1 method to Ledger util
    
    fills in logic of performRound1WithLedger
    
    reads identity from node and adds it to list of signer identities if it's
    missing
    
    * implements ledger dkg round2 (#5390)
    
    * implements ledger dkg round2
    
    adds dkgRound2 method to Ledger util
    
    implements performRound2WithLedger in round2 CLI command
    
    * implements ledger dkg round3 (#5391)
    
    * implements ledger dkg round3
    
    implements dkgRound3 method on Ledger util class
    
    implements dkgRetrieveKeys on Ledger util class to retrieve all shared multisig
    keys
    
    implements dkgGetPublicPackage on Ledger util class to retrieve public key
    package
    
    updates round3 command to run dkg round3, retrieve keys, get public key package,
    and import account with ledger flag
    
    * includes participant's own gsk share in round3 input
    
    * renames createParticipantWithLedger to getIdentityFromLedger

--- ironfish-cli/src/commands/wallet/multisig/dkg/round3.ts
+++ ironfish-cli/src/commands/wallet/multisig/dkg/round3.ts
@@ -1,6 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import {
+  deserializePublicPackage,
+  deserializeRound2CombinedPublicPackage,
+} from '@ironfish/rust-nodejs'
+import { AccountFormat, encodeAccountImport, RpcClient } from '@ironfish/sdk'
 import { Flags } from '@oclif/core'
 import { IronfishCommand } from '../../../../command'
 import { RemoteFlags } from '../../../../flags'
@@ -107,7 +112,13 @@ export class DkgRound3Command extends IronfishCommand {
     round2PublicPackages = round2PublicPackages.map((i) => i.trim())
 
     if (flags.ledger) {
-      await this.performRound3WithLedger()
+      await this.performRound3WithLedger(
+        client,
+        participantName,
+        round1PublicPackages,
+        round2PublicPackages,
+        round2SecretPackage,
+      )
       return
     }
 
@@ -125,10 +136,16 @@ export class DkgRound3Command extends IronfishCommand {
     )
   }
 
-  async performRound3WithLedger(): Promise<void> {
+  async performRound3WithLedger(
+    client: RpcClient,
+    participantName: string,
+    round1PublicPackagesStr: string[],
+    round2PublicPackagesStr: string[],
+    round2SecretPackage: string,
+  ): Promise<void> {
     const ledger = new Ledger(this.logger)
     try {
-      await ledger.connect()
+      await ledger.connect(true)
     } catch (e) {
       if (e instanceof Error) {
         this.error(e.message)
@@ -136,5 +153,76 @@ export class DkgRound3Command extends IronfishCommand {
         throw e
       }
     }
+
+    const identityResponse = await client.wallet.multisig.getIdentity({ name: participantName })
+    const identity = identityResponse.content.identity
+
+    // Sort packages by identity
+    const round1PublicPackages = round1PublicPackagesStr
+      .map(deserializePublicPackage)
+      .sort((a, b) => a.identity.localeCompare(b.identity))
+
+    // Filter out packages not intended for participant and sort by sender identity
+    const round2CombinedPublicPackages = round2PublicPackagesStr.map(
+      deserializeRound2CombinedPublicPackage,
+    )
+    const round2PublicPackages = round2CombinedPublicPackages
+      .flatMap((combined) =>
+        combined.packages.filter((pkg) => pkg.recipientIdentity === identity),
+      )
+      .sort((a, b) => a.senderIdentity.localeCompare(b.senderIdentity))
+
+    // Extract raw parts from round1 and round2 public packages
+    const participants = []
+    const round1FrostPackages = []
+    const gskBytes = []
+    for (const pkg of round1PublicPackages) {
+      // Exclude participant's own identity and round1 public package
+      if (pkg.identity !== identity) {
+        participants.push(pkg.identity)
+        round1FrostPackages.push(pkg.frostPackage)
+      }
+
+      gskBytes.push(pkg.groupSecretKeyShardEncrypted)
+    }
+
+    const round2FrostPackages = round2PublicPackages.map((pkg) => pkg.frostPackage)
+
+    // Perform round3 with Ledger
+    await ledger.dkgRound3(
+      0,
+      participants,
+      round1FrostPackages,
+      round2FrostPackages,
+      round2SecretPackage,
+      gskBytes,
+    )
+
+    // Retrieve all multisig account keys and publicKeyPackage
+    const dkgKeys = await ledger.dkgRetrieveKeys()
+
+    const publicKeyPackage = await ledger.dkgGetPublicPackage()
+
+    const accountImport = {
+      ...dkgKeys,
+      multisigKeys: {
+        publicKeyPackage: publicKeyPackage.toString('hex'),
+        identity,
+      },
+      version: 4,
+      name: participantName,
+      spendingKey: null,
+      createdAt: null,
+    }
+
+    // Import multisig account
+    const response = await client.wallet.importAccount({
+      account: encodeAccountImport(accountImport, AccountFormat.Base64Json),
+    })
+
+    this.log()
+    this.log(
+      `Account ${response.content.name} imported with public address: ${dkgKeys.publicAddress}`,
+    )
   }
 }
