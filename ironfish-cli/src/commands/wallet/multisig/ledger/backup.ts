e8c061af6d1116f70b4c40a8a87badde82f8743e
Hugh Cunningham <hugh@iflabs.network>
2024-09-20 11:05:58 -0700

    fixes lint

--- ironfish-cli/src/commands/wallet/multisig/ledger/backup.ts
+++ ironfish-cli/src/commands/wallet/multisig/ledger/backup.ts
@@ -26,6 +26,8 @@ export class MultisigLedgerBackup extends IronfishCommand {
     this.log(encryptedKeys.toString('hex'))
     this.log()
     this.log('Please save the encrypted keys show above.')
-    this.log('Use `ironfish wallet:multisig:ledger:restore` if you need to restore the keys to your Ledger.')
+    this.log(
+      'Use `ironfish wallet:multisig:ledger:restore` if you need to restore the keys to your Ledger.',
+    )
   }
 }
