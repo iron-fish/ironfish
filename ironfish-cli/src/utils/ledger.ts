84e766989f1c7a57811fa5ad944c1471e2b01ad9
Hugh Cunningham <57735705+hughy@users.noreply.github.com>
2024-09-23 07:47:11 -0700

    adds cli command to import multisig account from ledger (#5409)
    reads identity, shared multisig keys, and public key package from Ledger device
    
    imports multisig account to wallet and prompts for new name if the provided name
    is taken
    
    removes hardcoded account version number from other ledger imports (these
    imports aren't from serialized accounts, so the schema version should always be
    the current version)

--- ironfish-cli/src/utils/ledger.ts
+++ ironfish-cli/src/utils/ledger.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import {
+  ACCOUNT_SCHEMA_VERSION,
   AccountImport,
   createRootLogger,
   CurrencyUtils,
@@ -135,7 +136,7 @@ export class Ledger {
     }
 
     const accountImport: AccountImport = {
-      version: 4, // ACCOUNT_SCHEMA_VERSION as of 2024-05
+      version: ACCOUNT_SCHEMA_VERSION,
       name: 'ledger',
       viewKey: responseViewKey.viewKey.toString('hex'),
       incomingViewKey: responseViewKey.ivk.toString('hex'),
