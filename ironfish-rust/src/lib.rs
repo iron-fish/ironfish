57ca7ee3fa1c3ccd1cbc35cfcab9a35b858c363b
andrea <andrea@iflabs.network>
2024-10-31 15:31:28 -0700

    Make the Sapling parameters optional for documentation builds
    This is to allow our documentation to build on platforms like docs.rs,
    which need to build the documentation for our crates, but don't have
    access to the Sapling parameters.
    
    This also slightly improves the experience for developers who use this
    crate for the first time: if they use the crate without the Sapling
    parameters, they'll now get a helpful warning instead of a panic.

--- ironfish-rust/src/lib.rs
+++ ironfish-rust/src/lib.rs
@@ -1,8 +1,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-use blstrs::Bls12;
-use ironfish_bellperson::groth16;
+
+#[cfg(feature = "transaction-proofs")]
+mod sapling;
 
 pub mod assets;
 pub mod errors;
@@ -39,69 +40,7 @@ pub use {
 
 #[cfg(feature = "benchmark")]
 pub use ironfish_zkp::primitives::ValueCommitment;
-
+#[cfg(feature = "transaction-proofs")]
+pub use sapling::Sapling;
 #[cfg(feature = "transaction-proofs")]
 pub use transaction::ProposedTransaction;
-
-// The main entry-point to the sapling API. Construct this with loaded parameters, and then call
-// methods on it to do the actual work.
-//
-// spend and output are two arithmetic circuits for use in zksnark calculations provided by bellperson.
-// Though the *_params have a verifying key on them, they are not the prepared verifying keys,
-// so we store the prepared keys separately at the time of loading the params.
-//
-// The values are all loaded from a file in serialized form.
-pub struct Sapling {
-    pub spend_params: groth16::Parameters<Bls12>,
-    pub output_params: groth16::Parameters<Bls12>,
-    pub mint_params: groth16::Parameters<Bls12>,
-    pub spend_verifying_key: groth16::PreparedVerifyingKey<Bls12>,
-    pub output_verifying_key: groth16::PreparedVerifyingKey<Bls12>,
-    pub mint_verifying_key: groth16::PreparedVerifyingKey<Bls12>,
-}
-
-#[cfg(feature = "transaction-proofs")]
-impl Sapling {
-    /// Initialize a Sapling instance and prepare for proving. Load the parameters from files
-    /// at a known location (`$OUT_DIR/sapling_params`).
-    pub fn load() -> Self {
-        let spend_bytes = include_bytes!(concat!(
-            env!("OUT_DIR"),
-            "/sapling_params/sapling-spend.params"
-        ));
-        let output_bytes = include_bytes!(concat!(
-            env!("OUT_DIR"),
-            "/sapling_params/sapling-output.params"
-        ));
-        let mint_bytes = include_bytes!(concat!(
-            env!("OUT_DIR"),
-            "/sapling_params/sapling-mint.params"
-        ));
-
-        let spend_params = Sapling::load_params(&spend_bytes[..]);
-        let output_params = Sapling::load_params(&output_bytes[..]);
-        let mint_params = Sapling::load_params(&mint_bytes[..]);
-
-        let spend_vk = groth16::prepare_verifying_key(&spend_params.vk);
-        let output_vk = groth16::prepare_verifying_key(&output_params.vk);
-        let mint_vk = groth16::prepare_verifying_key(&mint_params.vk);
-
-        Sapling {
-            spend_verifying_key: spend_vk,
-            output_verifying_key: output_vk,
-            mint_verifying_key: mint_vk,
-            spend_params,
-            output_params,
-            mint_params,
-        }
-    }
-
-    /// Load sapling parameters from a provided filename. The parameters are huge and take a
-    /// couple seconds to load. They primarily contain the "toxic waste" for a specific sapling
-    /// curve.
-    ///
-    /// NOTE: If this is stupidly slow for you, try compiling in --release mode
-    fn load_params(bytes: &[u8]) -> groth16::Parameters<Bls12> {
-        groth16::Parameters::read(bytes, false).unwrap()
-    }
-}
