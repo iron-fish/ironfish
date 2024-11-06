7d26fb40fb7bf66847efa122dc3d4173be82df86
Hugh Cunningham <hugh@iflabs.network>
2024-11-06 14:18:45 -0800

    removes superfluous comments

--- ironfish/src/migrations/data/033-encrypted-wallet-template/old/index.ts
+++ ironfish/src/migrations/data/033-encrypted-wallet-template/old/index.ts
@@ -5,15 +5,6 @@ import { IDatabase, IDatabaseStore, StringEncoding } from '../../../../storage'
 import { AccountValue, AccountValueEncoding } from './accountValue'
 import { MasterKeyValue, MasterKeyValueEncoding } from './masterKeyValue'
 
-/* The schemaOld.ts file must define the value schema and database encoding for
- * ALL datastores that the migration reads from. Even if the migration does not
- * modify a datastore _A_, if the migration needs to read data from _A_ in order
- * to write to another datastore _B_, then the schema and encoding for _A_ must
- * be defined in schemaOld.ts.
- *
- * The example below is taken from Migration022, which added the viewKey field
- * to the AccountValue schema. */
-
 export function GetOldStores(db: IDatabase): {
   accounts: IDatabaseStore<{ key: string; value: AccountValue }>
   masterKey: IDatabaseStore<{ key: string; value: MasterKeyValue }>
