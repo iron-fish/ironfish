56fdf5540f72287afa728690cb7dd18573155fa9
andrea <andrea@iflabs.network>
2024-12-17 17:45:24 -0800

    WASM: add support for mnemonics

--- ironfish-rust-wasm/src/keys/mod.rs
+++ ironfish-rust-wasm/src/keys/mod.rs
@@ -2,11 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+mod mnemonics;
 mod proof_generation_key;
 mod public_address;
 mod sapling_key;
 mod view_keys;
 
+pub use mnemonics::Language;
 pub use proof_generation_key::ProofGenerationKey;
 pub use public_address::PublicAddress;
 pub use sapling_key::SaplingKey;
